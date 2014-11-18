8b97407e9c4b1ff88f27e6f34abaca1344cf3fd0
ryanlei <lei.ryan@gmail.com>
2014-11-18 01:04:37 -0800

    Implemented drawTrip, plot the trip given trip_id

--- CFC_WebApp/main/gmap_display.py
+++ CFC_WebApp/main/gmap_display.py
@@ -10,42 +10,34 @@ from pytz import timezone
 from dateutil import parser
 from home import detect_home, detect_home_from_db
 from work_place import  detect_work_office, detect_daily_work_office,detect_work_office_from_db,detect_daily_work_office_from_db
-from commute import get_daily_morning_commute_sections
-from work_time import get_work_start_time, get_work_end_time,get_Alluser_work_start_time_pie, get_Alluser_work_start_time, get_user_work_start_time_pie,\
-    get_Alluser_work_end_time_pie,get_Alluser_work_end_time
 from common import most_common, get_first_daily_point, Include_place,berkeley_area,getModeShare
 from distance import get_morning_commute_distance_pie,get_evening_commute_distance_pie
 from get_database import get_mode_db, get_section_db, get_trip_db, get_test_db
-from Berkeley import get_berkeley_mode_share_by_distance
-from carbon import getModeCarbonFootprint,getFootprintCompare
-# from zipcode import getZipcode,get_mode_share_by_Zipcode,get_period_mode_share_by_Zipcode
-from modeshare import get_Alluser_mode_share_by_distance,get_user_mode_share_by_distance
 import math
 import pygmaps
 from datetime import date, timedelta
-from visualize import Berkeley_pop_route,carbon_by_zip
 from uuid import *
 from tripManager import calDistance
 from pygeocoder import Geocoder
 
-USER = '1cc03940-57f5-3e35-a189-55d067dc6460'
-USER_B = '5322f635-a82c-3677-a1a4-5c26804a90b7'
 POINTS = 'points'
 PATH = 'path'
 ALL = 'all'
-COLOR = {0:"#00FFFF",
-         1:"#0000FF",
-         2:"#00FF00",
-         3:"#FFFF00",
-         4:"#FF0000",
+COLOR = {1:"#0000FF",       #walking - blue
+         2:"#00FF00",       #running - green
+         3:"#FFFF00",       #cycling - yellow
+         4:"#FF0000",       #transport - red
+         5:"#00FFFF",       #bus - aqua
+         6:"#FF8C00",       #train - darkOrange
+         7:"#778899",       #car - grey 
+         8:"#808000",       #mixed - olive
+         9:"#87CEFA"        #air - skyBlue
         }
 
-STARTPOINT = 'soda hall, berkeley'
-ENDPOINT = '2610 warring st. berkeley'
 
 
 def display_home(): 
-        mymap_home = pygmaps.maps(37.8656475757, -122.258774009,14)
+        mymap_home = pygmaps.maps(37.8556475757, -122.248774009,14)
         for user in get_section_db().distinct('user_id'):
             print(user)
             user_home=detect_home(user)
@@ -65,8 +57,8 @@ def Date(date):
 
 #Description: Display on google map, this user's trip during the specified date
 #             Look for html file in gmap_display folder, file is named as "date
-#             _user.html" eg. '05-12-2014_1cc03940-57f5-3e35-a189-55d067dc6460.h#             tml'
-#user: a string user id, eg. '1cc03940-57f5-3e35-a189-55d067dc6460'
+#             _user.html" eg. '1234567890.html'
+#user: a string user id, eg. '1234567890'
 #date: a string specifying the date, eg. '05/30/14'
 #option: a string indicating how to display the trip
 #        eg. 'points' for point
@@ -96,12 +88,8 @@ def searchTrip(user, period, startpoint, endpoint, mode, option):
     sectionList = []
     startpoint = Geocoder.geocode(startpoint)[0].coordinates
     endpoint = Geocoder.geocode(endpoint)[0].coordinates
-    #gmap.addpoint(startpoint[0], startpoint[1], COLOR[4])
-    #gmap.addpoint(endpoint[0], endpoint[1], COLOR[1])
 
     for section in get_section_db().find({"$and":[
-        #{'user_id':user_id},
-        #{"section_start_datetime": {"$gte": start, "$lt":end}},
         {"mode": mode},
         {"mode": {"$ne": 'airplane'}},
         {"mode": {"$ne":7}},
@@ -128,7 +116,8 @@ def drawSection(section, option, gmap, Color = 'default'):
             color = COLOR[section['mode']]
         for point in track_points:
             coordinate = point['track_location']['coordinates']
-            coordinate_tuple = tuple(coordinate)
+            
+            coordinate_tuple = tuple([coordinate[1], coordinate[0]])
             path.append(coordinate_tuple)
             if option == POINTS or option == ALL:
                 # coordinates are in GeoJSON format, ie lng, lat
@@ -148,17 +137,19 @@ def compareTrips(section1, section2):
     drawSection(section2, PATH,gmap,COLOR[4])
     gmap.draw('gmap_display/compare.html')
 
-def test():
-    start,end = Date('05/25/14')
-    sectionList = []
-    for section in get_section_db().find({"$and":[{'user_id':UUID(USER)},{"section_start_datetime": {"$gte": start, "$lt": end}}]}):
-        track_points = section['track_points']
-        if track_points != []:
-            sectionList.append(section)
-        #print section
-    #print len(sectionList)
-    compareTrips(sectionList[0], sectionList[3])
 
 
+def drawTrip(trip_id, db, gmap):
+    trip = None
+    trip_cursor = db.Stage_Trips.find({'trip_id': trip_id})
+
+    if trip_cursor.count() == 1:
+        trip = trip_cursor[0]
+    else:
+        print "Duplicated trip_id: " + trip_id
+        exit()
+    sections = db.Stage_Sections.find({'trip_id': trip_id})
+    drawSections(sections, ALL, gmap)
+    return gmap
     
 
