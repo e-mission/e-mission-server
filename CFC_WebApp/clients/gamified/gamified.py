d1584e038a3d1cf9d31433380498c13d15b6ccf7
K. Shankari <shankari@eecs.berkeley.edu>
2014-11-22 22:02:52 -0800

    Define a "score" field in the user profile to maintain a stateful score that can be updated
    Define the function to update it in gamified, and add a test case for it as
    well. Note that the score field should really be refactored out from the user
    profile since it applies only to the gamification client. The refactoring is
    deferred until a bit later, when we have a better idea of what the use cases
    are...

--- CFC_WebApp/clients/gamified/gamified.py
+++ CFC_WebApp/clients/gamified/gamified.py
@@ -1,7 +1,8 @@
 import logging
 from get_database import get_section_db
 from main import carbon, common
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
+from dao.user import User
 
 # sb375 is a weekly goal - we convert it to daily by dividing by 7
 sb375DailyGoal = 40.142892/7
@@ -39,24 +40,50 @@ def getScoreComponents(user_uuid, start, end):
   return components
 
 def calcScore(componentArr):
-  [pctClassified, mineMinusOptimal, allDriveMinusMine] = componentArr
+  [pctClassified, mineMinusOptimal, allDriveMinusMine, sb375DailyGoal] = componentArr
   # We want the ratio between the three components to be 5 : 3 : 2
   # Let's just convert everything to percentages to keep the ratios consistent
   # Also, we subtract the mineMinusOptimal term, since being way above optimal
   # should lower your score
-  return 5 * pctClassified + 3 * allDriveMinusMine - 2 * mineMinusOptimal
+  return 5 * pctClassified + 3 * allDriveMinusMine - 2 * mineMinusOptimal + 1 * sb375DailyGoal
+
+def getScore(user_uuid, start, end):
+    components = getScoreComponents(user_uuid, start, end)
+    return calcScore(components)
+
+# Ok so this is a big tricky to get right.
+# We want this to be in increments of a day, so that the SB375 target
+# calculation makes sense.
+# The high level plan is to run it around midnight everyday and update the
+# score based on the day that just passed. But what exactly does "around
+# midnight" mean?
+# It can't be just before midnight, because then we will skip trips around midnight
+# I guess we start right after midnight
+# Couple of other challenges:
+# - What to do about trips that span days?
+# - What to do about people in other time zones who have a different midnight?
+# In order to handle both of these, we should really have task scheduled from
+# the app instead of a cronjob, and have it track the last time it was run. But
+# let's do the cheap solution for now so that we know whether it works at all.
+def updateScore(user_uuid):
+    today = datetime.now().date()
+    yesterday = today - timedelta(days = 1)
+    yesterdayStart = datetime.combine(yesterday, time.min)
+    todayStart = datetime.combine(today, time.min)
+
+    user = User.fromUUID(user_uuid)
+    # TODO: getScore() shouldn't really be defined in User because it doesn't apply to all clients.
+    # Need to figure out how to structure client specific profile enhancements.
+    # Should they even be stored in the user profile?
+    newScore = user.getScore() + getScore(user_uuid, yesterdayStart, todayStart)
+    user.setScore(newScore)
 
 def getResult(user_uuid):
   # This is in here, as opposed to the top level as recommended by the PEP
   # because then we don't have to worry about loading bottle in the unit tests
   from bottle import template
 
-  now = datetime.now()
-  yearago = now - timedelta(days=365)
-
-  components = getScoreComponents(user_uuid, yearago, now)
-  [pctClassified, mineMinusOptimal, allDriveMinusMine] = components
-  score = calcScore(components)
+  score = user.fromUUID(user_uuid).getScore()
   renderedTemplate = template("clients/gamified/result_template.html",
                               pctClassified = pctClassified,
                               mineMinusOptimal = mineMinusOptimal,
