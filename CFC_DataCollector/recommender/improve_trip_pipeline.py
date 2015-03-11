ef7b4dfe9dc8bea3f9a4c6ba9a9f64dac4df6919
Lesley Chiang <lechiang@berkeley.edu>
2015-03-10 19:48:14 -0700

    update improve trip pipeline

--- CFC_DataCollector/recommender/improve_trip_pipeline.py
+++ CFC_DataCollector/recommender/improve_trip_pipeline.py
@@ -5,17 +5,20 @@
 #4. Augment perturbed trips
 #5. Apply model to alternate and perturbed trips
 #6. Return list of improved trips
+
+#construction of model' can part of utility model pipeline or this pipeline
+
+#getAugmentedTrips: retrieve augmented trips from database, 
+		#determine perturbed trips,
+		#combine augmented and perturbed trips (which may or may not be cached, tbd)
 ######
-def buildRecommendedTrip(user_uuid, alternative_trips, model):
+def buildRecommendedTrip(user_id, alternative_trips):
 	#get improved trips
 	tripsToImprove = getTripsToImprove(user_uuid, alternate_trips) 
-
+	model = UserUtilityModel.find_from_db(user_id)
 	improved_trips = []
 	#for each trip
 	for trip in tripsToImprove:
-		#getAugmentedTrips: retrieve augmented trips from database, 
-		#determine perturbed trip,
-		#combine augmented and perturbed trips (which may or may not be cached, tbd)
 		alt_trips = getAugmentedTrips(trip)
 		ultilized_trips = utilFunction(model, alt_trips)
 		improved_trips.add(ultilized_trips)
