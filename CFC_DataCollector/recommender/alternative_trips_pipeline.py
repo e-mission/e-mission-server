7e724852ab6c8d2827e403018de5a25a8cab5bf2
Shanthi Shanmugam <shanthish8@gmail.com>
2015-03-17 18:30:50 -0700

    Cleaned Alternative Trips Pipeline

--- CFC_DataCollector/recommender/alternative_trips_pipeline.py
+++ CFC_DataCollector/recommender/alternative_trips_pipeline.py
@@ -4,51 +4,19 @@ from common import store_trip_in_db
 from trip import E_Mission_Trip
 #import Profiles
 
-def alternative_trips(trip_list):
-    '''
-    store trips to database
-    '''
-    for trip in trip_list:
-        store_trip_in_db(trip)
+def run_alternative_trips_pipeline(trip_iterator):
+
+    while trip_iterator.hasNext():
+        list_of_perturbed_trips = find_perturbed_trips(trip_iterator.next())
+        schedule_queries(list_of_perturbed_trips)
+
+def store_alternative_trips(tripObj):
+    # store populated tripObj with _id (concatenated trip id and user id)
+
+
+def get_alternative_trips(_id):
+    # User Utility Pipeline calls this to get alternatve trips for one original trip (_id)
     
-
-
-def get_user_trips(user_ID, filter_queries):
-    '''
-    get a list of user trips, using get_trips logic (filter? will produce canonical trip?)
-
-    '''
-    return []
-
-def get_user_ids(filter_queries):
-    '''
-    get a list of user ids
-    '''
-
-    return []
-
-def get_alternative_trips(user_ID, trip_ID):
-    '''
-    return an array of Trip objects. Call Perturbation Module here (perturb times)
-    Will query at appropriate times.
-    '''
-    routes = directions["routes"]
-    trips = []
-    for route in routes:
-        legs = []
-        for leg in route["legs"]:
-            leg = Leg("some_trip_id")
-            leg.starting_point = (leg["steps"][0]["start_location"]["lat"], leg["steps"][0]["start_location"]["lng"])
-            leg.ending_point = (leg["steps"][-1]["end_location"]["lat"], leg["steps"][-1]["end_location"]["lng"])
-            leg.mode = step["travel_mode"]
-            totalDuration = 0
-            totalDistance = 0
-            for step in leg:
-                totalDuration += step["duration"]["value"]
-                totalDistance += step["distance"]["value"]
-            legs.push(leg)
-
-    return []
 
 
 #get_user_ids(filter_queries)
