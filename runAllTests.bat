0477ab6ffdf61bef907eb2bca868ef11eb66d5ef
Josh Zarrabi <joshzarrabi@berkeley.edu>
2015-03-19 11:17:58 -0700

    Added batch file to run all tests for windows users

---
+++ runAllTests.bat
@@ -0,0 +1,15 @@
+pushd "%cd%"/CFC_WebApp 
+cp config.json.sample keys.json 
+cp keys.json.sample keys.json 
+python -m unittest discover -s tests -p Test || goto :error
+popd
+
+pushd "%cd%"/CFC_DataCollector
+cp ../CFC_WebApp/config.json config.json
+cp ../CFC_WebApp/keys.json.sample keys.json
+set PYTHONPATH=. py 
+python -m unittest discover -s tests -p Test* || goto :error
+popd
+
+:error
+	exit /b %errorlevel%
