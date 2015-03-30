4d5323ca0756fdd7683d06ef2ad7102a0dd8894b
Jeff <jeffdh5@gmail.com>
2015-03-29 17:55:20 -0700

    fixed merge issue

--- CFC_DataCollector/recommender/alternative_trips_module.py
+++ CFC_DataCollector/recommender/alternative_trips_module.py
@@ -1,37 +1,30 @@
 import get_trips
-from common import store_trip_in_db, find_perturbed_trips
+from common import store_trip_in_db, find_perturbed_trips, get_uuid_list
 from trip import E_Mission_Trip
 from get_database import get_alternative_trips_db, get_perturbed_trips_db
 
 #import Profiles
 
 """
-
 High level overivew of alternative_trips_pipeline
-
 The main method for this pipeline is calc_perturbed_trips
 We first construct a trip iterator, and look at each trip where the pipelineFlags attribte "alternativesStarted" is False
 For each of these trips, we create an array of trip objects, which are modifications to the original trip object with perturbed times
 We also have a database collection that associates each original trip, with all of its perturbed trips
-
 For each array of perturbed trip objects, we schedule those queries via CRON jobs
 We initialize each perturbed trip as None, and we update the collection as the queries are being made
-
-
 Overview of helper files relevant to this pipeline:
     -query.py -> makes a google maps query immediately to get the directions
               -> will also store the query into the database collection
     -perturb.py -> schedules calls to query.py on cron jobs
-
     @TODO: this doesn't exist yet, but it would make more sense than to put it in here in order to keep the code clean
     -database_util.py -> contains all the helper methods to abstract out interaction with the database
-
-
 """
 
 
 # Invoked in recommendation pipeline to get perturbed trips user should consider
 def calc_alternative_trips(trip_iterator):
+
     while (trip_iterator.next() != None):
         curr_trip = trip_iterator.next()
         if not curr_trip.getPipelineFlags().alternativesStarted:
@@ -44,62 +37,33 @@ def calc_alternative_trips(trip_iterator):
             schedule_queries(list_of_perturbed_trips)
 
 
-
-#this call should be made asynchronously
-#if it returns true, that means the perturbation queries were finished, and we are now able to move on to the next pipeline
-#if it returns false, that means the perturbation queries were not finished
-def invoke_next_pipeline(_id):
-    if check_all_queries_made(_id) == True:
-        #invoke next pipeline - glue team please fill this in
-        return True
-    else:
-        return False
-
-
-#helper function for invoke_next_pipeline()
-#checks to see if all the perturbation queries have been finished
-def check_all_queries_made(_id):
-    #this looks inside the alternatives collection
-    perturbed_trips = query_alternatives(u_id) #looks into the database
-    for perturbed_u_id in perturbed_trips:
-        if perturbed_trips[perturbed_u_id] == None:
-            return False
-    return True
-
+#@TODO: put these methods in database_util.py
+#@TODO: These stubs need to be passed in a unique id as well as a trip ppboject
+#when we store in the database, they need to be able to associate the perturbed trips back to the original trip, 
+#otherwise the query made by the utility model team will be very difficult
 
 def store_alternative_trips(tripObj):
     # store populated tripObj with _id (concatenated trip id and user id)
     db = get_alternative_trips_db()
     _id = tripObj.get_id()
-    json = E_Mission_Trip.trip_to_json(tripObj)
-    db.insert_one({_id : json})
+    db.insert_one({_id : tripObj})
 
 def get_alternative_trips(_id):
     # User Utility Pipeline calls this to get alternatve trips for one original trip (_id)
     # db = get_alternative_trips_db()
     # _id = tripObj.get_id()
     # return db.find(_id)
-    return [Trip(jsonStr) for jsonStr in get_alternative_trips_db().find({'_id' : _id})].__iter__()
+    return [trip.E_Mission_Trip.trip_from_json(jsonStr) for jsonStr in get_alternative_trips_db().find({'_id' : _id})].__iter__()
 
 def store_perturbed_trips(tripObj):
     # store populated tripObj with _id (concatenated trip id and user id)
     db = get_perturbed_trips_db()
     _id = tripObj.get_id()
-    json = E_Mission_Trip.trip_to_json(tripObj)
-    db.insert_one({_id : json})
+    db.insert_one({_id : tripObj})
 
 def get_perturbed_trips(_id):
     # User Utility Pipeline calls this to get alternatve trips for one original trip (_id)
     # db = get_perturbed_trips_db()
     # _id = tripObj.get_id()
     # return db.find(_id)
-    return [Trip(jsonStr) for jsonStr in get_perturbed_trips_db().find({'_id' : _id})].__iter__()
-
-
-
-
-# Invoked to find the choices user had when making their trip
-#Jeff - what is this code doing? it seems to do nothing
-def calc_alternative_trips(trip_iterator):
-    #schedule_queries(trip_iterator) 
-    pass
+    return [trip.E_Mission_Trip.trip_from_json(jsonStr) for jsonStr in get_perturbed_trips_db().find({'_id' : _id})].__iter__()
