8090c5620d72cc7836f188a73da084aaa724ca86
jessica wong <jesca116@berkeley.edu>
2015-04-02 13:35:39 -0700

    added canonical trip code to query_modules.py

--- CFC_DataCollector/recommender/query_modules.py
+++ CFC_DataCollector/recommender/query_modules.py
@@ -4,21 +4,65 @@ structured:
 module_name { query_string: function_for_query }
 
 """
-from get_database import get_section_db
+import sys
+import os
+import math
+sys.path.append("%s" % os.getcwd())
+sys.path.append("%s/../CFC_WebApp/" % os.getcwd())
+
+from main.K_medoid_2 import kmedoids, user_route_data
+from main.route_matching import update_user_routeDistanceMatrix, update_user_routeClusters
+
+
+from get_database import get_section_db, get_routeCluster_db
 import trip
+import random
+
 
 #returns the top trips for the user, defaulting to the top 10 trips
-def getTopTrips(uid, options = 10):
-    """ options that we can have for get top trips:
-    """
-    return []
+def getCanonicalTrips(uid, number_returned = 10):
+    canonical_trip_list = []
+    x = 0
+    # if route clusters return nothing, then get common routes for user
+    clusters = get_routeCluster_db().find_one({'$and':[{'user':uid},{'method':'lcs'}]})['clusters']
+    print get_section_db().find({"user_id": uid}).count()
+
+    if len(clusters) == 0:
+        print "updating route clusters"
+        # no clusters found for user, run algorithm to populate database
+        routes_user = user_route_data(uid,get_section_db())
+        print "routes_user = %s" % routes_user
+        update_user_routeDistanceMatrix(uid,routes_user,step1=100000,step2=100000,method='lcs')
+        clusters_user = kmedoids(routes_user,int(math.ceil(len(routes_user)/8) + 1),uid,method='lcs')
+        print "clusters_users = %s" % str(clusters_user)
+        update_user_routeClusters(uid,clusters_user[2],method='lcs')
+        #try getting clusters again
+        clusters = get_routeCluster_db().find_one({'$and':[{'user':uid},{'method':'lcs'}]})['clusters']
+        #assert len(clusters) > 0, ("Could not get any route clusters for user with uid ", uid)
+        if len(clusters) == 0:
+            #TODO: returns a random ten trips right now if clusters aren't created
+            for trip in get_section_db().find({"user_id":uid}):
+                if x <= number_returned:
+                    canonical_trip_list.append(trip)
+                    x+=1
+
+            return iter(canonical_trip_list)
+
+    # sort user route clusters to get most popular trips
+    print "After constructing clusters, list is %s" % clusters
+    sorted_clusters = sorted(clusters, key=lambda cluster_key: len(user_route_clusters[cluster_key]), reverse=True)
+    for cid in sorted_clusters:
+        if x <= number_returned:
+            canonical_trip_list.append(random.choice(user_route_clusters[cid]))
+            x+=1
+
+    return iter(canonical_trip_list)
+
 
 #returns all trips to the user
 def getAllTrips(uid):
-
     #return [trip.E_Mission_Trip.trip_from_json(jsonStr) for jsonStr in get_section_db().find({'user_id' : uid})].__iter__()
     return [trip.E_Mission_Trip(jsonStr) for jsonStr in get_section_db().find({'user_id' : uid})].__iter__()
-
 
 def trip_comparator_date(less_than):
     def compare(x, y):
@@ -29,7 +73,6 @@ def trip_comparator_date(less_than):
         else:
             return 0
     return compare
-
 
 def getRecentTrips(uid, options = 10):
     return []
@@ -46,10 +89,15 @@ def getTrainingTrips(uid):
     #return [trip.E_Mission_Trip.trip_from_json(jsonStr) for jsonStr in get_section_db().find(queryString)].__iter__()
     return [trip.E_Mission_Trip(jsonStr) for jsonStr in get_section_db().find(queryString)].__iter__()
 
+def getTopAlternatives(uid, options = 10):
+  return []
+
+
+
 modules = {
    # Trip Module
    'trips': {
-   'get_top': getTopTrips,
+   'get_canonical': getCanonicalTrips,
    'get_all': getAllTrips,
    'get_most_recent': getRecentTrips,
    'get_trips_by_mode': getTripsThroughMode},
@@ -60,5 +108,10 @@ modules = {
     },
 
    #Pertubation Module
-   'pertubation': {}
+   'pertubation': {},
+
+   #Alternatives Module
+   # note: uses a different collection than section_db
+   'alternatives': {
+   }
  }
