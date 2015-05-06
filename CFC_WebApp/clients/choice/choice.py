bd338c6ea2099b541026dd04ed4547c65ac6a918
Lesley Chiang <lechiang2904@gmail.com>
2015-05-06 16:20:29 -0700

    fixed indent error

--- CFC_WebApp/clients/choice/choice.py
+++ CFC_WebApp/clients/choice/choice.py
@@ -55,7 +55,7 @@ def getResult(user_uuid):
                                        'client_key': Client("choice").getClientKey()}),
                           gameResult = base64.b64encode(socialgame.getResult(user_uuid)),
                           leaderboardResult = base64.b64encode(leaderboard.getResult(user_uuid)),
-                          dataResult = base64.b64encode(default.getResult(user_uuid)))
+                          dataResult = base64.b64encode(default.getResult(user_uuid)), 
                           recommendationResult = base64.b64encode(recommendation.getResult(user_uuid)))
 
   return renderedTemplate
