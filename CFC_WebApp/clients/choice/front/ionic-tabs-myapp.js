bb184769a099d2efb545c79fd04c0f6912ed585a
K. Shankari <shankari@eecs.berkeley.edu>
2015-05-07 09:53:36 -0700

    Fix remaining issues with the choice screen
    - Fix the order of the tabs
    - Fix the error in loading the recommendation tab through a completely random change which appears to work!!

--- CFC_WebApp/clients/choice/front/ionic-tabs-myapp.js
+++ CFC_WebApp/clients/choice/front/ionic-tabs-myapp.js
@@ -8,7 +8,7 @@ angular.module('e-mission-choice', ['ionic'])
   };
 
   $scope.getIndexForTab = function(label) {
-    var tabLabelMap = {'data': 0, 'game': 1, 'leaderboard': 2};
+    var tabLabelMap = {'data': 0, 'leaderboard': 1, 'game': 2, 'recommendation': 3};
     return tabLabelMap[label];
   }
 
