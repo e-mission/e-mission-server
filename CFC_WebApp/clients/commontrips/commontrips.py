e32e55e81a195e4589821eecf032af178c9b3c2d
zacatac <field.zackery@gmail.com>
2015-05-06 22:11:04 -0700

    Quick implementation of tab

--- CFC_WebApp/clients/commontrips/commontrips.py
+++ CFC_WebApp/clients/commontrips/commontrips.py
@@ -1,16 +1,46 @@
 from dao.user import User
 import json 
 
-def getCommonTrips(user):
-    """
-    Gets common trips for a given user
-    """
-    #TODO: Don't just get a random trip
+import sys, os, random
+
+# This is in here so the pygmaps associated functions can be imported 
+# from the webapp
+# sys.path.append("%s/../../CFC_WebApp/" % os.getcwd())
+# This is in here so the getCanonicalTrips function can be imported 
+# from the recommender pipeline
+sys.path.append("%s/../CFC_DataCollector/" % os.getcwd())
+from uuid import UUID 
     
+def getUserTour(user_uuid):
+    """
+    Gets a users "tour"
+    """
+    from recommender.filter_modules import getCanonicalTrips
+    canonical_trips = getCanonicalTrips(user_uuid)
+    print "Number of canonical_trips: %i" % len(canonical_trips)
+    return canonical_trips
+            
+def generate_tour_map(user_uuid):
+    """
+    Generates tour map and saves it to result_template.html 
+    """
+    import main.pygmaps_modified as pygmaps
+    from main.gmap_display import drawSection
+    from get_database import get_section_db, get_routeCluster_db
+    gmap = pygmaps.maps(37.8717, -122.2728, 14)
+    r = lambda: random.randint(0,255)
+    color = '#%02X%02X%02X' % (r(),r(),r())
+    canonical_trips = getUserTour(user_uuid)
+    section = canonical_trips[0]
+    drawSection(section, 'path', gmap, color)
+    os.remove('clients/commontrips/result_template.html')
+    gmap.draw('clients/commontrips/result_template.html')
 
 def getResult(user_uuid):
   # This is in here, as opposed to the top level as recommended by the PEP
   # because then we don't have to worry about loading bottle in the unit tests
   from bottle import template
+
+  generate_tour_map(user_uuid)
   renderedTemplate = template("clients/commontrips/result_template.html")
   return renderedTemplate
