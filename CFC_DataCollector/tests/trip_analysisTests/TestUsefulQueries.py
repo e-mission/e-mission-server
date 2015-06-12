faecfe90b326e722b18cbdc73c5265793a5431cd
K. Shankari <shankari@eecs.berkeley.edu>
2015-06-12 16:45:24 -0700

    Add a couple of trivial useful queries
    In particular, a query to get the bounds for a set of sections, and a test for it.
    Also refactored the code to import the package and then use functions from it,
    which is my new standard for avoiding namespace collisions and supporting
    easier reloads.

--- CFC_DataCollector/tests/trip_analysisTests/TestUsefulQueries.py
+++ CFC_DataCollector/tests/trip_analysisTests/TestUsefulQueries.py
@@ -1,7 +1,8 @@
 import unittest
 from get_database import get_section_db
-from trip_analysis.useful_queries import get_all_sections, get_trip_before, get_all_sections_for_user_day
+import trip_analysis.useful_queries as tauq
 from datetime import datetime
+import logging
 
 class UsefulQueriesTests(unittest.TestCase):
     def setUp(self):
@@ -21,7 +22,7 @@ class UsefulQueriesTests(unittest.TestCase):
         get_section_db().insert({"_id": "foo_1", "trip_id": "bar"})
         get_section_db().insert({"_id": "foo_2", "trip_id": "bar"})
         get_section_db().insert({"_id": "foo_3", "trip_id": "baz"})
-        self.assertEqual(len(get_all_sections("foo_1")), 2)
+        self.assertEqual(len(tauq.get_all_sections("foo_1")), 2)
 
     def testGetAllSectionsForUserDay(self):
         dt1 = datetime(2015, 1, 1, 1, 1, 1)
@@ -43,7 +44,7 @@ class UsefulQueriesTests(unittest.TestCase):
             "trip_id": "trip_3",
             "section_id": 0,
             "section_start_datetime": dt3})
-        self.assertEqual(get_trip_before("foo_3")["_id"], "foo_1")
+        self.assertEqual(tauq.get_trip_before("foo_3")["_id"], "foo_1")
 
     def testGetTripBefore(self):
         dt1 = datetime(2015, 1, 1, 1, 1, 1)
@@ -65,9 +66,43 @@ class UsefulQueriesTests(unittest.TestCase):
             "type": "move",
             "section_id": 0,
             "section_start_datetime": dt3})
-        secList = get_all_sections_for_user_day("test_user", 2015, 1, 1)
+        secList = tauq.get_all_sections_for_user_day("test_user", 2015, 1, 1)
         self.assertEqual(len(secList), 1)
         self.assertEqual(secList[0]._id, "foo_1")
 
+    def testGetBounds(self):
+        dt1 = datetime(2015, 1, 1, 1, 1, 1)
+        dt2 = datetime(2015, 1, 1, 2, 1, 1)
+        dt3 = datetime(2015, 1, 2, 3, 1, 1)
+        sectionJsonList = []
+        sectionJsonList.append({"_id": "foo_1",
+            "user_id": "test_user",
+            "type":"move",
+            "section_id": 3,
+            "section_start_datetime": dt1,
+            "section_end_datetime": dt2,
+            "section_start_point": {"coordinates": [1,2], "type": "Point"},
+            "section_end_point": {"coordinates": [3,4], "type": "Point"}})
+        sectionJsonList.append({"_id": "foo_2",
+            "user_id": "test_user",
+            "type":"place",
+            "section_start_datetime": dt2,
+            "section_end_datetime": dt3,
+            "section_start_point": {"coordinates": [5,6], "type": "Point"},
+            "section_end_point": {"coordinates": [7,8], "type": "Point"}})
+        sectionJsonList.append({"_id": "foo_3",
+            "user_id": "test_user",
+            "type": "move",
+            "section_id": 0,
+            "section_start_datetime": dt3,
+            "section_start_point": {"coordinates": [9,10], "type": "Point"},
+            "section_end_point": {"coordinates": [11,12], "type": "Point"}})
+        bounds = tauq.get_bounds(sectionJsonList)
+        self.assertEqual(bounds[0].lat, 2)
+        self.assertEqual(bounds[0].lon, 1)
+        self.assertEqual(bounds[1].lat, 12)
+        self.assertEqual(bounds[1].lon, 11)
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     unittest.main()
