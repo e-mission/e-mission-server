85a24ee292ffd047b46d16bfd4fb465c966f293e
K. Shankari <shankari@eecs.berkeley.edu>
2015-08-11 16:33:08 -0700

    Evaluate filtering by generating a list of maps with the filtering results
    Bonus fix: Refactor existing code to create a function that displays a set of
    points on a map

--- emission/analysis/plotting/leaflet_osm/our_plotter.py
+++ emission/analysis/plotting/leaflet_osm/our_plotter.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import folium
+import emission.analysis.classification.cleaning.location_smoothing as ls
+import itertools
 
 def df_to_string_list(df):
     """
@@ -21,9 +23,36 @@ def get_map_list(df, potential_splits):
             # point other than the start and the end, bail
             print "Ignoring trip from %s to %s because start = %d and end = %d" % (df.formatted_time.loc[start], df.formatted_time.loc[end], start, end)
             continue
-        currMap = folium.Map([trip.mLatitude.mean(), trip.mLongitude.mean()])
-        currMap.div_markers(trip[['mLatitude', 'mLongitude']].as_matrix().tolist(),
-            df_to_string_list(trip), marker_size=5)
-        currMap.line(trip[['mLatitude', 'mLongitude']].as_matrix().tolist())
-        mapList.append(currMap)
+        mapList.append(get_map(trip))
     return mapList
+
+def get_map(section_points):
+    currMap = folium.Map([section_points.mLatitude.mean(), section_points.mLongitude.mean()])
+    currMap.div_markers(section_points[['mLatitude', 'mLongitude']].as_matrix().tolist(),
+        df_to_string_list(section_points), marker_size=5)
+    currMap.line(section_points[['mLatitude', 'mLongitude']].as_matrix().tolist())
+    return currMap
+
+def evaluate_filtering(section_list, outlier_algos, filtering_algos):
+    """
+    TODO: Is this the best place for this? If not, what is?
+    It almost seems like we need to have a separate evaluation module that is
+    separate from the plotting and the calculation modules.
+    But then, what is the purpose of this module?
+    """
+    nCols = 2 + len(outlier_algos) * len(filtering_algos)
+    nRows = len(section_list)
+
+    map_list = []
+
+    for section in section_list:
+        curr_compare_list = []
+        section_df = ls.get_section_points(section)
+        curr_compare_list.append(get_map(section_df))
+        curr_compare_list.append(get_map(ls.filter_points(section_df, None, None)))
+        for (oa, fa) in itertools.product(outlier_algos, filtering_algos):
+            curr_compare_list.append(get_map(ls.filter_points(section_df, oa, fa)))
+        assert(len(curr_compare_list) == nCols)
+        map_list.append(curr_compare_list)
+    assert(len(map_list) == nRows)
+    return map_list
