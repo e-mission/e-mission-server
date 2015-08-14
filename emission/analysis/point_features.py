9958c2abf9a0835c8aa16e4e3fadc72144584c4b
K. Shankari <shankari@eecs.berkeley.edu>
2015-08-14 13:04:30 -0700

    Provide some more wrappers that use our point formats

--- emission/analysis/point_features.py
+++ emission/analysis/point_features.py
@@ -3,9 +3,22 @@ import math
 import logging
 import numpy as np
 import emission.core.common as ec
+import emission.analysis.section_features as sf
+
+def calDistance(point1, point2):
+    return ec.calDistance([point1.mLongitude, point1.mLatitude], [point2.mLongitude, point2.mLatitude])
+
+def calHeading(point1, point2):
+    return sf.calHeading([point1.mLongitude, point1.mLatitude],
+                    [point2.mLongitude, point2.mLatitude])
+
+def calHC(point1, point2, point3):
+    return sf.calHC([point1.mLongitude, point1.mLatitude],
+                    [point2.mLongitude, point2.mLatitude],
+                    [point3.mLongitude, point3.mLatitude])
 
 def calSpeed(point1, point2):
-    distanceDelta = ec.calDistance([point1.mLongitude, point1.mLatitude], [point2.mLongitude, point2.mLatitude])
+    distanceDelta = calDistance(point1, point2)
     timeDelta = point2.mTime - point1.mTime
     # print "Distance delta = %s and time delta = %s" % (distanceDelta, timeDelta)
     # assert(timeDelta != 0)
