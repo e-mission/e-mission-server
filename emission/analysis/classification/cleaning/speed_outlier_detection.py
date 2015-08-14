e89602fec93ca86c3952b4bf33ee7151bfe2e6b0
K. Shankari <shankari@eecs.berkeley.edu>
2015-08-14 12:59:53 -0700

    Support an option to ignore zeros while calculating thresholds
    Based on the results, the default should be to ignore

--- emission/analysis/classification/cleaning/speed_outlier_detection.py
+++ emission/analysis/classification/cleaning/speed_outlier_detection.py
@@ -9,20 +9,30 @@ logging.basicConfig(level=logging.DEBUG)
 class BoxplotOutlier(object):
     MINOR = 1.5
     MAJOR = 3
-    def __init__(self, multiplier = MAJOR):
+    def __init__(self, multiplier = MAJOR, ignore_zeros = False):
         self.multiplier = multiplier
+        self.ignore_zeros = ignore_zeros
 
     def get_threshold(self, with_speeds_df):
-        quartile_vals = with_speeds_df.quantile([0.25, 0.75]).speed
+        if self.ignore_zeros:
+            df_to_use = with_speeds_df[with_speeds_df.speed > 0]
+        else:
+            df_to_use = with_speeds_df
+        quartile_vals = df_to_use.quantile([0.25, 0.75]).speed
         logging.debug("quartile values are %s" % quartile_vals)
         iqr = quartile_vals.iloc[1] - quartile_vals.iloc[0]
         logging.debug("iqr %s" % iqr)
         return quartile_vals.iloc[1] + self.multiplier * iqr
 
 class SimpleQuartileOutlier(object):
-    def __init__(self, quantile = 0.99):
+    def __init__(self, quantile = 0.99, ignore_zeros = False):
         self.quantile = quantile
+        self.ignore_zeros = ignore_zeros
 
     def get_threshold(self, with_speeds_df):
-        return with_speeds_df.speed.quantile(self.quantile)
+        if self.ignore_zeros:
+            df_to_use = with_speeds_df[with_speeds_df.speed > 0]
+        else:
+            df_to_use = with_speeds_df
+        return df_to_use.speed.quantile(self.quantile)
 
