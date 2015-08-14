624f17bf6f1db11652cc00fd9d62b6ea9e114631
K. Shankari <shankari@eecs.berkeley.edu>
2015-08-14 12:26:27 -0700

    Add support for a new filtering algorithm (Zigzag)
    This is a new filtering algorithm that addresses some of the limitation of the
    prior "boundary" algorithm.
    
    - First point is erroneous
    - Some jumps are below the threshold
    
    This is still not perfect - it will not work on trips that have two jumps below
    the threshold, but then, what is?

--- emission/analysis/classification/cleaning/jump_smoothing.py
+++ emission/analysis/classification/cleaning/jump_smoothing.py
@@ -10,6 +10,7 @@ import math
 import pandas as pd
 import numpy as np
 import attrdict as ad
+from enum import Enum
 
 import emission.analysis.point_features as pf
 import emission.core.common as ec
@@ -38,6 +39,174 @@ class SmoothBoundary(object):
                     logging.debug("currSpeed < %s, retaining index %s " % (self.maxSpeed, i))
                     prev_pt = pt
         logging.info("Filtering complete, removed indices = %s" % np.nonzero(self.inlier_mask_))
+
+# We intentionally don't use a dataframe for the segment list, using a
+# segment class instead. The reasons are as follows:
+# 1. We don't really need any of the fancy row, column or matrix
+# operations. We are going to iterate over the list one step at a time.
+# 2. We have potentially have to resplit segments, which involves inserting
+# elements at arbitrary points in the list. With a dataframe, we would need
+# to reindex every time we did this, which we won't have to do if we use a
+# simple list. It should be trivial to re-implement this as a DataFrame if
+# we so choose.
+# This is only used by SmoothZigzag currently. I had originally made this an
+# inner class, but inner classes in python are not special so let's move it out
+# and make the indenting less complicated
+class Segment(object):
+    State = Enum("Segment_State", "UNKNOWN BAD GOOD")
+    CLUSTER_RADIUS = 100
+
+    def __init__(self, start, end, zigzag_algo):
+        self.start = start
+        self.end = end
+        self.state = Segment.State.UNKNOWN
+        self.point_count = end - start
+        self.za = zigzag_algo
+        self.segment_df = self.za.with_speeds_df[start:end]
+        self.distance = self.za.cal_distance(self)
+        self.is_cluster = (self.distance < Segment.CLUSTER_RADIUS)
+        logging.debug("For cluster %s - %s, distance = %s, is_cluster = %s" % 
+            (self.start, self.end, self.distance, self.is_cluster))
+
+    def __repr__(self):
+        return "Segment(%s, %s, %s)" % (self.start, self.end, self.distance)
+
+class SmoothZigzag(object):
+    Direction = Enum("IterationDirection", 'RIGHT LEFT')
+
+    @staticmethod
+    def end_points_distance(segment):
+        if segment.start == segment.end:
+            raise RuntimeError("This is messed up segment. Investigate further")
+        return pf.calDistance(segment.segment_df.iloc[0], segment.segment_df.iloc[-1])
+
+    @staticmethod
+    def shortest_non_cluster_segment(segment_list):
+        segment_distance_list = [[segment.distance, segment.is_cluster] for segment in segment_list]
+        segment_distance_df = pd.DataFrame(segment_distance_list, columns = ["distance", "is_cluster"])
+        retVal = segment_distance_df[segment_distance_df.is_cluster == False].distance.argmin()
+        logging.debug("shortest_non_cluster_segment = %s" % retVal)
+        return retVal
+
+    def __init__(self, maxSpeed = 100):
+        self.maxSpeed = maxSpeed
+        self.cal_distance = self.end_points_distance
+        self.find_start_segment = self.shortest_non_cluster_segment
+
+    def find_segments(self):
+        segmentation_points = self.with_speeds_df[self.with_speeds_df.speed > self.maxSpeed].index
+        segmentation_points = segmentation_points.insert(0, 0)
+        last_point = self.with_speeds_df.shape[0] - 1
+        if last_point not in segmentation_points:
+            segmentation_points = segmentation_points.insert(len(segmentation_points), last_point) 
+
+        self.segment_list = [Segment(start, end, self) for (start, end) in 
+                                zip(segmentation_points, segmentation_points[1:])]
+
+    def split_segment(self, i, curr_seg, direction):
+        import emission.analysis.classification.cleaning.location_smoothing as ls
+
+        if direction == SmoothZigzag.Direction.RIGHT:
+            recomputed_speed_df = ls.recalc_speed(curr_seg.segment_df)
+            # Find the first point that does not belong to the cluster
+            new_split_point = recomputed_speed_df[recomputed_speed_df.distance > Segment.CLUSTER_RADIUS].index[0]
+            new_seg = Segment(new_split_point, curr_seg.end, self)
+            replace_seg = Segment(curr_seg.start, new_split_point, self)
+            self.segment_list[i] = replace_seg
+            self.segment_list.insert(i+1, new_seg)
+            return replace_seg
+
+        if direction == SmoothZigzag.Direction.LEFT:
+            # Need to compute speeds and distances from the left edge
+            recomputed_speed_df = ls.recalc_speed(curr_seg.segment_df.iloc[::-1])
+            logging.debug("Recomputed_speed_df = %s", recomputed_speed_df.speed)
+            # Find the first point that does not belong to the cluster
+            new_split_point = recomputed_speed_df[recomputed_speed_df.distance > Segment.CLUSTER_RADIUS].index[0]
+            logging.debug("new split point = %s", new_split_point)
+            new_seg = Segment(curr_seg.start, new_split_point + 1, self)
+            replace_seg = Segment(new_split_point + 1, curr_seg.end, self)
+            self.segment_list[i] = replace_seg
+            self.segment_list.insert(i, new_seg)
+            return replace_seg
+
+    @staticmethod
+    def toggle(expected_state):
+        assert expected_state == Segment.State.BAD or expected_state == Segment.State.GOOD, "Unable to toggle %s " % expected_state
+        if expected_state == Segment.State.BAD:
+            return Segment.State.GOOD
+        if expected_state == Segment.State.GOOD:
+            return Segment.State.BAD
+
+    def mark_segment_states(self, start_segment_idx, direction):
+        """
+        This is the most complicated part of the algorithm.
+        """
+        if direction == SmoothZigzag.Direction.RIGHT:
+            inc = 1
+            check = lambda i: i < len(self.segment_list)
+        if direction == SmoothZigzag.Direction.LEFT:
+            inc = -1
+            check = lambda i: i >= 0
+
+        i = start_segment_idx + inc
+        expected_state = Segment.State.BAD
+
+        while(check(i)):
+            curr_seg = self.segment_list[i]
+            logging.debug("Processing segment %d: %s, expecting state %s" % (i, curr_seg, expected_state))
+            assert curr_seg.state == Segment.State.UNKNOWN, "Attempting to overwite state for segment %s, curr state is %s" % (i, curr_seg.state)
+
+            if expected_state == Segment.State.BAD and not curr_seg.is_cluster: # mixed cluster case
+                curr_seg = self.split_segment(i, curr_seg, direction)
+                # We inserted a new segment before this, so this is now moved down by one.
+                # When we are moving right, we insert after this one, so the
+                # current index is not affected
+                if (direction == SmoothZigzag.Direction.LEFT):
+                    i = i + 1
+                logging.debug("Finishing process for %s after splitting mixed cluster"% curr_seg)
+                assert curr_seg.is_cluster, "after splitting, the segment is not a cluster?!"
+                # In the mixed case, we just inserted an element, so we don't
+                # want to increment, because otherwise we will terminate too
+                # early. Note that the end conditions are embedded in the
+                # closure
+
+            i = i + inc
+            curr_seg.state = expected_state
+            expected_state = SmoothZigzag.toggle(expected_state)
+            logging.debug("At the end of the loop for direction %s, i = %s" % (direction, i))
+
+        logging.debug("Finished marking segment states for direction %s " % direction)
+
+    def filter(self, with_speeds_df):
+        self.inlier_mask_ = pd.Series([True] * with_speeds_df.shape[0])
+        self.with_speeds_df = with_speeds_df
+        self.find_segments()
+        start_segment_idx = self.find_start_segment(self.segment_list)
+        self.segment_list[start_segment_idx].state = Segment.State.GOOD
+        self.mark_segment_states(start_segment_idx, SmoothZigzag.Direction.RIGHT)
+        self.mark_segment_states(start_segment_idx, SmoothZigzag.Direction.LEFT)
+        unknown_segments = [segment for segment in self.segment_list if segment.state == Segment.State.UNKNOWN]
+        logging.debug("unknown_segments = %s" % unknown_segments)
+        assert len(unknown_segments) == 0, "Found %s unknown segments - early termination of loop?" % len(unknown_segments)
+        bad_segments = [segment for segment in self.segment_list if segment.state == Segment.State.BAD]
+        logging.debug("bad_segments = %s" % bad_segments)
+        for segment in bad_segments:
+            self.inlier_mask_[segment.start:segment.end] = False
+
+        logging.debug("after setting values, outlier_mask = %s" % np.nonzero(self.inlier_mask_ == False))
+
+        # TODO: This is not the right place for this - adds too many dependencies
+        # Should do this in the outer class in general so that we can do
+        # multiple passes of any filtering algorithm
+        import emission.analysis.classification.cleaning.speed_outlier_detection as cso
+        import emission.analysis.classification.cleaning.location_smoothing as ls
+
+        recomputed_speeds_df = ls.recalc_speed(self.with_speeds_df[self.inlier_mask_])
+        recomputed_threshold = cso.BoxplotOutlier(ignore_zeros = True).get_threshold(recomputed_speeds_df)
+        # assert recomputed_speeds_df[recomputed_speeds_df.speed > recomputed_threshold].shape[0] == 0, "After first round, still have outliers %s" % recomputed_speeds_df[recomputed_speeds_df.speed > recomputed_threshold] 
+        if recomputed_speeds_df[recomputed_speeds_df.speed > recomputed_threshold].shape[0] != 0:
+            logging.warn("After first round, still have outliers %s" % recomputed_speeds_df[recomputed_speeds_df.speed > recomputed_threshold])
+
 
 class SmoothPosdap(object):
     def __init__(self, maxSpeed = 100):
@@ -175,5 +344,5 @@ class SmoothPiecewiseRansac(object):
             logging.debug("Retain mask is of size %d" % len(retain_mask))
             ransac_mask[area.index] = retain_mask
         logging.debug("with speed df shape is %s, ransac_mask size = %s" % (with_speeds_df.shape, len(ransac_mask)))
-        logging.debug("filtering done, ransac deleted points = %s" % np.nonzero(ransac_mask.as_matrix))
+        logging.debug("filtering done, ransac deleted points = %s" % np.nonzero(ransac_mask == False))
         self.inlier_mask_ = ransac_mask.as_matrix().tolist()
