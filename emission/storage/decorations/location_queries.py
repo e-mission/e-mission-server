5e625420f6355e052f4af6118aa30f513e652ddb
K. Shankari <shankari@eecs.berkeley.edu>
2015-08-22 21:54:19 -0700

    Refactor the location query class to work more with sections
    The `get_plottable_df` method now creates a section and then calls
    `get_points_for_section with the section`. This makes it easier to invoke
    `get_points_for_section` with a real section object.
    
    It also defines a enum class for the activities to make our code cleaner and a
    function to return all the activities for a section.

--- emission/storage/decorations/location_queries.py
+++ emission/storage/decorations/location_queries.py
@@ -1,10 +1,12 @@
 import logging
-import emission.core.get_database as edb
 import datetime as pydt
 import pandas as pd
 import time
 import pymongo
 import attrdict as ad
+from enum import Enum
+
+import emission.core.get_database as edb
 
 def get_uuid_list():
     return edb.get_usercache_db().distinct('user_id')
@@ -12,11 +14,67 @@ def get_uuid_list():
 def get_plottable_df(user_id, loc_filter, start_dt, end_dt):
     tempSection = ad.AttrDict()
     tempSection.user_id = user_id
-    tempSection.filter = loc_filter
+    tempSection.loc_filter = loc_filter
     if (start_dt is not None and end_dt is not None):
-        section.startTs = time.mktime(start_dt.timetuple()) * 1000
-        section.endTs = time.mktime(end_dt.timetuple()) * 1000
-    return get_points_for_section(section)
+        tempSection.start_ts = time.mktime(start_dt.timetuple()) * 1000
+        tempSection.end_ts = time.mktime(end_dt.timetuple()) * 1000
+    return get_points_for_section(tempSection)
+
+from_micros = lambda x: pydt.datetime.fromtimestamp(x/1000)
+
+class Activities(Enum):
+    IN_VEHICLE = 0
+    ON_BICYCLE = 1
+    ON_FOOT = 2
+    STILL = 3
+    UNKNOWN = 4
+    TILTING = 5 
+    WALKING = 7
+    RUNNING = 8
+
+to_activity_enum = lambda i: Activities(i)
+
+def get_activities_for_section(section):
+    query = {"user_id": section.user_id,
+             "metadata.filter": section.loc_filter,
+             "metadata.key": "background/activity"}
+
+    start_ts = section.start_ts
+    end_ts = section.end_ts
+    query.update({'$and': [{'metadata.write_ts': {'$gt': start_ts}},
+                           {'metadata.write_ts': {'$lt': end_ts}}]})
+
+    full_entries = list(edb.get_usercache_db().find(query))
+    merged_entries = [dict(entry["metadata"].items() + entry["data"].items()) for entry in full_entries]
+    entries_df = pd.DataFrame(merged_entries)
+    entries_df['formatted_time'] = entries_df.write_ts.apply(from_micros)
+    entries_df['activity'] = entries_df.agb.apply(to_activity_enum)
+    return entries_df
+
+def get_transitions_df(user_id, loc_filter, start_dt, end_dt):
+    query = {"user_id": user_id,
+             "metadata.filter": loc_filter,
+             "metadata.key": "statemachine/transition"}
+
+    if (start_dt is not None and end_dt is not None):
+        start_ts = time.mktime(start_dt.timetuple()) * 1000
+        end_ts = time.mktime(end_dt.timetuple()) * 1000
+        query.update({'$and': [{'metadata.write_ts': {'$gt': start_ts}},
+                               {'metadata.write_ts': {'$lt': end_ts}}]})
+
+    full_entries = list(edb.get_usercache_db().find(query))
+    merged_entries = [dict(entry["metadata"].items() + entry["data"].items()) for entry in full_entries]
+    entries_df = pd.DataFrame(merged_entries)
+    entries_df['formatted_time'] = entries_df.write_ts.apply(from_micros)
+    return entries_df
+
+def get_points_for_transitions(user_id, transitions_df):
+    get_section = lambda(transition): ad.AttrDict({'user_id': user_id,
+                                    'loc_filter': transition["filter"],
+                                    'start_ts': transition["write_ts"] - 10 * 60 * 1000,
+                                    'end_ts': transition["write_ts"] + 10})
+    get_last_point = lambda(transition): get_points_for_section(get_section(transition)).iloc[-1] 
+    return transitions_df.apply(get_last_point, axis=1)
 
 def get_points_for_section(section):
     query = {"user_id": section.user_id,
@@ -30,10 +88,10 @@ def get_points_for_section(section):
         logging.debug("Start and end times not defined, no time query")
 
     print "final query = %s " % query
-    full_entries = list(edb.get_usercache_db().find(query).sort("data.mTime", pymongo.ASCENDING))
+    # full_entries = list(edb.get_usercache_db().find(query).sort("data.mTime", pymongo.ASCENDING))
+    full_entries = list(edb.get_usercache_db().find(query))
     merged_entries = [dict(entry["metadata"].items() + entry["data"].items()) for entry in full_entries]
     entries_df = pd.DataFrame(merged_entries)
-    from_micros = lambda x: pydt.datetime.fromtimestamp(x/1000)
     entries_df['formatted_time'] = entries_df.mTime.apply(from_micros)
     return entries_df
 
