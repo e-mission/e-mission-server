6e3cf9d6f08d0ddd27be5453462e27218e7afd88
Josh Zarrabi <joshzarrabi@berkeley.edu>
2015-08-23 19:31:14 -0700

    Tour Model Code added, also added updated trip generation and abstracted out geocoding

---
+++ emission/analysis/modelling/tour_model/tour_model_matrix.py
@@ -0,0 +1,289 @@
+# Our imports
+import emission.simulation.markov_model_counter as esmmc
+from emission.core.our_geocoder import Geocoder
+
+# Standard imports
+import numpy as np
+import math, datetime, folium, heapq
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Constants
+DAYS_IN_WEEK = 7
+HOURS_IN_DAY = 24
+MODES_TO_NUMBERS = {"walking" : 0, "car" : 1, "train" : 2, "bart" : 3, "bike" : 4}
+NUM_MODES = len(MODES_TO_NUMBERS)
+
+class Commute(object):
+    
+    """ An edge in the graph """
+
+    def __init__(self, starting_point, ending_point):
+        self.probabilities = np.zeros((DAYS_IN_WEEK, HOURS_IN_DAY))
+        self.starting_point = starting_point
+        self.ending_point = ending_point
+        self.trips = [ ]
+
+    def increment_prob(self, hour, day):
+        self.probabilities[day, hour] += 1
+
+    def set_predicted_mode(self, pred_mode):
+        self.predicted_mode = pred_mode
+
+    def get_predicted_mode(self):
+        return self.predicted_mode
+
+    def set_confirmed_mode(self, mode):
+        self.confirmed_mode = mode
+
+    def get_confirmed_mode(self):
+        return self.confirmed_mode
+
+    @classmethod
+    def make_lookup_key(cls, starting_point, ending_point):
+        return "%s->%s" % (starting_point, ending_point)
+
+    def __repr__(self):
+        return self.make_lookup_key(self.starting_point, self.ending_point)
+
+    def add_trip(self, trip):
+        self.trips.append(trip)
+
+    def get_rough_time_duration(self):
+        ## Based on a 40 mph guess
+        dist = self.starting_point.rep_coords.distance(self.ending_point.rep_coords)
+        miles = dist * 0.000621371192
+        time = miles/float(40)
+        if time < 1.0/2.0:
+            time = 1.0/2.0   ## Pushes the random walk forward
+        return datetime.timedelta(hours=time)
+
+    def get_distance(self):
+        return self.starting_point.rep_coords.distance(self.ending_point.rep_coords)
+
+    def weight(self):
+        return len(self.trips)
+
+    def __eq__(self, other):
+        return (self.starting_point == other.starting_point) and (self.ending_point == other.ending_point)
+
+class Location(object):
+
+    """ A node in the graph, lots of cool functions, will add more for units """
+
+    def __init__(self, name, tour_model):
+        self.tm = tour_model
+        self.name = name
+        self.successors = set( )
+        self.edges = set( )
+        self.rep_coords = None ## coordinates that represent the location
+        self.address = None  ## Address that corresponds to rep_coords  
+
+    def set_rep_coords(self, rep_coords):
+        self.rep_coords = rep_coords
+
+    def get_address(self):
+        if self.rep_coords is None:
+            raise Exception("You need to input representative coordinates first!")
+        if self.address is None:
+            geo = Geocoder()
+            self.address = geo.reverse_geocode(self.rep_coords.get_lat(), self.rep_coords.get_lon())
+        return self.address
+
+    def increment_successor(self, suc, hour, day):
+        the_edge = self.tm.get_edge(self, suc)
+        the_edge.increment_prob(hour, day)
+        suc_key = Location.make_lookup_key(suc.name)
+        edge_key = Commute.make_lookup_key(self, suc)
+        self.successors.add(self.tm.locs[suc_key])
+        self.edges.add(self.tm.edges[edge_key])
+
+    def get_successor(self):
+        temp_counter = esmmc.Counter( )
+        time = self.tm.time
+        day = time.weekday()
+        for suc in self.successors:
+            suc_obj = self.tm.get_location(suc)
+            edge = self.tm.get_edge(self, suc_obj)
+            for temp_hour in xrange(time.hour, HOURS_IN_DAY):
+                counter_key = (suc_obj, temp_hour, edge.get_rough_time_duration())
+                temp_counter[counter_key] = edge.probabilities[day, temp_hour]
+        return esmmc.sampleFromCounter(temp_counter)
+
+    def hasSuccessor(self):
+        day = self.tm.time.weekday()
+        time = self.tm.time
+        for suc in self.successors:
+            suc_obj = self.tm.get_location(suc)
+            edge = self.tm.get_edge(self, suc_obj)
+            for temp_hour in xrange(time.hour, HOURS_IN_DAY):
+                if edge.probabilities[day, temp_hour] > 0:
+                    return True
+        return False
+
+    def get_in_degree(self):
+        count = 0
+        for loc in self.tm.locs.values():
+            if loc in self.successors:
+                print "count inceasing"
+                count += 1
+        return count
+
+    @classmethod
+    def make_lookup_key(cls, name):
+        return "At Location %s" % name 
+
+    def __eq__(self, other):
+        if type(other) != Location:
+            return False
+        return (self.name == other.name) or (self.rep_coords.distance(other.rep_coords) < 300)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __repr__(self):
+        return str(self.name)
+
+    def __str__(self):
+        return str(self.name)
+
+class TourModel(object):
+
+    """ 
+    A class that represents a canconical week of travel for a user of e-mission. 
+    ie the graph that store the location and commute classes 
+    
+    """
+
+    def __init__(self, user, day, time):
+        self.user = user
+        self.edges = { }
+        self.locs = { }
+        self.min_of_each_day = [0]*DAYS_IN_WEEK  ## Stores (location, hour) pairs as a way to find the day's starting point
+        self.time = time
+
+    def get_top_trips(self, n):
+        # sort edges by weight 
+        # return n most common trips
+        edges_list = list(self.edges.values())
+        # edges_list.sort(reverse=True, key=lambda v: v.weight())
+        # return edges_list[:n]
+        return heapq.nlargest(n, edges_list, key=lambda v: v.weight())
+
+    def define_locations(self):
+        for loc in self.locs.itervalues():
+            print "%s : %s" % (loc.name, loc.get_address())
+
+    def get_prob_of_place_x_at_time_y_on_date_z(x, y, z):
+        loc_key = Location.make_lookup_key(x)
+        loc = self.get_location(loc_key)
+
+    def add_location(self, location, coords):
+        location.set_rep_coords(coords)
+        key = Location.make_lookup_key(location.name)
+        self.locs[key] = location
+
+    def add_start_hour(self, loc, time):
+        day = time.weekday()
+        if self.min_of_each_day[day] == 0:
+            self.min_of_each_day[day] = (loc, time)
+        else:
+            if time < self.min_of_each_day[day][1]:
+                self.min_of_each_day[day] = (loc, time)
+
+    def get_tour_model_for_day(self, day):
+        tour_model = [ ]
+        if self.min_of_each_day[day] == 0:
+            return "No data for this day"
+        curr_node = self.min_of_each_day[day][0]
+        self.time = self.min_of_each_day[day][1]
+        print "hour = %s | day = %s | place = %s" % (self.time.hour, self.time.weekday(), curr_node.name)
+        tour_model.append(curr_node)
+        while curr_node.hasSuccessor():
+            info = curr_node.get_successor()
+            curr_node = info[0]
+            self.time = datetime.datetime(self.time.year, self.time.month, self.time.day, hour=info[1], minute=self.time.minute) + info[2]
+            if self.time.weekday() != day:
+                break
+            print "hour = %s | day = %s | place = %s" % (self.time.hour, self.time.weekday(), info[0].name)
+            if curr_node != tour_model[-1]:
+                tour_model.append(curr_node)
+        return tour_model
+
+    def get_location(self, location):
+        if type(location) == str:
+            return self.locs[location]
+        key = Location.make_lookup_key(location.name)
+        return self.locs[key]
+ 
+    def build_tour_model(self):
+        tour_model = [ ]
+        for day in xrange(DAYS_IN_WEEK):
+            tour_model.append(self.get_tour_model_for_day(day))
+        return tour_model 
+
+    def get_edge(self, starting_point, ending_point):
+        key = Commute.make_lookup_key(starting_point, ending_point)
+        return self.edges[key]
+
+    def add_edge(self, commute):
+        key = Commute.make_lookup_key(commute.starting_point, commute.ending_point)
+        self.edges[key] = commute
+
+    def see_graph(self):
+        pos = {}
+        edge_colors = []
+        node_sizes = []
+        plt.clf()
+        G = nx.MultiDiGraph()
+        labels = { }
+        for v in self.locs.values():
+            G.add_node(v)
+            pos[v] = (v.rep_coords.lon, v.rep_coords.lat)
+            labels[v] = v.get_address()
+        for e in self.edges.values():
+            start = e.starting_point
+            end = e.ending_point
+            G.add_edge(start,end)
+        for v in G.nodes():
+            n = 0
+            for e in G.in_edges(v):
+                edge = self.get_edge(e[0], e[1])
+                n += edge.weight()
+            node_sizes.append(max(n,1)*30)
+        G = nx.MultiGraph(G)
+        nx.draw_networkx(G, pos, node_color='#00FF80', with_labels=True, node_size=node_sizes, width=3.5)
+        plt.show()
+
+    def plot_leaflet(self):
+        arbitrary_center = self.locs.values()[0].rep_coords
+        map_obj = folium.Map(location=coord_list(arbitrary_center), tiles="OpenStreetMap")
+        for location in self.locs.itervalues():
+            coords = location.rep_coords
+            map_obj.circle_marker(location=coord_list(coords), radius=300, fill_color="#fb9f9f")
+        map_obj.line(locations=create_lines_list(self.locs.values()))
+        map_obj.create_map(path="%s.html" % self.user)
+
+def plot_random_walk(walk_for_one_day, user):
+    # Still have to do some hard coding if you want to see the moving point
+    arbitrary_center = walk_for_one_day[0].rep_coords
+    map_obj = folium.Map(location=coord_list(arbitrary_center), tiles="OpenStreetMap")
+    for location in walk_for_one_day:
+        map_obj.circle_marker(location=coord_list(location.rep_coords), radius=500, fill_color="#fb9f9f")
+    map_obj.line(locations=create_lines_list(walk_for_one_day))
+    map_obj.create_map(path="random_walk_for_%s" % user)
+
+
+## These are utility functions
+
+def coord_list(coords):
+    return [coords.get_lat(), coords.get_lon()]
+
+def create_lines_list(walk_for_one_day):
+    lst = [ ]
+    for location in walk_for_one_day:
+        lst.append(coord_list(location.rep_coords))
+    return lst
+
+def is_weekday(day):
+    return not (day == 5 or day == 6)
--- emission/simulation/trip_gen.py
+++ emission/simulation/trip_gen.py
@@ -1,14 +1,11 @@
 # Standard imports
-import json
-import datetime
-from pygeocoder import Geocoder
-import random, math
-import uuid
+import random, math, json, datetime
 
 # Our imports
 from emission.net.ext_service.otp.otp import OTP, PathNotFoundException
 from emission.core.wrapper.trip import Coordinate 
-
+import emission.simulation.markov_model_counter as esmmc
+from emission.core.our_geocoder import Geocoder
 
 class Address:
 
@@ -20,7 +17,7 @@ class Address:
         self.cord = None
     
     def __str__(self):
-        return self.text            
+        return self.text
 
 class Creator: 
 
@@ -31,26 +28,30 @@ class Creator:
         self.num_trips = None
         self.radius = None
         self.amount_missed = 0
-        self.starting_addresses = [ ]
-        self.ending_addresses = [ ]
-        self.labels = [ ]
+        self.starting_counter = esmmc.Counter( )
+        self.ending_counter = esmmc.Counter( )
+        self.mode_counter = esmmc.Counter( )
+        self.prog_bar = ""
 
-    def get_starting_ending_points(self):
+    def set_up(self):
         city_file = open("emission/simulation/input.json", "r") ## User (Naomi) specifies locations and radius they want
         jsn = json.load(city_file)
         self.num_trips = jsn["number of trips"]
         self.radius = float(jsn["radius"])
-        for address in jsn["starting centroids"]:            
-            self.starting_points.extend(generate_random_locations_in_radius(Address(address), self.radius, self.num_trips/len(jsn["starting centroids"])))
-            for i in range(self.num_trips/len(jsn["starting centroids"])):
+        for place, weight in jsn['starting centroids'].iteritems():
+            self.starting_counter[Address(place)] = weight
+        for place, weight in jsn['ending centroids'].iteritems():
+            self.ending_counter[Address(place)] = weight
+        for mode, weight in jsn['modes'].iteritems():
+            self.mode_counter[mode] = weight
+        city_file.close()
 
-                self.starting_addresses.append(address)
-        for address in jsn["ending centroids"]:
-            self.ending_points.extend(generate_random_locations_in_radius(Address(address), self.radius, self.num_trips/len(jsn["starting centroids"])))
-            for i in range(self.num_trips/len(jsn["starting centroids"])):
-                self.ending_addresses.append(address)
-
-
+    def get_starting_ending_points(self):
+        for _ in xrange(self.num_trips):
+            start_addr = esmmc.sampleFromCounter(self.starting_counter)
+            end_addr = esmmc.sampleFromCounter(self.ending_counter)
+            self.starting_points.append(get_one_random_point_in_radius(start_addr, self.radius))
+            self.ending_points.append(get_one_random_point_in_radius(end_addr, self.radius))
 
     def make_a_to_b(self):
         for _ in xrange(self.num_trips):  ## Based on very rough estimate of how many of these end up in the ocean
@@ -58,13 +59,10 @@ class Creator:
             end_index = random.randint(0, len(self.ending_points) - 1)
             starting_point = self.starting_points[start_index]
             ending_point = self.ending_points[end_index]
-            self.labels.append(str(self.starting_addresses[start_index]) + " to " + str(self.ending_addresses[end_index]))
             to_add = ( starting_point, ending_point )
-            print to_add
             self.a_to_b.append(to_add)
 
     def get_trips_from_a_to_b(self):
-        mode_tuple = ("CAR", "WALK", "BICYCLE", "TRANSIT")
         curr_time = datetime.datetime.now()
         curr_month = curr_time.month
         curr_year = curr_time.year
@@ -73,28 +71,17 @@ class Creator:
             curr_day = random.randint(1, 28)
             curr_hour = random.randint(0, 23)
             t = self.a_to_b[i]
-            print t
-            mode = mode_tuple[random.randint(0, len(mode_tuple) - 1)] ## Unsophisticated mode choice, Alexi would throw up
+            mode = esmmc.sampleFromCounter(self.mode_counter) ## Unsophisticated mode choice, Alexi would throw up
             try:
-                id = str(random.random())
-                rand_user_id = str(uuid.uuid4())
-                rand_trip_id = rand_user_id + curr_time.strftime("%Y%m%dT%H%M%S-%W00")
+                self.prog_bar += "."
+                print self.prog_bar
+                rand_trip_id = random.random()
+                rand_user_id = random.random()
                 otp_trip = OTP(t[0], t[1], mode, write_day(curr_month, curr_day, curr_year), write_time(curr_hour, curr_minute), True)
-                alt_trip = otp_trip.turn_into_trip(self.labels[i] + id, rand_user_id, rand_trip_id, True)   ## ids
+                alt_trip = otp_trip.turn_into_trip("%f%f" % (rand_user_id, rand_trip_id), rand_user_id, rand_trip_id, True)   ## ids
                 alt_trip.save_to_db()
             except PathNotFoundException:
-                print "In the sea, skipping"
                 self.amount_missed += 1
-
-# def geocode_address(address):
-#     print address.text
-#     if address.cord is None:
-#         g = Geocode()
-#         address.cord = g.get_coords(address)
-#         results = address.cord
-#     else:
-#         results = address.cord
-#     return results
 
 def geocode_address(address):
     if address.cord is None:
@@ -103,22 +90,19 @@ def geocode_address(address):
         address.cord = results
     else:
         results = address.cord
-    return Coordinate(results[0].coordinates[0], results[0].coordinates[1])
+    return results
 
 def generate_random_locations_in_radius(address, radius, num_points):
     # Input the desired radius in kilometers
-    print "num points is %s" % num_points
     locations = [ ]
     for _ in xrange(num_points):
         loc = get_one_random_point_in_radius(address, radius)
         locations.append(loc)
     return locations
 
-
 def get_one_random_point_in_radius(address, radius):
     # From https://gis.stackexchange.com/questions/25877/how-to-generate-random-locations-nearby-my-location
     crd = geocode_address(address)
-    print crd
     radius_in_degrees = kilometers_to_degrees(radius)
     x_0 = crd.get_lon()
     y_0 = crd.get_lat()
@@ -128,15 +112,8 @@ def get_one_random_point_in_radius(address, radius):
     t = 2 * math.pi * v
     x = w * math.cos(t)
     y = w * math.sin(t)
-    print "type of y is %s" % type(y_0)
-    print "x = %s" % x
-    x = float(x) / float(math.cos(y_0))   # To account for Earth something 
+    x = float(x) / float(math.cos(y_0)) # To account for Earth curvature stuff
     to_return = Coordinate(y + y_0, x + x_0)
-    print to_return
-    if abs(to_return.get_lat()) < 30 or abs(to_return.get_lon() < 30):
-        print to_return.get_lon()
-        print to_return.get_lat()
-
     return to_return
 
 def kilometers_to_degrees(km):
@@ -149,12 +126,14 @@ def write_day(month, day, year):
 def write_time(hour, minute):
     return "%s:%s" % (hour, minute) 
 
-
 def create_fake_trips():
-    ### Remember to fill in input.json!!!! 
-    ### (This may not be the best way to do it. but for now it should be fine)
+    ### This is the main function, its the only thing you need to run
     my_creator = Creator()
+    my_creator.set_up()
     my_creator.get_starting_ending_points()
     my_creator.make_a_to_b()
     my_creator.get_trips_from_a_to_b()
     return my_creator
+
+if __name__ == "__main__":
+    create_fake_trips()
