cee537f3ae67453c5e271d6c90542dc2bae04ea8
K. Shankari <shankari@eecs.berkeley.edu>
2015-08-26 13:17:21 -0700

    Add a compatibility layer for old style document data
    Add a simple compatibility layer with a set of functions that will convert old
    style document-based data into the new style points and decoration style data.

---
+++ emission/storage/compat/convert_moves_style_data.py
@@ -0,0 +1,24 @@
+import json
+import attrdict as ad
+from dateutil import parser
+import logging
+import time
+import pandas as pd
+
+to_ts = lambda(dt): time.mktime(dt.timetuple())
+
+def convert_track_point(tp):
+    point = ad.AttrDict()
+    tpDict = ad.AttrDict(tp)
+    point.mTime = to_ts(parser.parse(tpDict.time))
+    point.mLatitude = tpDict.track_location.coordinates[1]
+    point.mLongitude = tpDict.track_location.coordinates[0]
+    point.mAccuracy = 0
+    return point
+
+def convert_track_point_array(tp_array):
+    return [convert_track_point(tp) for tp in tp_array]
+
+def convert_track_point_array_to_df(tp_array):
+    point_array = convert_track_point_array(tp_array)
+    return pd.DataFrame(point_array)
