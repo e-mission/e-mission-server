186ffb6f7a9f5c4871a8ec245f4f85d21b19874e
K. Shankari <shankari@eecs.berkeley.edu>
2015-08-26 13:09:53 -0700

    Change the comment in runAllTests
    Do we run tests to catch bugs (in which case a failure is a positive) or to
    ensure correctness in which case a clean run is a positive)? Switch to the latter...

--- runAllTests.sh
+++ runAllTests.sh
@@ -1,4 +1,4 @@
 set -e
-# Clean out the .pyc files so that we don't get false negatives when we refactor code
+# Clean out the .pyc files so that we don't get false positives when we refactor code
 find . -name \*.pyc | xargs rm
 PYTHONPATH=. python -m unittest discover -s emission/tests -p Test*;
