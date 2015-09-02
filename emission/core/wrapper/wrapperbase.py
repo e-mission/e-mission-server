ee5ee70fc72cd10b0053ef73c2ee6604cc9047c1
K. Shankari <shankari@eecs.berkeley.edu>
2015-09-02 05:22:34 -0700

    Fix to the naming convention for wrapper classes
    Originally, we had assumed that the JSON would have CamelCase class name, i.e.
    
    {'Metadata": {...}}
    
    But this does not match our standard for the data format.
    We could change the JSON to match this.
    Instead, I am changing the code so that the module name is the key from JSON
    while the class name is the module name with the first letter capitalized.
    
    Also add some (commented out) logging to help debug this.

--- emission/core/wrapper/wrapperbase.py
+++ emission/core/wrapper/wrapperbase.py
@@ -19,20 +19,24 @@ class WrapperBase(ad.AttrDict):
   """
   """
     All subclasses should define:
-    - name: the name to use instead of AttrDict in the string representation
-    - properties: a map of valid properties, with a value indicating whether
+    - props: a map of valid properties, with a value indicating whether
       they are read-only or not
+    - _populateDependencies: a method to pre-populate fields based on the
+      current field. This can be empty, and implemented by pass
   """
   Access = enum.Enum("PropertyAccess", "RO RW")
 
 
   def __init__(self, *args, **kwargs):
     super(WrapperBase, self).__init__(*args, **kwargs)
+    self._populateDependencies()
 
   def __dir__(self):
     return dir(super) + self.props.keys()
 
   def __getattr__(self, key):
+    # logging.debug("__getattr__ called for key = %s" % key)
+
     if key in self.props:
         # This code is copied from the base Attr code.
         # This allows us to pass the key into _build as well instead of only the value
@@ -42,6 +46,7 @@ class WrapperBase(ad.AttrDict):
                     cls=self.__class__.__name__, name=key
                 )
             )
+        # logging.debug("Returning self._build")
         return self._build(key, self[key])
     else:
         raise AttributeError("property %s is not defined for %s" % (key, self.__class__.__name__))
@@ -81,13 +86,15 @@ class WrapperBase(ad.AttrDict):
     # Convention is to name the module with the wrapper name in lower case, and
     # then have a class in the module with the name.
     # e.g. The Location class will be in a module named location.
-    wrapperModule = importlib.import_module("emission.core.wrapper.%s" % (wrapper_name.lower()))
-    return getattr(wrapperModule, wrapper_name)
+    wrapperModule = importlib.import_module("emission.core.wrapper.%s" % wrapper_name)
+    wrapperClassName = wrapper_name[0].upper() + wrapper_name[1:]
+    return getattr(wrapperModule, wrapperClassName)
 
   def _build(self, key, obj):
-    # print("_build called with %s, %s, %s" % (self, key, obj))
+    # logging.debug("_build called with %s, %s, %s" % (self, key, obj))
     if isinstance(obj, coll.Mapping):
         key_class = self._get_class(key)
+        # logging.debug("key_class = %s" % key_class)
         return key_class._constructor(obj, self._configuration())
     else:
         return super(WrapperBase, self)._build(obj)
