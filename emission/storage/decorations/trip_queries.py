89192071eca8e5737729fd4601ec6dce6952a401
K. Shankari <shankari@eecs.berkeley.edu>
2015-09-17 15:53:18 -0700

    Add support for reading the trips and places that match a particular time range
    This code is tested by the TestTripSegmentation test.

--- emission/storage/decorations/trip_queries.py
+++ emission/storage/decorations/trip_queries.py
@@ -9,3 +9,15 @@ def create_new_trip(user_id):
 
 def save_trip(trip):
     edb.get_trip_new_db().save(trip)
+
+def _get_ts_query(tq):
+    time_key = tq.timeType
+    ret_query = {time_key : {"$lt": tq.endTs}}
+    if (tq.startTs is not None):
+        ret_query[time_key].update({"$gte": tq.startTs})
+    return ret_query
+
+def get_trips(user_id, time_query):
+    trip_doc_cursor = edb.get_trip_new_db().find(_get_ts_query(time_query))
+    # TODO: Fix "TripIterator" and return it instead of this list
+    return [ecwt.Trip(doc) for doc in trip_doc_cursor]
