440a25076eebb2e4c46c32ab2dde297d4185f2c1
K. Shankari <shankari@eecs.berkeley.edu>
2015-09-21 10:52:58 -0700

    Fix small issue with the android activity formatting
    We were trying to set the key to be the new "background/motion_activity", but
    recall that chaining of sets does not work! So needed to switch to "dict"
    format instead.

--- emission/net/usercache/formatters/android/activity.py
+++ emission/net/usercache/formatters/android/activity.py
@@ -2,5 +2,5 @@ import logging
 import emission.net.usercache.formatters.android.motion_activity as fam
 
 def format(entry):
-    entry.metadata.key = "background/motion_activity"
+    entry["metadata"]["key"] = "background/motion_activity"
     return fam.format(entry)
