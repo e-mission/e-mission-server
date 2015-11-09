9885a8669659cb069e122b26e36e49db2a3f72cb
Josh Zarrabi <joshzarrabi@berkeley.edu>
2015-11-08 17:10:33 -0800

    fixed parse data

--- emission/ce186/parse_data.py
+++ emission/ce186/parse_data.py
@@ -9,15 +9,19 @@ def make_average_data_time_delta(time_period, our_file, given_delta, start_time)
 	averages = [ ]
 	place = 1
 	total_now = 0
-	num_points_per_average = time_period // given_delta
+	num_points_per_average = int(time_period // given_delta)
 	for line in data:
-		num = int(line.read())
+		print line
+		num = int(line)
 		total_now += num
+		print "place is %s" % place
+		print "num_points_per_average is %s" % num_points_per_average
 		if place % num_points_per_average == 0:
+			print "here"
 			averages.append(total_now/num_points_per_average)
 			total_now = 0
 		place += 1
 	return averages
 
 
-make_average_data_time_delta(100, "emission/data.csv", .5, 0)
+print make_average_data_time_delta(5, "emission/ce186/data.csv", .5, 0)
