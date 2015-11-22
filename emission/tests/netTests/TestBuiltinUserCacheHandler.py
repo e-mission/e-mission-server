d6a991f2f517266666bbc34d15a9add433fcf955
K. Shankari <shankari@eecs.berkeley.edu>
2015-11-22 09:10:12 -0800

    Deal with android and ios differently in the usercache code
    android currently has millisecond timestamps
    ios has second timestamps
    
    we store second timestamps internally.
    so for android, we need to multiply the returned query by 1000, but for ios we
    don't.
    
    So we pass in the platform and deal with it accordingly.
    We also update the test case accordingly.
    
    Note that this is part of the hack list that can be removed after all the data
    is converted and the clients have been modified accordingly.

--- emission/tests/netTests/TestBuiltinUserCacheHandler.py
+++ emission/tests/netTests/TestBuiltinUserCacheHandler.py
@@ -48,12 +48,12 @@ class TestBuiltinUserCacheHandler(unittest.TestCase):
         self.curr_ts = int(time.time())
         for offset in range(self.curr_ts - 5 * 60, self.curr_ts, 30):
             for entry in self.entry_list:
-                entry["metadata"]["write_ts"] = self.curr_ts - offset
+                entry["metadata"]["write_ts"] = offset * 1000
             mauc.sync_phone_to_server(self.testUserUUID1, self.entry_list)
 
         for offset in range(self.curr_ts - 7 * 60 + 1, self.curr_ts - 2 * 60 + 1, 30):
             for entry in self.entry_list:
-                entry["metadata"]["write_ts"] = self.curr_ts - offset
+                entry["metadata"]["write_ts"] = offset * 1000
             mauc.sync_phone_to_server(self.testUserUUID2, self.entry_list)
             
         self.ios_activity_entry = json.load(open("emission/tests/data/netTests/ios.activity.txt"))
@@ -67,7 +67,7 @@ class TestBuiltinUserCacheHandler(unittest.TestCase):
 
         for offset in range(self.curr_ts - 5 * 60, self.curr_ts, 30):
             for entry in self.ios_entry_list:
-                entry["metadata"]["write_ts"] = self.curr_ts - offset
+                entry["metadata"]["write_ts"] = offset
             mauc.sync_phone_to_server(self.testUserUUIDios, self.ios_entry_list)
 
 
@@ -106,6 +106,82 @@ class TestBuiltinUserCacheHandler(unittest.TestCase):
         # But all existing entries still in usercache for the second user
         self.assertEqual(len(self.uc2.getMessage()), 30)
         self.assertEqual(len(list(self.ts2.find_entries())), 0)
+    
+    # The first query for every platform is likely to work 
+    # because startTs = None and endTs, at least for iOS, is way out there
+    # But on the next call, if we are multiplying by 1000, it won't work any more.
+    # Let's add a new test for this
+    def testTwoLongTermCalls(self):
+        # First all the entries are in the usercache
+        self.assertEqual(len(self.uc1.getMessage()), 30)
+        self.assertEqual(len(list(self.ts1.find_entries())), 0)
+        
+        self.assertEqual(len(self.ucios.getMessage()), 30)
+        self.assertEqual(len(list(self.tsios.find_entries())), 0)
+
+        # Then we move entries for user1 into longterm
+        enuah.UserCacheHandler.getUserCacheHandler(self.testUserUUID1).moveToLongTerm()
+
+        # So we end up with all user1 entries in longterm
+        self.assertEqual(len(self.uc1.getMessage()), 0)
+        self.assertEqual(len(list(self.ts1.find_entries())), 30)
+        
+        # Then, we move entries for the ios user into longterm
+        enuah.UserCacheHandler.getUserCacheHandler(self.testUserUUIDios).moveToLongTerm()
+        
+        self.assertEqual(len(self.ucios.getMessage()), 0)
+        self.assertEqual(len(list(self.tsios.find_entries())), 30)
+        
+        # 30 entries from android + 30 entries from ios = 60
+        self.assertEqual(edb.get_timeseries_db().find().count(), 60)
+        self.assertEqual(edb.get_timeseries_error_db().find().count(), 0)
+
+        logging.debug("old curr_ts = %d" % self.curr_ts)
+        time.sleep(20)
+        self.curr_ts = int(time.time())
+        logging.debug("new curr_ts = %d" % self.curr_ts)
+        
+        # Now, re-insert entries for android user1 and ios user1
+        for offset in range(self.curr_ts - 10, self.curr_ts):
+            for entry in self.entry_list:
+                entry["metadata"]["write_ts"] = offset * 1000
+            mauc.sync_phone_to_server(self.testUserUUID1, self.entry_list)
+
+        for offset in range(self.curr_ts - 10, self.curr_ts):
+            for entry in self.ios_entry_list:
+                entry["metadata"]["write_ts"] = offset
+            mauc.sync_phone_to_server(self.testUserUUIDios, self.ios_entry_list)
+            
+        # Now, repeat the above tests to ensure that they get moved again
+        self.assertEqual(len(self.uc1.getMessage()), 30)
+        # We will already have 30 entries in long-term for both android
+        self.assertEqual(len(list(self.ts1.find_entries())), 30)
+        
+        self.assertEqual(len(self.ucios.getMessage()), 30)
+        # and ios
+        self.assertEqual(len(list(self.tsios.find_entries())), 30)
+
+        # The timequery is 5 secs into the past, to avoid races
+        # So let's sleep here for 5 secs
+        time.sleep(5)
+
+        # Then we move entries for user1 into longterm
+        enuah.UserCacheHandler.getUserCacheHandler(self.testUserUUID1).moveToLongTerm()
+
+        # Now, we have two sets of entries, so we will have 60 entries in longterm
+        self.assertEqual(len(self.uc1.getMessage()), 0)
+        self.assertEqual(len(list(self.ts1.find_entries())), 60)
+        
+        # Then, we move entries for the ios user into longterm
+        enuah.UserCacheHandler.getUserCacheHandler(self.testUserUUIDios).moveToLongTerm()
+        
+        self.assertEqual(len(self.ucios.getMessage()), 0)
+        self.assertEqual(len(list(self.tsios.find_entries())), 60)
+        
+        # 60 entries from android + 60 entries from ios = 120
+        self.assertEqual(edb.get_timeseries_db().find().count(), 120)
+        self.assertEqual(edb.get_timeseries_error_db().find().count(), 0)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
