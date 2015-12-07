967016eab70b356654049dbf5eaf73da4e51ccd4
Jeff Huang <jeffdh5@gmail.com>
2015-12-07 05:07:03 +0000

    fix

--- emission/net/api/stats.py
+++ emission/net/api/stats.py
@@ -44,23 +44,22 @@ def setClientMeasurements(user, reportedVals):
 def storeClientEntry(user, key, ts, reading, metadata):
   logging.debug("storing client entry for user %s, key %s at timestamp %s" % (user, key, ts))
   response = None
-  try:
-    # float first, because int doesn't recognize floats represented as strings.
-    # Android timestamps are in milliseconds, while Giles expects timestamps to be
-    # in seconds, so divide by 1000 when you hit this case.
-    # ios timestamps are in seconds.
-    if ts > 9999999999:
-      ts = ts/1000
+  # float first, because int doesn't recognize floats represented as strings.
+  # Android timestamps are in milliseconds, while Giles expects timestamps to be
+  # in seconds, so divide by 1000 when you hit this case.
+  # ios timestamps are in seconds.
+  if ts > 9999999999:
+    ts = ts/1000
 
-    currEntry = createEntry(user, key, ts, reading)
-    # Add the os and app versions from the metadata dict
-    currEntry.update(metadata)
+  currEntry = createEntry(user, key, ts, reading)
+  # Add the os and app versions from the metadata dict
+  currEntry.update(metadata)
+
+  try:
     response = get_client_stats_db().insert(currEntry)
   except Exception as e:
     logging.debug("failed to store client entry for user %s, key %s at timestamp %s" % (user, key, ts))
     logging.debug("exception was: %s" % (e))
-
-    currEntry = createEntry(user, key, ts, reading)
     get_client_stats_db_backup().insert(currEntry)
   return response != None
 
@@ -73,15 +72,13 @@ def storeClientEntry(user, key, ts, reading, metadata):
 def storeServerEntry(user, key, ts, reading):
   logging.debug("storing server entry %s for user %s, key %s at timestamp %s" % (reading, user, key, ts))
   response = None
+  currEntry = createEntry(user, key, ts, reading)
+  
   try:
-
-    currEntry = createEntry(user, key, ts, reading)
     response = get_server_stats_db().insert(currEntry)
   except Exception as e:
     logging.debug("failed to store server entry %s for user %s, key %s at timestamp %s" % (reading, user, key, ts))
     logging.debug("exception was: %s" % (e))
-
-    currEntry = createEntry(user, key, ts, reading)
     get_server_stats_db_backup().insert(currEntry)
 
   # Return boolean that tells you whether the insertion was successful or not
@@ -94,16 +91,13 @@ def storeResultEntry(user, key, ts, reading):
   # Sometimes timestamp comes in as a float, represented as seconds.[somethign else]; truncate digits after the
   # decimal
   ts = int(ts)
+  currEntry = createEntry(user, key, ts, reading)
 
   try:
-    currEntry = createEntry(user, key, ts, reading)
     response = get_result_stats_db().insert(currEntry)
-
   except Exception as e:
     logging.debug("failed to store result entry %s for user %s, key %s at timestamp %s" % (reading, user, key, ts))
     logging.debug("exception was: %s" % (e))
-
-    currEntry = createEntry(user, key, ts, reading)
     get_result_stats_db_backup().insert(currEntry)
 
   # Return boolean that tells you whether the insertion was successful or not
