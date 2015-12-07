94e0ac847bb59a7befbfb47cda4e9b6bbcc14298
Jeff Huang <jeffdh5@gmail.com>
2015-12-07 04:54:54 +0000

    fixed bug in exception handling

--- emission/net/api/stats.py
+++ emission/net/api/stats.py
@@ -58,6 +58,7 @@ def storeClientEntry(user, key, ts, reading, metadata):
     response = get_client_stats_db().insert(currEntry)
   except Exception as e:
     logging.debug("failed to store client entry for user %s, key %s at timestamp %s" % (user, key, ts))
+    currEntry = createEntry(user, key, ts, reading)
     get_client_stats_db_backup().insert(currEntry)
   return response != None
 
@@ -77,6 +78,7 @@ def storeServerEntry(user, key, ts, reading):
   except Exception as e:
     print(e)
     logging.debug("failed to store server entry %s for user %s, key %s at timestamp %s" % (reading, user, key, ts))
+    currEntry = createEntry(user, key, ts, reading)
     get_server_stats_db_backup().insert(currEntry)
 
   # Return boolean that tells you whether the insertion was successful or not
@@ -96,6 +98,7 @@ def storeResultEntry(user, key, ts, reading):
 
   except Exception as e:
     logging.debug("failed to store result entry %s for user %s, key %s at timestamp %s" % (reading, user, key, ts))
+    currEntry = createEntry(user, key, ts, reading)
     get_result_stats_db_backup().insert(currEntry)
 
   # Return boolean that tells you whether the insertion was successful or not
