aed451bc41ee09a9ff11f350881c320557fea71b
K. Shankari <shankari@eecs.berkeley.edu>
2016-02-08 22:47:23 -0800

    Support loading with a specified username so that we can test more easily
    Example timelines that can be used with the data are at:
    https://github.com/shankari/data-collection-eval/tree/master/results_dec_2015/ucb.sdb.android.1/timeseries
    
    Note that timeline dumps contain object IDS, so reloading the same timeline
    with multiple usernames does not work since the `save` with the same objectID
    results in the objects getting overwritten.

--- bin/debug/load_timeline_for_day_and_user.py
+++ bin/debug/load_timeline_for_day_and_user.py
@@ -2,13 +2,25 @@ import json
 import bson.json_util as bju
 import emission.core.get_database as edb
 import sys
+import argparse
+import uuid
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print "Usage: %s <filename>" % (sys.argv[0])
-
-    fn = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("timeline_filename",
+        help="the name of the file that contains the json representation of the timeline")
+    parser.add_argument("-u", "--user_uuid",
+        help="overwrite the user UUID from the file")
+    args = parser.parse_args()
+    fn = args.timeline_filename
+    print fn
     print "Loading file " + fn
+    tsdb = edb.get_timeseries_db()
+    override_uuid = None
+    if args.user_uuid is not None:
+        override_uuid = uuid.uuid3(uuid.NAMESPACE_URL, "mailto:%s" % args.user_uuid.encode("UTF-8"))
     entries = json.load(open(fn), object_hook = bju.object_hook)
     for entry in entries:
-        edb.get_timeseries_db().save(entry)
+        if args.user_uuid is not None:
+            entry["user_id"] = override_uuid
+        tsdb.save(entry)
