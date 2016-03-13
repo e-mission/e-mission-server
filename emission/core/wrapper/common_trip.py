31b50811cfcfbd02b592e03b4d3dd8ec9326466a
K. Shankari <shankari@eecs.berkeley.edu>
2016-03-13 09:23:57 -0700

    Fixed the common place and common trips wrappers to be consistent with existing wrappers
    In particular:
    - removed the new common_place_id which is a concatenation of the user_id and
      the coordinates of the new one
    - renamed the geojson fields to match the ones in place and trip
    - added a places array to common place, consistent with the trips array for common trip (currently unused)
    - in common_trip had start_loc and end_loc represent locations, and added new fields to represent the starting place id and ending place id, similar to trip

--- emission/core/wrapper/common_trip.py
+++ emission/core/wrapper/common_trip.py
@@ -2,8 +2,10 @@ import logging
 import emission.core.wrapper.wrapperbase as ecwb
 
 class CommonTrip(ecwb.WrapperBase):
-    props = {"start_loc" : ecwb.WrapperBase.Access.WORM, # _id of common_place of starting point
-             "end_loc" : ecwb.WrapperBase.Access.WORM, # _id of common_place of ending point
+    props = {"start_place" : ecwb.WrapperBase.Access.WORM, # _id of common_place of starting point
+             "end_place" : ecwb.WrapperBase.Access.WORM, # _id of common_place of ending point
+             "start_loc" : ecwb.WrapperBase.Access.WORM, # JSON of the start location, duplicated for ease of access
+             "end_loc" : ecwb.WrapperBase.Access.WORM, # JSON of the end location, duplicated for ease of access
              "trips" : ecwb.WrapperBase.Access.WORM, # List of trip_ids that are associated with this common trip
              "probabilites" : ecwb.WrapperBase.Access.WORM,  # a matrix that represents the probabilites for edge
              "user_id" : ecwb.WrapperBase.Access.WORM}
@@ -12,6 +14,6 @@ class CommonTrip(ecwb.WrapperBase):
     def _populateDependencies(self):
         pass
 
-    geojson = []
+    geojson = ['start_loc', 'end_loc']
     enums = {}
     nullable = []
