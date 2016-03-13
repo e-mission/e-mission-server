f874aadfc82b0d1cd4eced742db915aa1693ba18
K. Shankari <shankari@eecs.berkeley.edu>
2016-03-13 09:28:31 -0700

    Common place and trip generation code changes for new wrapper classes + remove pickling!
    This currently iterates twice over the place -> successor structure as opposed to once before.
    
    It should be fairly easy to fix - just create new entries in the database every
    time you insert a new value into succcessor map. But I think that there needs
    to be much more careful design of the data structures, the storage and the computation.
    In particular, see https://github.com/e-mission/e-mission-server/issues/243
    
    So we can fix this then.
    
    Bonus fix: Remove pickling to as a technique numpy array into the database.
    pickled data cannot be unpickled by javascript. We could unpickle before
    storing into the usercache, but it is much more human readable to directly
    store a 2-D array in mongo, which is a supported data structure.

--- emission/storage/decorations/common_place_queries.py
+++ emission/storage/decorations/common_place_queries.py
@@ -13,21 +13,23 @@ def save_common_place(common_place):
     db = edb.get_common_place_db()
     db.save(common_place)
 
-def get_common_place_from_db(_id):
+def get_common_place_from_db(common_place_id):
     db = edb.get_common_place_db()
-    json_obj = db.find_one({"common_place_id" : _id})
+    json_obj = db.find_one({"_id" : common_place_id})
     return make_common_place(json_obj)
 
 def get_all_common_places_for_user(user_id):
     db = edb.get_common_place_db()
     return db.find({"user_id" : user_id})
 
-def make_new_common_place(user_id, coords, successors):
+def get_common_place_at_location(loc):
+    db = edb.get_common_place_db()
+    return make_common_place(db.find_one({"location": loc}))
+
+def make_new_common_place(user_id, loc):
     place = ecwcp.CommonPlace()
     place.user_id = user_id
-    place.coords = coords
-    place.common_place_id = "%s%s" % (user_id, coords["coordinates"])
-    place.successors = successors
+    place.location = loc
     return place
 
 def make_common_place(props):
@@ -40,19 +42,25 @@ def create_places(list_of_cluster_data, user_id):
     for dct in list_of_cluster_data:
         start_name = dct['start']
         end_name = dct['end']
-        start_coords = dct['start_coords']
-        end_coords = dct['end_coords']
-        start_place_id = "%s%s" % (user_id, start_coords)
-        end_place_id = "%s%s" % (user_id, end_coords)
-        if start_place_id not in places_to_successors:
-            places_to_successors[start_place_id] = {"successors" : [], "coords" : start_coords.coordinate_list()}
+        start_loc = gj.Point(dct['start_coords'].coordinate_list())
+        end_loc = gj.Point(dct['end_coords'].coordinate_list())
+        start_loc_str = gj.dumps(start_loc, sort_keys=True)
+        end_loc_str = gj.dumps(end_loc, sort_keys=True)
+        if start_loc_str not in places_to_successors:
+            places_to_successors[start_loc_str] = []
         else:
-            places_to_successors[start_place_id]["successors"].append(end_place_id)
-        if end_place_id not in places_to_successors:
-            places_to_successors[end_place_id] = {"successors" : [], "coords" : end_coords.coordinate_list()}
+            places_to_successors[start_loc_str].append(end_loc)
+        if end_loc_str not in places_to_successors:
+            places_to_successors[end_loc_str] = []
 
-    for place_id, info in places_to_successors.iteritems():
-        start = make_new_common_place(user_id, gj.Point(info['coords']), info["successors"])
+    for loc_str in places_to_successors.iterkeys():
+        start = make_new_common_place(user_id, gj.loads(loc_str))
+        save_common_place(start)
+
+    for loc_str, successors in places_to_successors.iteritems():
+        start = get_common_place_at_location(gj.loads(loc_str))
+        successor_places = map(lambda loc:get_common_place_at_location(loc), successors)
+        start.successors = successor_places
         save_common_place(start)
 
 ### Graph queries
