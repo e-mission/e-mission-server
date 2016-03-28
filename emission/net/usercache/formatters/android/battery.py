13646a041b58411d606bb7816cba71e9d953ae3d
K. Shankari <shankari@eecs.berkeley.edu>
2016-03-27 17:00:27 -0700

    Fixes to better handle the battery wrappers
    Removed some wrappers that were not really relevant for android (or at least,
    not readable via the intent), and were apparently not present even when read
    directly.
    
    http://stackoverflow.com/questions/32280582/android-batterymanager-returning-0-for-all-property-retrieval-calls
    
    Also ensured that the android formatter also converted metadata, converted the
    enums, and so on.

--- emission/net/usercache/formatters/android/battery.py
+++ emission/net/usercache/formatters/android/battery.py
@@ -1,9 +1,34 @@
-import emission.core.wrapper.motionactivity as ecwa
+import emission.core.wrapper.battery as ecwb
 import emission.net.usercache.formatters.common as fc
 import attrdict as ad
 
 def format(entry):
-    formatted_entry = entry
-    formatted_entry.data.ts = formatted_entry.metadata.write_ts
-    formatted_entry.data.local_dt = formatted_entry.metadata.write_local_dt
-    formatted_entry.data.fmt_time = formatted_entry.metadata.write_fmt_time
+    formatted_entry = ad.AttrDict()
+    formatted_entry["_id"] = entry["_id"]
+    formatted_entry.user_id = entry.user_id
+
+    metadata = entry.metadata
+    # adds the python datetime and fmt_time entries. important for future searches!
+    fc.expand_metadata_times(metadata)
+    formatted_entry.metadata = metadata
+
+    data = ad.AttrDict()
+    # ios sets data in a ratio between 0 and 1, so let's convert to percent to be consistent
+    # with android
+    data.battery_level_pct = entry.data.battery_level_ratio * 100
+
+    if entry.data.battery_status == 0:
+        data.battery_status = ecwb.BatteryStatus.UNKNOWN
+    elif entry.data.battery_status == 1:
+        data.battery_status = ecwb.BatteryStatus.DISCHARGING
+    elif entry.data.battery_status == 2:
+        data.battery_status = ecwb.BatteryStatus.CHARGING
+    elif entry.data.battery_status == 3:
+        data.battery_status = ecwb.BatteryStatus.FULL
+
+    data.ts = formatted_entry.metadata.write_ts
+    data.local_dt = formatted_entry.metadata.write_local_dt
+    data.fmt_time = formatted_entry.metadata.write_fmt_time
+    formatted_entry.data = data
+
+    return formatted_entry
--- emission/net/usercache/formatters/ios/battery.py
+++ emission/net/usercache/formatters/ios/battery.py
@@ -1,9 +1,36 @@
-import emission.core.wrapper.motionactivity as ecwa
+import emission.core.wrapper.battery as ecwb
 import emission.net.usercache.formatters.common as fc
 import attrdict as ad
 
 def format(entry):
-    formatted_entry = entry
-    formatted_entry.data.ts = formatted_entry.metadata.write_ts
-    formatted_entry.data.local_dt = formatted_entry.metadata.write_local_dt
-    formatted_entry.data.fmt_time = formatted_entry.metadata.write_fmt_time
+    formatted_entry = ad.AttrDict()
+    formatted_entry["_id"] = entry["_id"]
+    formatted_entry.user_id = entry.user_id
+
+    metadata = entry.metadata
+    # adds the python datetime and fmt_time entries. important for future searches!
+    fc.expand_metadata_times(metadata)
+    formatted_entry.metadata = metadata
+
+    data = ad.AttrDict()
+    # There are lots of fields incoming, so instead of copying each one over, let's just
+    # copy the whole thing
+    data = copy.copy(entry.data)
+
+    if entry.data.battery_status == 1:
+        data.battery_status = ecwb.BatteryStatus.UNKNOWN
+    elif entry.data.battery_status == 2:
+        data.battery_status = ecwb.BatteryStatus.CHARGING
+    elif entry.data.battery_status == 3:
+        data.battery_status = ecwb.BatteryStatus.DISCHARGING
+    elif entry.data.battery_status == 4:
+        data.battery_status = ecwb.BatteryStatus.NOT_CHARGING
+    elif entry.data.battery_status == 5:
+        data.battery_status = ecwb.BatteryStatus.FULL
+
+    data.ts = formatted_entry.metadata.write_ts
+    data.local_dt = formatted_entry.metadata.write_local_dt
+    data.fmt_time = formatted_entry.metadata.write_fmt_time
+    formatted_entry.data = data
+
+    return formatted_entry
