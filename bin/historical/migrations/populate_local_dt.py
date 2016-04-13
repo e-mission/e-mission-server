710d9ddb6aee154b1ed8b885b6f3a3b3f91f5fdd
K. Shankari <shankari@eecs.berkeley.edu>
2016-04-13 06:57:03 -0700

    Add support for converting timeline files to the new format as well
    We used this to convert the real data test timeline files.
    This also allowed us to test the conversion code which found a real bug!

--- bin/historical/migrations/populate_local_dt.py
+++ bin/historical/migrations/populate_local_dt.py
@@ -3,6 +3,7 @@ import logging
 
 import arrow
 import argparse
+import json
 
 import emission.core.get_database as edb
 import emission.storage.decorations.local_date_queries as ecsdlq
@@ -31,7 +32,7 @@ def fix_timeseries(key):
     logging.debug("Fixing %s entries for key %s" % (tsdb_cursor.count(), key))
     data_local_dt = False
     for entry in tsdb.find({'metadata.key': key}):
-        entry["data"]["write_local_dt"] = get_local_date(entry['metadata']['write_fmt_time'],
+        entry["metadata"]["write_local_dt"] = get_local_date(entry['metadata']['write_fmt_time'],
             entry['metadata']['time_zone'])
         if 'local_dt' in entry['data']:
             if data_local_dt == False:
@@ -44,6 +45,26 @@ def fix_timeseries(key):
                 logging.info("not overriding local_dt for key %s" % key)
                 data_local_dt = False
         tsdb.save(entry)
+
+def fix_file(filename):
+    timeseries = json.load(open(filename))
+    logging.debug("Fixing %s entries for filename %s" % (len(timeseries), filename))
+    data_local_dt = False
+    for entry in timeseries:
+        entry["metadata"]["write_local_dt"] = get_local_date(entry['metadata']['write_fmt_time'],
+            entry['metadata']['time_zone'])
+        if 'local_dt' in entry['data']:
+            if data_local_dt == False:
+                logging.info("overriding local_dt for file %s" % filename)
+                data_local_dt = True
+            entry['data']['local_dt'] = get_local_date(entry['data']['fmt_time'],
+                entry['metadata']['time_zone'])
+        else:
+            if data_local_dt == True:
+                logging.info("not overriding local_dt for key %s" % key)
+                data_local_dt = False
+    logging.debug("Finished converting %s entries" % len(timeseries))
+    json.dump(timeseries, open(filename, "w"), indent=4)
 
 def fix_trips_or_sections(collection):
     tsdb = edb.get_timeseries_db()
@@ -96,9 +117,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("key",
         help="the key representing the stream that we want to fix")
+    parser.add_argument("-f", "--filename",
+        help="a saved timeline whose local_dt needs to be fixed. If this is specified, key is ignored")
 
     args = parser.parse_args()
-    if args.key == "trips":
+    if args.filename is not None:
+        fix_file(args.filename)
+    elif args.key == "trips":
         fix_trips_or_sections(edb.get_trip_new_db())
     elif args.key == "sections":
         fix_trips_or_sections(edb.get_section_new_db())
