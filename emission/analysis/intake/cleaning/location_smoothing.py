ccf76ebb03bb1c1c70a37d6f606c1ea5c2f5a41d
K. Shankari <shankari@eecs.berkeley.edu>
2016-04-23 15:03:40 -0700

    Minor fix to pass the key while reading objects
    We should have done this last time, but apparently, we forgot

--- emission/analysis/intake/cleaning/location_smoothing.py
+++ emission/analysis/intake/cleaning/location_smoothing.py
@@ -89,7 +89,8 @@ def add_heading_change(points_df):
 def filter_current_sections(user_id):
     time_query = epq.get_time_range_for_smoothing(user_id)
     try:
-        sections_to_process = esda.get_objects(user_id, time_query)
+        sections_to_process = esda.get_objects(esda.RAW_SECTION_KEY, user_id,
+                                               time_query)
         for section in sections_to_process:
             logging.info("^" * 20 + ("Smoothing section %s for user %s" % (section.get_id(), user_id)) + "^"
  * 20)
