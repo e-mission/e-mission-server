cb87a5639eee28f27b43f06cbfe4a523b0bc1a9f
K. Shankari <shankari@eecs.berkeley.edu>
2016-04-23 14:56:34 -0700

    Remove OSM entries + mark mode as enum
    Our local nominatim server does not appear to return the OSM IDs, so let's
    skip storing them for now. Also, ensure that the mode for a section is marked
    as an enum so that it is serialized properly

--- emission/core/wrapper/cleanedplace.py
+++ emission/core/wrapper/cleanedplace.py
@@ -5,10 +5,11 @@ class Cleanedplace(ecwp.Place):
     props = ecwp.Place.props
     props.update(
         {"raw_places": ecwb.WrapperBase.Access.WORM, # raw places that were combined to from this cleaned place
-         "display_name": ecwb.WrapperBase.Access.WORM, # The human readable name for this place
-         "osm_type": ecwb.WrapperBase.Access.WORM, # The open street map type for this place. we can use this to query for other geospatial information
-         "osm_id": ecwb.WrapperBase.Access.WORM # The open street map id for this place. we can use this to query for other geospatial information
+         "display_name": ecwb.WrapperBase.Access.WORM # The human readable name for this place
     })
 
     def _populateDependencies(self):
         super(Cleanedplace, self)._populateDependencies()
+
+    def append_raw_place(self, place_id):
+        self["raw_places"].append(place_id)
--- emission/core/wrapper/recreatedlocation.py
+++ emission/core/wrapper/recreatedlocation.py
@@ -1,4 +1,5 @@
 import emission.core.wrapper.location as ecwl
+import emission.core.wrapper.motionactivity as ecwm
 import emission.core.wrapper.wrapperbase as ecwb
 
 class Recreatedlocation(ecwl.Location):
@@ -8,6 +9,7 @@ class Recreatedlocation(ecwl.Location):
         "section": ecwb.WrapperBase.Access.WORM, # The section that this point belongs to
         "idx": ecwb.WrapperBase.Access.WORM # The index of this point in this section
     })
-
+    enums = ecwl.Location.enums
+    enums.update({"mode": ecwm.MotionTypes})
     def _populateDependencies(self):
         super(Recreatedlocation, self)._populateDependencies()
