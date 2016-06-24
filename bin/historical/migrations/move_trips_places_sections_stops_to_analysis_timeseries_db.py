b6867aa7a3d0e53f82612dcd6e79532fcddb0166
K. Shankari <shankari@eecs.berkeley.edu>
2016-06-23 21:59:59 -0700

    Move raw trip/place/section/stop objects to the analysis TSDB
    The prior version of this script copied objects from their base collection,
    but to the timeseries database instead of the analysis timeseries database.
    We now copy them over to the analysis TSDB.
    We also stop removing entries so that we don't inadvertently lose them
    
    Once this script has been successfully run, we should write and run a separate
    script to delete the entries.
    
    This is being tracked by
    https://github.com/e-mission/e-mission-server/issues/291

--- bin/historical/migrations/move_trips_places_sections_stops_to_analysis_timeseries_db.py
+++ bin/historical/migrations/move_trips_places_sections_stops_to_analysis_timeseries_db.py
@@ -20,7 +20,7 @@ def convert_collection(collection, key):
         if i % 10000 == 0:
             print "converted %s -> %s" % (wrapper, entry)
         edb.get_timeseries_db().insert(entry)
-        collection.remove(wrapper)
+        # collection.remove(wrapper)
 
 def move_ts_entries(key):
     tdb = edb.get_timeseries_db()
@@ -30,15 +30,22 @@ def move_ts_entries(key):
     logging.info("About to convert %s entries" % result_cursor.count())
 
     for i, entry_doc in enumerate(result_cursor):
+      try:
         if i % 10000 == 0:
             print "moved %s from one ts to the other" % (entry_doc)
         atdb.insert(entry_doc)
-        tdb.remove(entry_doc)
+        # tdb.remove(entry_doc)
+      except:
+        logging.info("Got error while moving %s, skipping" % (entry_doc))
 
 if __name__ == '__main__':
     # No arguments - muahahahaha. Just going to copy known fields over.
-    convert_collection(edb.get_trip_new_db(), "segmentation/raw_trip")
-    convert_collection(edb.get_place_db(), "segmentation/raw_place")
-    convert_collection(edb.get_section_new_db(), "segmentation/raw_section")
-    convert_collection(edb.get_stop_db(), "segmentation/raw_stop")
+#    convert_collection(edb.get_trip_new_db(), "segmentation/raw_trip")
+#    convert_collection(edb.get_place_db(), "segmentation/raw_place")
+#    convert_collection(edb.get_section_new_db(), "segmentation/raw_section")
+#    convert_collection(edb.get_stop_db(), "segmentation/raw_stop")
+    move_ts_entries("segmentation/raw_trip")
+    move_ts_entries("segmentation/raw_place")
+    move_ts_entries("segmentation/raw_section")
+    move_ts_entries("segmentation/raw_stop")
     move_ts_entries("analysis/smoothing")
