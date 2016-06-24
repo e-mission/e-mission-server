4204c2ab9d6e800e89b725d70b33efbdd7b33386
K. Shankari <shankari@eecs.berkeley.edu>
2016-06-23 23:19:07 -0700

    Strip out test phone data from the heatmap visualization

--- emission/net/api/visualize.py
+++ emission/net/api/visualize.py
@@ -1,6 +1,7 @@
 __author__ = 'Yin'
 # Standard imports
 import logging
+from uuid import UUID
 
 # Our imports
 from emission.analysis.result.carbon import getModeCarbonFootprint, carbonFootprintForMode
@@ -67,11 +68,21 @@ def range_mode_heatmap(modes, from_ld, to_ld, region):
     else:
         geo_query = estg.GeoQuery(["data.loc"], region)
 
-    if modes is None:
-        extra_query_list = None
-    else:
+    test_phone_ids = [UUID("079e0f1a-c440-3d7c-b0e7-de160f748e35"),
+                      UUID("c76a0487-7e5a-3b17-a449-47be666b36f6"),
+                      UUID("c528bcd2-a88b-3e82-be62-ef4f2396967a"),
+                      UUID("95e70727-a04e-3e33-b7fe-34ab19194f8b"),
+                      UUID("e471711e-bd14-3dbe-80b6-9c7d92ecc296"),
+                      UUID("fd7b4c2e-2c8b-3bfa-94f0-d1e3ecbd5fb7"),
+                      UUID("86842c35-da28-32ed-a90e-2da6663c5c73"),
+                      UUID("3bc0f91f-7660-34a2-b005-5c399598a369")]
+                      
+    test_phones_query = {"user_id": {"$nin": test_phone_ids}}
+    extra_query_list = [test_phones_query]
+
+    if modes is not None:
         mode_enum_list = [ecwm.MotionTypes[mode] for mode in modes]
-        extra_query_list = [esdlq.get_mode_query(mode_enum_list)]
+        extra_query_list.append(esdlq.get_mode_query(mode_enum_list))
 
     loc_entry_list = esda.get_entries(esda.CLEANED_LOCATION_KEY, user_id=None,
                                       time_query=time_query, geo_query=geo_query,
