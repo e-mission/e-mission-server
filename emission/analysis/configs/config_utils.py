359684f05f60c5ce7ec12fe49c12b87f34ee1ea8
K. Shankari <shankari@eecs.berkeley.edu>
2016-06-24 11:48:16 -0700

    Add support for multiple configs that are stored as RW documents
    For the pipeline, we just pick the max ts since all the configs are from the
    same user, and time on the phone is synchronous. So if we have seen a
    particular config override, we must have seen all prior values.

--- emission/analysis/configs/config_utils.py
+++ emission/analysis/configs/config_utils.py
@@ -6,8 +6,9 @@ import emission.core.wrapper.entry as ecwe
 def get_last_entry(user_id, time_query, config_key):
     user_ts = esta.TimeSeries.get_time_series(user_id)
     
-    # get the max write_ts for this stream, which corresponds to the last entry
-    # We expect this to be small, unless users are continuously overriding values
+    # get the list of overrides for this time range. This should be non zero
+    # only if there has been an override since the last run, which needs to be
+    # saved back into the cache.
     config_overrides = list(user_ts.find_entries([config_key], time_query))
     logging.debug("Found %d user overrides for user %s" % (len(config_overrides), user_id))
     if len(config_overrides) == 0:
