ec4deda0354e63d2bed8cfa01433291e3354fcca
K. Shankari <shankari@eecs.berkeley.edu>
2016-06-24 11:57:06 -0700

    Add a new entry for the syncconfig
    - new wrapper class
    - new formatters
    - new entry in entry
    
    Fairly straightforward now that we have a template

---
+++ emission/net/usercache/formatters/ios/sync_config.py
@@ -0,0 +1,26 @@
+import copy
+
+import emission.core.wrapper.syncconfig as ecws
+import emission.net.usercache.formatters.common as fc
+
+# Currently, we just reflect this back to the user, so not much editing to do
+# here. Since we get the timezone from javascript guessing, though, let's just
+# verify that it is correct.
+def format(entry):
+    formatted_entry = entry
+
+    metadata = entry.metadata
+    try:
+        valid_tz = pytz.timezone(entry.metadata.time_zone)
+    except UnknownTimeZoneError, e:
+        logging.warn("Got error %s while checking format validity" % e)
+        # Default timezone in for the Bay Area, which is probably a fairly safe
+        # assumption for now
+        metadata.time_zone = "America/Los_Angeles"
+    # adds the python datetime and fmt_time entries. important for future searches!
+    fc.expand_metadata_times(metadata)
+    formatted_entry.metadata = metadata
+
+    formatted_entry.data = entry.data
+
+    return formatted_entry;
