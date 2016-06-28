17736b9270ebd149af0eb0fc249448cfa8155fc5
K. Shankari <shankari@eecs.berkeley.edu>
2016-06-28 00:57:36 -0700

    Convert all tests to the new format as well
    It turns out that the tests were not ported over when the code was, so they
    were not actually testing the current code. This was exposed when the crutch
    for the old code was knocked away, and the tests all started failing. Ported
    over the tests to use the new data structures.
    
    Also did some minor refactoring/cleaning to move commonly used code into a
    common module so that it could be reused. The common code included setup,
    cleanup and sample trip creation functions.

--- emission/tests/analysisTests/TestSimilarity.py
+++ emission/tests/analysisTests/TestSimilarity.py
@@ -1,29 +1,36 @@
+import logging
 import unittest
-import emission.core.get_database as edb
-import emission.analysis.modelling.tour_model.similarity as similarity
-import emission.simulation.trip_gen as tg
-import math
-from emission.core.wrapper.trip_old import Trip, Coordinate
-import emission.analysis.modelling.tour_model.cluster_pipeline as cp
+import uuid
+import time
 import datetime 
 import os, os.path
 
+import emission.tests.analysisTests.tourModelTests.common as etatc
+
+import emission.core.get_database as edb
+
+import emission.analysis.modelling.tour_model.similarity as similarity
+import emission.analysis.modelling.tour_model.cluster_pipeline as cp
+
+import emission.storage.timeseries.abstract_timeseries as esta
+
 class SimilarityTests(unittest.TestCase):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self,  *args, **kwargs):
         super(SimilarityTests, self).__init__(*args, **kwargs)
-        self.data = cp.read_data(size=100)
+
+    def setUp(self):
+        self.testUUID = uuid.uuid4()
+        self.data = cp.read_data()
         #if len(self.data) == 0:
         #    tg.create_fake_trips()
         #    self.data = cp.read_data(size=100)
-        print 'there are ' + str(len(self.data))
-
-    def setUp(self):
-        pass
+        logging.info("Found %s trips" % len(self.data))
+        self.ts = esta.TimeSeries.get_time_series(self.testUUID)
 
     def tearDown(self):
-        return
-
+        edb.get_timeseries_db().remove({'user_id': self.testUUID})
+        edb.get_analysis_timeseries_db().remove({'user_id': self.testUUID})
 
     def testInit(self):
         try:
@@ -33,15 +40,18 @@ class SimilarityTests(unittest.TestCase):
         except Exception:
             self.assertTrue(False)
 
+        logging.debug("STARTING init test")
         sim = similarity.similarity([], 100)
         self.assertTrue(len(sim.data) == 0)
-        now = datetime.datetime.now()
-        start = Coordinate(47,-122)
-        end = Coordinate(47,-123)
-        t1 = Trip(None, None, None, None, now, now, start, start)
-        t2 = Trip(None, None, None, None, now, now, start, end)
+        now = time.time()
+        start = [-122,47]
+        end = [-123,47]
+        t1 = etatc._createTripEntry(self, now, now, start, start)
+        t2 = etatc._createTripEntry(self, now, now, start, end)
         sim = similarity.similarity([t1, t2], 100)
+        logging.debug("sim.data = %s" % sim.data)
         simmy = similarity.similarity([t2], 100)
+        logging.debug("simmy.data = %s" % simmy.data)
         self.assertTrue(sim.data == simmy.data)
 
     def testBinData(self):
@@ -62,17 +72,15 @@ class SimilarityTests(unittest.TestCase):
             self.assertTrue(len(sim.bins[i]) >= len(sim.bins[i+1]))
 
         data = []
-        now = datetime.datetime.now()
-        start = Coordinate(47,-122)
-        end = Coordinate(47,-123)
+        now = time.time()
+        start = [-122, 47]
+        end = [-123, 47]
         for i in range(10):
-            a = Trip(None, None, None, None, now, now, start, end)
-            data.append(a)
-        start = Coordinate(41,-74)
-        end = Coordinate(42, -74)
+            data.append(etatc._createTripEntry(self, now, now, start, end))
+        start = [-74, 41]
+        end = [-74, 42]
         for i in range(10):
-            a = Trip(None, None, None, None, now, now, start, end)
-            data.append(a)
+            data.append(etatc._createTripEntry(self, now, now, start, end))
         sim = similarity.similarity(data, 300)
         sim.bin_data()
         self.assertTrue(len(sim.bins) == 2)
@@ -86,10 +94,10 @@ class SimilarityTests(unittest.TestCase):
             self.assertTrue(b == sim.num)
 
     def testElbowDistance(self):
-        start = Coordinate(47,-122)
-        end = Coordinate(47,-123)
-        now = datetime.datetime.now()
-        t = Trip(None, None, None, None, now, now, start, end)
+        start = [-122,47]
+        end = [-133,47]
+        now = time.time()
+        t = etatc._createTripEntry(self, now, now, start, end)
         data = [t] * 11
         bins = [[1,2,3,4], [5,6,10], [7], [8], [9], [0]]
         sim = similarity.similarity(data, 300)
@@ -106,15 +114,15 @@ class SimilarityTests(unittest.TestCase):
                     self.assertTrue(sim.distance_helper(b,c))
 
     def testDistance(self):
-        start = Coordinate(-122.259447, 37.875174)
-        end1 = Coordinate(-122.259279, 37.875479)
-        end2 = Coordinate(-122.252287, 37.869569)
-        now = datetime.datetime.now()
-        t1 = Trip(None, None, None, None, now, now, start, end1)
-        t2 = Trip(None, None, None, None, now, now, start, end2)
+        start = [-122.259447, 37.875174]
+        end1 = [-122.259279, 37.875479]
+        end2 = [-122.252287, 37.869569]
+        now = time.time()
+        t1 = etatc._createTripEntry(self, now, now, start, end1)
+        t2 = etatc._createTripEntry(self, now, now, start, end2)
         sim = similarity.similarity(self.data, 300)
-        self.assertTrue(sim.distance(start.lat, start.lon, end1.lat, end1.lon))
-        self.assertTrue(not sim.distance(start.lat, start.lon, end2.lat, end2.lon))
+        self.assertTrue(sim.distance(start[1], start[0], end1[1], end1[0]))
+        self.assertTrue(not sim.distance(start[1], start[0], end2[1], end2[0]))
 
     def testGraph(self):
         if os.path.isfile('./histogram.png'):
@@ -136,12 +144,11 @@ class SimilarityTests(unittest.TestCase):
         a = sim.evaluate_bins()
         self.assertTrue(not a)
         sim = similarity.similarity(self.data, 300)
-        b = sim.evaluate_bins()
-        self.assertTrue(not b)
         sim.bin_data()
         c = sim.evaluate_bins()
         if sim.data:
             self.assertTrue(c)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     unittest.main()
