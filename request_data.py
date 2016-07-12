3d48ca62a0b56808640c8cfa217ad713e3699724
YawenWang <yw374@cornell.edu>
2016-07-11 21:04:36 -0700

    small change to the request_data script

--- request_data.py
+++ request_data.py
@@ -19,6 +19,10 @@ args = parser.parse_args()
 from_date = args.from_date
 to_date = args.to_date
 server_url = args.server_url
+
+if args.verbose:
+	logging.basicConfig(level=logging.DEBUG)
+
 r = requests.get("http://" + server_url + "/getData?from_date=" + from_date + "&to_date=" + to_date)
 
 dic = r.json() 
@@ -26,33 +30,25 @@ iphone_list = dic['iphone_data']
 android_list = dic['android_data']
 phone_list = iphone_list + android_list 
 
-file_name = "timeseries"
 tsdb = edb.get_timeseries_db()
 
 print "Loading data from " + from_date + " to " + to_date 
 print "..."
 
-for entry_list in phone_list:
-	json.dump(entry_list, open(file_name, "w"), default=bju.default, allow_nan=False, indent=4)
-	entries = json.load(open(file_name), object_hook = bju.object_hook)
+for index, entry_list in enumerate(phone_list):
+	if index < 4:
+		logging.debug("iphone" + str(index%4+1) + " first entry:")
+	else:
+		logging.debug("android" + str(index%4+1) + " first entry:")
+
+	if len(entry_list) == 0:
+		logging.debug("...has no data...")
+	else:
+		logging.debug(str(entry_list[0]))
+
+	json_str = json.dumps(entry_list, default=bju.default, allow_nan=False, indent=4)
+	entries = json.loads(json_str, object_hook = bju.object_hook)
 	for entry in entries:
 		tsdb.save(entry)
 
 print "Data loaded to local server!"
-
-if args.verbose:
-	logging.basicConfig(level=logging.DEBUG)
-
-for index, entry_list in enumerate(iphone_list):
-	logging.debug("iphone" + str(index+1) + " first entry:")
-	if len(entry_list) == 0:
-		logging.debug("...has no data...")
-	else:
-		logging.debug(str(entry_list[0]))
-
-for index, entry_list in enumerate(android_list):
-	logging.debug("android" + str(index+1) + " first entry:")
-	if len(entry_list) == 0:
-		logging.debug("...has no data...")
-	else:
-		logging.debug(str(entry_list[0]))
