46d01d371e181a4e5e5dece02ba534f64ca7af24
K. Shankari <shankari@eecs.berkeley.edu>
2016-07-12 23:49:51 -0700

    Address multiple issues with the metrics test
    Ensure that bugs such as 62ab3d4e7b1bf5e7e873ab468dba8b4fba8bd946
    are caught in the future.
    
    In particular:
    - load data for two days with different user IDs
    - ensure that the aggregate results are bigger than the user results
        - by days spanned in the timestamp case
        - by being larger by a particular number in the local_date case
    - ensure that intake pipeline is run before running the test!

--- emission/tests/netTests/TestMetrics.py
+++ emission/tests/netTests/TestMetrics.py
@@ -16,15 +16,18 @@ from emission.net.api import metrics
 class TestMetrics(unittest.TestCase):
     def setUp(self):
         etc.setupRealExample(self,
+                             "emission/tests/data/real_examples/shankari_2015-aug-21")
+        self.testUUID1 = self.testUUID
+        etc.setupRealExample(self,
                              "emission/tests/data/real_examples/shankari_2015-aug-27")
-        eaicf.filter_accuracy(self.testUUID)
-        estfm.move_all_filters_to_data()
+        etc.runIntakePipeline(self.testUUID1)
+        etc.runIntakePipeline(self.testUUID)
         logging.info(
             "After loading, timeseries db size = %s" % edb.get_timeseries_db().count())
-        self.day_start_ts = 1440658800
-        self.day_end_ts = 1440745200
-        self.day_start_dt = esdldq.get_local_date(self.day_start_ts, "America/Los_Angeles")
-        self.day_end_dt = esdldq.get_local_date(self.day_end_ts, "America/Los_Angeles")
+        self.aug_start_ts = 1438387200
+        self.aug_end_ts = 1441065600
+        self.day_start_dt = esdldq.get_local_date(self.aug_start_ts, "America/Los_Angeles")
+        self.day_end_dt = esdldq.get_local_date(self.aug_end_ts, "America/Los_Angeles")
 
     def tearDown(self):
         self.clearRelatedDb()
@@ -32,10 +35,12 @@ class TestMetrics(unittest.TestCase):
     def clearRelatedDb(self):
         edb.get_timeseries_db().remove({"user_id": self.testUUID})
         edb.get_analysis_timeseries_db().remove({"user_id": self.testUUID})
+        edb.get_timeseries_db().remove({"user_id": self.testUUID1})
+        edb.get_analysis_timeseries_db().remove({"user_id": self.testUUID1})
 
     def testCountTimestampMetrics(self):
         met_result = metrics.summarize_by_timestamp(self.testUUID,
-                                       self.day_start_ts, self.day_end_ts,
+                                                    self.aug_start_ts, self.aug_end_ts,
                                        'd', 'count')
         logging.debug(met_result)
 
@@ -49,17 +54,19 @@ class TestMetrics(unittest.TestCase):
         self.assertEqual(user_met_result[0].ON_FOOT, 4)
         self.assertEqual(user_met_result[0].BICYCLING, 2)
         self.assertEqual(user_met_result[0].IN_VEHICLE, 3)
-        # We are not going to make assertions about the aggregate values since
-        # they are affected by other entries in the database but we expect them
-        # to be at least as much as the user values
-        self.assertGreaterEqual(agg_met_result[0].BICYCLING,
-                                user_met_result[0].BICYCLING)
+        # We are not going to make absolute value assertions about
+        # the aggregate values since they are affected by other
+        # entries in the database. However, because we have at least
+        # data for two days in the database, the aggregate data
+        # must be at least that much larger than the original data.
+        self.assertEqual(len(agg_met_result), 8)
+
 
     def testCountLocalDateMetrics(self):
         met_result = metrics.summarize_by_local_date(self.testUUID,
                                                      ecwl.LocalDate({'year': 2015, 'month': 8}),
                                                      ecwl.LocalDate({'year': 2015, 'month': 9}),
-                                                     'DAILY', 'count')
+                                                     'MONTHLY', 'count')
         self.assertEqual(met_result.keys(), ['aggregate_metrics', 'user_metrics'])
         user_met_result = met_result['user_metrics']
         agg_met_result = met_result['aggregate_metrics']
@@ -68,7 +75,6 @@ class TestMetrics(unittest.TestCase):
 
         # local timezone means that we only have one entry
         self.assertEqual(len(user_met_result), 1)
-        self.assertEqual(user_met_result[0].local_dt.day, 27)
         self.assertEqual(user_met_result[0].ON_FOOT, 6)
         self.assertEqual(user_met_result[0].BICYCLING, 4)
         self.assertEqual(user_met_result[0].IN_VEHICLE, 5)
@@ -76,7 +82,11 @@ class TestMetrics(unittest.TestCase):
         # they are affected by other entries in the database but we expect them
         # to be at least as much as the user values
         self.assertGreaterEqual(agg_met_result[0].BICYCLING,
-                                user_met_result[0].BICYCLING)
+                                user_met_result[0].BICYCLING + 1) # 21s has one bike trip
+        self.assertGreaterEqual(agg_met_result[0].ON_FOOT,
+                                user_met_result[0].ON_FOOT + 3) # 21s has one bike trip
+        self.assertGreaterEqual(agg_met_result[0].IN_VEHICLE,
+                                user_met_result[0].IN_VEHICLE + 3) # 21s has one bike trip
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
