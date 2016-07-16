e-mission/e-mission-server

commit e24b3656197e86b680735024228ab2698ea6a10c

Move all logging to the main function
Before this change, we logged the message, but did not log any timestamps,
which makes it harder to see how long various operations took, correlate with
logs on other servers, etc.

The logs were also configured individually in each test and some non-test
modules, making it hard to change the log format easily. Finally, the logs
were occasionally configured globally on module load. This meant that they
would override any configurations in main.

I went through and unified the logging module configuration.
- modules do not have module level logging configurations. no exceptions.
    (scripts in `bin` are exceptions because they are intended to be more like scripts)
- modules that are invoked directly by the user (e.g intake, webapp) have
  existing configurations
- tests have a standard logging configuration defined in `emission.tests.common`
- all tests configure their logging in main using the standard logging configuration

The exception is cfc_webapp. As part of this change, I created a __main__
function, and the app starts up correctly with it, but if I move the config in
there, it is never applied. In particular, the `This should go to the log file`
never shows up.

Some other module must be configuring logging, but I am not sure which it is,
since a search for logging.basicConfig() in the emission directory does not
find any matches that are not commented out or in `prior_unused` or `notebooks`
or `incomplete_tests`.

emission/net/api/Profile.py
@@ -14,10 +14,6 @@ from emission.analysis.modelling.tour_model.trajectory_matching.route_matching i
 from emission.analysis.modelling.tour_model.K_medoid import kmedoids, user_route_data
 import emission.analysis.modelling.tour_model.cluster_pipeline as cp
 
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.DEBUG)
-Profiles=get_profile_db()
-TOLERANCE = 200 #How much movement we allow before updating zip codes again. Should be pretty large.. this is conservative
-
 def update_profiles(dummy_users=False):
     if dummy_users:
         user_list = ['1']
@@ -83,5 +79,9 @@ def _check_zip_validity(user_home, user):
     return False
 
 if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.DEBUG)
+    Profiles=get_profile_db()
+    TOLERANCE = 200 #How much movement we allow before updating zip codes again. Should be pretty large.. this is conservative
+
     update_profiles()
 

emission/analysis/intake/cleaning/cleaning_methods/jump_smoothing.py
@@ -14,7 +14,7 @@ from enum import Enum
 
 import emission.analysis.point_features as pf
 import emission.core.common as ec
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 class SmoothBoundary(object):
     def __init__(self, maxSpeed = 100):

emission/analysis/intake/cleaning/cleaning_methods/speed_outlier_detection.py
@@ -4,8 +4,6 @@
 # Standard imports
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
-
 class BoxplotOutlier(object):
     MINOR = 1.5
     MAJOR = 3

emission/core/wrapper/filter_modules.py
@@ -10,7 +10,7 @@ import os
 import math
 import datetime
 import logging
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.DEBUG)
+# logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.DEBUG)
 import random
 from uuid import UUID
 

emission/incomplete_tests/TestApiCalls.py
@@ -17,5 +17,3 @@ class TestCarbon(unittest.TestCase):
         self.server_port = config_data["server"]["port"]
         self.log_base_dir = config_data["paths"]["log_base_dir"]
 
-    def testStoreSensedTrips(self):
-        

emission/net/api/cfc_webapp.py
@@ -7,9 +7,6 @@ import bottle as bt
 import sys
 import os
 import logging
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(thread)d:%(message)s',
-                  filename='webserver_debug.log', level=logging.DEBUG)
-logging.debug("This should go to the log file")
 
 from datetime import datetime
 import time
@@ -581,35 +578,40 @@ def getUUID(request, inHeader=False):
   return retUUID
 # Auth helpers END
 
-# We have see the sockets hang in practice. Let's set the socket timeout = 1
-# hour to be on the safe side, and see if it is hit.
-socket.setdefaulttimeout(float(socket_timeout))
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(thread)d:%(message)s',
+                        filename='webserver_debug.log', level=logging.DEBUG)
+    logging.debug("This should go to the log file")
 
-for plugin in app.plugins:
-    if isinstance(plugin, JSONPlugin):
-        print("Replaced json_dumps in plugin with the one from bson")
-        plugin.json_dumps = bson.json_util.dumps
+    # We have see the sockets hang in practice. Let's set the socket timeout = 1
+    # hour to be on the safe side, and see if it is hit.
+    socket.setdefaulttimeout(float(socket_timeout))
 
-print("Changing bt.json_loads from %s to %s" % (bt.json_loads, bson.json_util.loads))
-bt.json_loads = bson.json_util.loads
+    for plugin in app.plugins:
+        if isinstance(plugin, JSONPlugin):
+            print("Replaced json_dumps in plugin with the one from bson")
+            plugin.json_dumps = bson.json_util.dumps
 
-# The selection of SSL versus non-SSL should really be done through a config
-# option and not through editing source code, so let's make this keyed off the
-# port number
-if server_port == "443":
-  # We support SSL and want to use it
-  run(host=server_host, port=server_port, server='cherrypy', debug=True,
-      certfile=ssl_cert, keyfile=private_key, ssl_module='builtin')
-else:
-  # Non SSL option for testing on localhost
-  # We can theoretically use a separate skipAuth flag specified in the config file,
-  # but then we have to define the behavior if SSL is true and we are not
-  # running on localhost but still want to run without authentication. That is
-  # not really an important use case now, and it makes people have to change
-  # two values and increases the chance of bugs. So let's key the auth skipping from this as well.
-  skipAuth = True
-  print "Running with HTTPS turned OFF, skipAuth = True"
+    print("Changing bt.json_loads from %s to %s" % (bt.json_loads, bson.json_util.loads))
+    bt.json_loads = bson.json_util.loads
 
-  run(host=server_host, port=server_port, server='cherrypy', debug=True)
+    # The selection of SSL versus non-SSL should really be done through a config
+    # option and not through editing source code, so let's make this keyed off the
+    # port number
+    if server_port == "443":
+      # We support SSL and want to use it
+      run(host=server_host, port=server_port, server='cherrypy', debug=True,
+          certfile=ssl_cert, keyfile=private_key, ssl_module='builtin')
+    else:
+      # Non SSL option for testing on localhost
+      # We can theoretically use a separate skipAuth flag specified in the config file,
+      # but then we have to define the behavior if SSL is true and we are not
+      # running on localhost but still want to run without authentication. That is
+      # not really an important use case now, and it makes people have to change
+      # two values and increases the chance of bugs. So let's key the auth skipping from this as well.
+      skipAuth = True
+      print "Running with HTTPS turned OFF, skipAuth = True"
 
-# run(host="0.0.0.0", port=server_port, server='cherrypy', debug=True)
+      run(host=server_host, port=server_port, server='cherrypy', debug=True)
+
+    # run(host="0.0.0.0", port=server_port, server='cherrypy', debug=True)

emission/net/ext_service/moves/collect.py
@@ -14,11 +14,6 @@ import math
 from sdk import Moves
 from emission.core.get_database import get_mode_db, get_section_db, get_trip_db, get_moves_db
 
-config_data = json.load(open('conf/net/api/webserver.conf'))
-log_base_dir = config_data['paths']['log_base_dir']
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
-                    filename="%s/moves_collect.log" % log_base_dir, level=logging.DEBUG)
-
 def collect():
   key_file = open('keys.json')
   key_data = json.load(key_file)
@@ -308,5 +303,10 @@ def _cleanGPSData(old_points):
     return old_points
 
 if __name__ == "__main__":
-  collect()
+    config_data = json.load(open('conf/net/api/webserver.conf'))
+    log_base_dir = config_data['paths']['log_base_dir']
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
+                    filename="%s/moves_collect.log" % log_base_dir, level=logging.DEBUG)
+
+    collect()
 

emission/tests/analysisTests/TestAlternativeTripPipeline.py
@@ -17,8 +17,7 @@ from emission.net.ext_service.moves import collect
 # from emission.net.ext_services.gmaps.common import *
 import emission.core.get_database as edb
 from crontab import CronTab
-
-logging.basicConfig(level=logging.DEBUG)
+import emission.tests.common as etc
 
 class TestAlternativeTripPipeline(unittest.TestCase):
   def setUp(self):
@@ -172,4 +171,5 @@ class TestAlternativeTripPipeline(unittest.TestCase):
    '''
 
 if __name__ == '__main__':
-    unittest.main()
+  etc.configLogging()
+  unittest.main()

commit 26e45012f6af16330aa730291ab942b91f8cda18

Move the TOLERANCE variable to the module
This was a regression caused by
e24b3656197e86b680735024228ab2698ea6a10c

emission/net/api/Profile.py
@@ -14,6 +14,8 @@ from emission.analysis.modelling.tour_model.trajectory_matching.route_matching i
 from emission.analysis.modelling.tour_model.K_medoid import kmedoids, user_route_data
 import emission.analysis.modelling.tour_model.cluster_pipeline as cp
 
+TOLERANCE = 200 #How much movement we allow before updating zip codes again. Should be pretty large.. this is conservative
+
 def update_profiles(dummy_users=False):
     if dummy_users:
         user_list = ['1']
@@ -81,7 +83,5 @@ def _check_zip_validity(user_home, user):
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.DEBUG)
     Profiles=get_profile_db()
-    TOLERANCE = 200 #How much movement we allow before updating zip codes again. Should be pretty large.. this is conservative
-
     update_profiles()
 