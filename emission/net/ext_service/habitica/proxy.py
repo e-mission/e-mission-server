adf060b03af6630462163f664df3ebce6ea176b5
juemura <ju.uemura@gmail.com>
2016-07-20 11:33:15 -0700

    different vs of habitica setup

--- emission/net/ext_service/habitica/proxy.py
+++ emission/net/ext_service/habitica/proxy.py
@@ -3,9 +3,11 @@ import json
 import requests
 import logging
 import arrow
+import urllib2
 
 # Our imports
 import emission.core.get_database as edb
+import emission.net.ext_service.habitica.setup as assist
 
 key_file = open('conf/net/keys.json')
 key_data = json.load(key_file)
@@ -14,29 +16,47 @@ url = key_data["habitica"]["url"]
 
 
 def habiticaRegister(username, email, password, our_uuid):
-  register_url = url + '/api/v3/user/auth/local/register'
-  user_request = {'username': username,'email': email,'password': password,'confirmPassword': password}
-  logging.debug("About to register %s"% user_request)
-  u = requests.post(register_url, json=user_request)
-  # Bail out if we get an error
-  u.raise_for_status()
-  user_dict = json.loads(u.text)
-  logging.debug("parsed json from habitica has keys = %s" % user_dict.keys())
+  user_dict = {}
+  #try to login and see if user is already in Habitica
+  try:
+    login_url = url + '/api/v3/user/auth/local/login'
+    user_request = {'username': username,'email': email,'password': password}
+    logging.debug("About to login %s"% user_request)
+    u = requests.post(login_url, json=user_request)
+    user_dict = json.loads(u.text)
+    logging.debug("parsed json from habitica has keys = %s" % user_dict.keys())
 
-  #Since we are randomly generating the password, we store it in case users 
-  #want to access their Habitica account from the browser
-  #Need to create a way from them to retrieve username/password
-  #metrics_data is used to calculate points based on km biked/walked
-  #last_timestamp is the last time the user got points, and bike/walk_count are the leftover km
-  habitica_user_table = edb.get_habitica_db()
-  habitica_user_table.insert({'user_id': our_uuid, 
-    'metrics_data': {'last_timestamp': arrow.utcnow().timestamp, 'bike_count': 0, 'walk_count': 0},
-    'habitica_username': username, 
-    'habitica_password': password, 
-    'habitica_id': user_dict['data']['_id'], 
-    'habitica_token': user_dict['data']['apiToken']})
+  #if u fails, then user is not in Habitica, so needs to create an account
+  except urllib2.HTTPError:
+    register_url = url + '/api/v3/user/auth/local/register'
+    user_request = {'username': username,'email': email,'password': password,'confirmPassword': password}
+    logging.debug("About to register %s"% user_request)
+    u = requests.post(register_url, json=user_request)
+    # Bail out if we get an error
+    u.raise_for_status()
+    user_dict = json.loads(u.text)
+    logging.debug("parsed json from habitica has keys = %s" % user_dict.keys())
+
+  #if user is not in e-mission db, create it
+  if edb.get_habitica_db().find({'user_id': our_uuid}).count() == 0:
+    #Since we are randomly generating the password, we store it in case users 
+    #want to access their Habitica account from the browser
+    #Need to create a way from them to retrieve username/password
+    #metrics_data is used to calculate points based on km biked/walked
+    #last_timestamp is the last time the user got points, and bike/walk_count are the leftover km
+    habitica_user_table = edb.get_habitica_db()
+    habitica_user_table.insert({'user_id': our_uuid, 
+      'metrics_data': {'last_timestamp': arrow.utcnow().timestamp, 'bike_count': 0, 'walk_count': 0},
+      'habitica_username': username, 
+      'habitica_password': password, 
+      'habitica_id': user_dict['data']['_id'], 
+      'habitica_token': user_dict['data']['apiToken'],
+      'habitica_group_id': ""})
+  
+  #Do initial setup and append GroupId to the user
+  group_id = assist.user_setup(our_uuid)
+  user_dict['group_id'] = group_id
   return user_dict
-  response = proxy.habiticaProxy(uid, 'DELETE', "/api/v3/user", {'password': password})
 
 def habiticaProxy(user_uuid, method, method_url, method_args):
   logging.debug("For user %s, about to proxy %s method %s with args %s" %
