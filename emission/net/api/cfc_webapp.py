eac1be51000fe48079ffc807d3f68c5a122cdfc2
YawenWang <yw374@cornell.edu>
2016-08-24 18:44:00 -0400

    checking number of entries before reading them into memory on the server

--- emission/net/api/cfc_webapp.py
+++ emission/net/api/cfc_webapp.py
@@ -400,16 +400,16 @@ def getPublicData():
 
   for q in user_queries:
     q.update(time_query)
-
-  data_list = map(lambda q: list(edb.get_timeseries_db().find(q).sort("metadata.write_ts")), user_queries)
-
-  num_entries = map(lambda q: len(q), data_list)
+  
+  num_entries = map(lambda q: edb.get_timeseries_db().find(q).count(), user_queries)
   total_entries = sum(num_entries)
-  logging.debug("Total entries retrieved %d" % total_entries)
+  logging.debug("Total entries requested: %d" % total_entries)
 
   threshold = 200000 
   if total_entries > threshold:
     data_list = None
+  else:
+    data_list = map(lambda q: list(edb.get_timeseries_db().find(q).sort("metadata.write_ts")), user_queries)
    
   return {'phone_data': data_list}
 
