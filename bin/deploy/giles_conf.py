94240f753704f71a1c72d829277d45e2d1cefc26
K. Shankari <shankari@eecs.berkeley.edu>
2016-09-18 15:00:05 -0700

    Fix the giles_conf script
    To match the filename change from 07dffd07583c05dc565757b34494963e3743c0e6

--- bin/deploy/giles_conf.py
+++ bin/deploy/giles_conf.py
@@ -1,11 +1,12 @@
 import json
 
-path = "conf/net/int_service/giles_conf.json"
+sample_path = "conf/net/int_service/giles_conf.json.sample"
 f = open(path, "r")
 data = json.loads(f.read())
 f.close()
 
+real_path = "conf/net/int_service/giles_conf.json"
 data['giles_base_url'] = 'http://50.17.111.19:8079'
-f = open(path, "w")
+f = open(real_path, "w")
 f.write(json.dumps(data))
 f.close()
