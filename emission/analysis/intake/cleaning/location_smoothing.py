2699b23eb4ad4c1a6553d4d5243acb2889828571
K. Shankari <shankari@eecs.berkeley.edu>
2016-09-21 12:00:44 -0700

    Switch from logging exceptions to logging info
    More log spew cleanup

--- emission/analysis/intake/cleaning/location_smoothing.py
+++ emission/analysis/intake/cleaning/location_smoothing.py
@@ -175,7 +175,7 @@ def get_points_to_filter(section_points_df, outlier_algo, filtering_algo):
             to_delete_mask = np.logical_not(filtering_algo.inlier_mask_)
             return with_speeds_df[to_delete_mask]
         except Exception as e:
-            logging.exception("Caught error %s while processing section, skipping..." % e)
+            logging.info("Caught error %s while processing section, skipping..." % e)
             return None
     else:
         logging.debug("no filtering algo specified, returning None")
