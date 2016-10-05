da6e3baaa4b67ff131276c3fcfef253c6f43a1a8
juemura <ju.uemura@gmail.com>
2016-10-05 00:58:36 -0700

    changed default tasks for new habitica user

--- emission/net/ext_service/habitica/proxy.py
+++ emission/net/ext_service/habitica/proxy.py
@@ -153,10 +153,10 @@ def setup_party(user_id, group_id_from_url, inviterId):
   return group_id
 
 def setup_default_habits(user_id):
-  bike_habit = {'type': "habit", 'text': "Bike", 'notes': "3 km = 1+ (calculated automatically)", 'up': True, 'down': False, 'priority': 2}
-  bike_habit_id = create_habit(user_id, bike_habit)
-  walk_habit = {'type': "habit", 'text': "Walk", 'notes': "1 km = 1+ (calculated automatically)", 'up': True, 'down': False, 'priority': 2}
-  walk_habit_id = create_habit(user_id, walk_habit)
+  bike_walk_habit = {'type': "habit", 'text': "Bike and Walk", 'notes': "Automatically get points for every 1 km walked or 3 km biked. ***=== DO NOT EDIT BELOW THIS POINT ===*** AUTOCHECK: {\"mapper\": \"invite_friends\", \"args\": {\"walk_scale\": 1000, \"bike_scale\": 3000}}", 'up': True, 'down': False, 'priority': 2}
+  bike_walk_habit_id = create_habit(user_id, bike_walk_habit)
+  invite_friends = {'type': "habit", 'text': "Spread the word", 'notes': "Get points for inviting your friends! We're better together. ***=== DO NOT EDIT BELOW THIS POINT ===*** AUTOCHECK: {\"mapper\": \"invite_friends\", \"args\": {}}", 'up': True, 'down': False, 'priority': 2}
+  invite_friends_id = create_habit(user_id, invite_friends)
 
 def create_habit(user_id, new_habit):
   method_uri = "/api/v3/tasks/user"
