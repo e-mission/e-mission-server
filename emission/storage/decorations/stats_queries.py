ca7ce14fac190db96fda415eb6b6e9c5fcff4203
K. Shankari <shankari@eecs.berkeley.edu>
2016-10-30 20:46:04 -0700

    Add stats for the time taken for each stage of the pipeline
    This can help us track which areas we need to focus on for performance
    improvements.

--- emission/storage/decorations/stats_queries.py
+++ emission/storage/decorations/stats_queries.py
@@ -5,26 +5,37 @@ import time
 # Our imports
 import emission.storage.timeseries.abstract_timeseries as esta
 import emission.core.wrapper.entry as ecwe
-from emission.core.get_database import get_client_stats_db_backup, get_server_stats_db_backup, get_result_stats_db_backup
 
 
 # metadata format is 
 
 def store_server_api_time(user_id, call, ts, reading):
-  data = {
-    "name": call,
-    "ts": ts,
-    "reading": reading
-  }
-  new_entry = ecwe.Entry.create_entry(user_id, "stats/server_api_time", data)
-  return esta.TimeSeries.get_time_series(user_id).insert(new_entry)
+  store_stats_entry(user_id, "stats/server_api_time", call, ts, reading)
 
 def store_server_api_error(user_id, call, ts, reading):
+  store_stats_entry(user_id, "stats/server_api_error", call, ts, reading)
+
+def store_pipeline_time(user_id, stage_string, ts, reading):
+  """
+
+  :param user_id: id of the user
+  :param stage: string representing a particular time. Typically the stage name,
+  but can also be used for sub sections of a stage
+  :param ts: timestamp at the time of the reading
+  :param reading: the duration of the stage in ms
+  :return:
+  """
+  store_stats_entry(user_id, "stats/pipeline_time", stage_string, ts, reading)
+
+def store_pipeline_error(user_id, stage_string, ts, reading):
+  store_stats_entry(user_id, "stats/pipeline_error", stage_string, ts, reading)
+
+def store_stats_entry(user_id, metadata_key, name, ts, reading):
   data = {
-    "name": call,
+    "name": name,
     "ts": ts,
     "reading": reading
   }
-  new_entry = ecwe.Entry.create_entry(user_id, "stats/server_api_error", data)
+  new_entry = ecwe.Entry.create_entry(user_id, metadata_key, data)
   return esta.TimeSeries.get_time_series(user_id).insert(new_entry)
 
