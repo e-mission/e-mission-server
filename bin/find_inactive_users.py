76d68dea3366d25896fda91040acbea36636fe48
juemura <ju.uemura@gmail.com>
2016-11-23 22:07:16 -0800

    created script to find inactive users

---
+++ bin/find_inactive_users.py
@@ -0,0 +1,28 @@
+import logging
+import emission.core.get_database as edb
+import emission.storage.timeseries.abstract_timeseries as esta
+import arrow
+
+
+def find_inactive_users():
+    inactive_users = []
+    one_week_ago_ts = arrow.utcnow().replace(weeks=-1).timestamp
+    for user in edb.get_uuid_db().find():
+        db = esta.TimeSeries.get_time_series(user['uuid']).get_data_df("stats/server_api_time", time_query=None)
+        if db.empty:
+            inactive_users.append(user)
+        else:
+            #check last usercache call: 
+            #the user is inactive if there are no calls or if the last one was before one_week_ago_ts
+            last_usercache_call = db[db['name'].str.contains('usercache', case=False)].tail(1)
+            if last_usercache_call.empty:
+                inactive_users.append(user)
+            elif last_usercache_call.iloc[0]['ts'] < one_week_ago_ts:
+                inactive_users.append(user)
+    return inactive_users
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    print find_inactive_users()
+
