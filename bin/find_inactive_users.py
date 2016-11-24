08cd5558a719ce530a0eb4074985cd2a4efc22ed
juemura <ju.uemura@gmail.com>
2016-11-23 23:09:46 -0800

    now script returns a comma separated list of emails

--- bin/find_inactive_users.py
+++ bin/find_inactive_users.py
@@ -5,21 +5,21 @@ import arrow
 
 
 def find_inactive_users():
-    inactive_users = []
+    inactive_users = ""
     one_week_ago_ts = arrow.utcnow().replace(weeks=-1).timestamp
     for user in edb.get_uuid_db().find():
         db = esta.TimeSeries.get_time_series(user['uuid']).get_data_df("stats/server_api_time", time_query=None)
         if db.empty:
-            inactive_users.append(user)
+            inactive_users+=str(user['user_email'])+', '
         else:
             #check last usercache call: 
             #the user is inactive if there are no calls or if the last one was before one_week_ago_ts
             last_usercache_call = db[db['name'].str.contains('usercache', case=False)].tail(1)
             if last_usercache_call.empty:
-                inactive_users.append(user)
+                inactive_users+=str(user['user_email'])+', '
             elif last_usercache_call.iloc[0]['ts'] < one_week_ago_ts:
-                inactive_users.append(user)
-    return inactive_users
+                inactive_users+=str(user['user_email'])+', '
+    return inactive_users[:-2]
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
