77dac57e5dccfdebdd24bbe376ace7a7bf7c85e2
K. Shankari <shankari@eecs.berkeley.edu>
2017-01-02 08:07:47 -0800

    Continue with no TEMP_HANDLED_PUBLIC_PHONES in general
    This ensures that the tests, notably
    testFilterAccuracy
    continue to pass

--- emission/pipeline/scheduler.py
+++ emission/pipeline/scheduler.py
@@ -8,14 +8,17 @@ import emission.net.usercache.abstract_usercache as enua
 import emission.storage.timeseries.aggregate_timeseries as estag
 import emission.pipeline.intake_stage as epi
 
-TEMP_HANDLED_PUBLIC_PHONES = [UUID("079e0f1a-c440-3d7c-b0e7-de160f748e35"), # iphone 1
-                         UUID("c76a0487-7e5a-3b17-a449-47be666b36f6"), # iphone 2
-                         UUID("c528bcd2-a88b-3e82-be62-ef4f2396967a"), # iphone 3
-                         UUID("95e70727-a04e-3e33-b7fe-34ab19194f8b"), # iphone 4
-                         UUID("e471711e-bd14-3dbe-80b6-9c7d92ecc296"), # android 1
-                         UUID("fd7b4c2e-2c8b-3bfa-94f0-d1e3ecbd5fb7"), # android 2
-                         UUID("86842c35-da28-32ed-a90e-2da6663c5c73"), # android 3
-                         UUID("3bc0f91f-7660-34a2-b005-5c399598a369")] # android 4
+# Comment this out and comment in the stuff below in production
+TEMP_HANDLED_PUBLIC_PHONES = []
+
+#TEMP_HANDLED_PUBLIC_PHONES = [UUID("079e0f1a-c440-3d7c-b0e7-de160f748e35"), # iphone 1
+#                         UUID("c76a0487-7e5a-3b17-a449-47be666b36f6"), # iphone 2
+#                         UUID("c528bcd2-a88b-3e82-be62-ef4f2396967a"), # iphone 3
+#                         UUID("95e70727-a04e-3e33-b7fe-34ab19194f8b"), # iphone 4
+#                         UUID("e471711e-bd14-3dbe-80b6-9c7d92ecc296"), # android 1
+#                         UUID("fd7b4c2e-2c8b-3bfa-94f0-d1e3ecbd5fb7"), # android 2
+#                         UUID("86842c35-da28-32ed-a90e-2da6663c5c73"), # android 3
+#                         UUID("3bc0f91f-7660-34a2-b005-5c399598a369")] # android 4
 
 
 def get_split_uuid_lists(n_splits, is_public_pipeline):
