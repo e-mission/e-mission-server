752108e9e735bdd60c58acfafb40f741a291ac1f
K. Shankari <shankari@eecs.berkeley.edu>
2017-01-18 10:28:12 -0800

    Fixes to cache_series after actually using it
    We are lot supporting explicit limits because we have implicit limits...
    TODO: create a proper checked-in test case for this

--- emission/storage/timeseries/cache_series.py
+++ emission/storage/timeseries/cache_series.py
@@ -13,16 +13,15 @@ import emission.storage.timeseries.geoquery as estg
 import emission.storage.timeseries.timequery as estt
 import emission.storage.timeseries.tcquery as esttc
 
-def find_entries(uuid, key_list=None, time_query=None, limit=None):
+def find_entries(uuid, key_list=None, time_query=None):
     # the usercache does not support aggreate queries, so we require
     # a UUID to return combined values. Similarly, we don't support
     # any queries other than time queries
     assert(uuid is not None)
-    ts = esta.Timeseries.get_time_series(uuid)
+    ts = esta.TimeSeries.get_time_series(uuid)
     uc = enua.UserCache.getUserCache(uuid)
     ts_entries = ts.find_entries(key_list, time_query,
                                  geo_query = None,
-                                 extra_query_list = None,
-                                 limit=limit)
-    uc_entries = uc.getMessage(key_list, time_query, limit=limit)
+                                 extra_query_list = None)
+    uc_entries = uc.getMessage(key_list, time_query)
     return list(ts_entries) + uc_entries
