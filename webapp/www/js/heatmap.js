3d9616fab26c5eef53ae0592e1dc11d76d3ef27b
K. Shankari <shankari@eecs.berkeley.edu>
2017-02-10 10:12:41 -0800

    Change the server tiles to stamen terrain to match the phone

--- webapp/www/js/heatmap.js
+++ webapp/www/js/heatmap.js
@@ -9,7 +9,7 @@ angular.module('starter.heatmap', ['ui-leaflet', 'emission.plugin.logger',
 
   angular.extend($scope.mapCtrl, {
     defaults : {
-      tileLayer: 'http://tile.stamen.com/toner/{z}/{x}/{y}.png',
+      tileLayer: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.png',
       tileLayerOptions: {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
         opacity: 0.9,
