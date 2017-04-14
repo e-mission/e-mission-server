e15be962f7fd7f0c07b6de321f9966713157b619
K. Shankari <shankari@eecs.berkeley.edu>
2017-04-14 10:40:43 -0700

    Shuffle around some imports that were causing tests to fail

--- emission/analysis/intake/cleaning/filter_accuracy.py
+++ emission/analysis/intake/cleaning/filter_accuracy.py
@@ -18,6 +18,7 @@ The high level algorithm is to:
 import logging
 
 # Our imports
+import emission.storage.pipeline_queries as epq
 import emission.storage.decorations.user_queries as esdu
 import emission.storage.timeseries.abstract_timeseries as esta
 import emission.storage.timeseries.aggregate_timeseries as estag
@@ -63,7 +64,6 @@ def convert_to_filtered(entry):
     return entry
 
 def continuous_collection_in_range(timeseries):
-    import emission.pipeline.scheduler as eps
     return timeseries.user_id in estag.TEST_PHONE_IDS and \
            timeseries.user_id not in esdu.TEMP_HANDLED_PUBLIC_PHONES
 
