7d7dc1a02245ba87aa2feab7c368222462059aef
K. Shankari <shankari@eecs.berkeley.edu>
2017-04-16 19:35:55 -0700

    Change the message displayed as part of the setup screen
    - Provide some more information on what e-mission is so that people don't think
      it is spam
    - Add QR codes in case people don't want to forward to their phones
    - Change some messages to be more explicit about what the link will do

--- webapp/www/js/app.js
+++ webapp/www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services',
-                           'starter.directives', 'ui.bootstrap'])
+                           'starter.directives', 'ui.bootstrap', 'monospaced.qrcode'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
