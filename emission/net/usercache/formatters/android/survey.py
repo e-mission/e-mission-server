fdc645091dcc9d7612dbe0f51901de808200cfdf
tan95 <andrew--tan@outlook.com>
2017-11-05 16:15:28 -0800

    First try to fix error

--- emission/net/usercache/formatters/android/survey.py
+++ emission/net/usercache/formatters/android/survey.py
@@ -9,16 +9,18 @@ def format(entry):
     formatted_entry = ad.AttrDict()
     formatted_entry["_id"] = entry["_id"]
     formatted_entry.user_id = entry.user_id
-    
+
     metadata = entry.metadata
     if "time_zone" not in metadata:
         metadata.time_zone = "America/Los_Angeles"
-    logging.debug("Timestamp conversion: %s -> %s done" % (entry.metadata.write_ts, metadata.write_ts))
-    fc.expand_metadata_times(metadata)
+    if "write_ts" in metadata:
+        logging.debug("Timestamp conversion: %s -> %s done" % (metadata.write_ts, metadata.write_ts))
+        fc.expand_metadata_times(metadata)
     formatted_entry.metadata = metadata
 
     data = entry.data
-    fc.expand_data_times(data, metadata)
+    if "write_ts" in metadata:
+        fc.expand_data_times(data, metadata)
     formatted_entry.data = data
 
     return formatted_entry
