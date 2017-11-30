6fdb8dce52c7661b0087471d8a15f29e7c924611
K. Shankari <shankari@eecs.berkeley.edu>
2017-11-30 11:53:21 -0800

    Cleanup the `emission/net` subdirectory
    Remove:
    - obsolete or overly specific functions (most of the changes in
      `emission/net/api`),
    - obsolete and currently unused services (`moves`, `walkscore`, `giles`,
      `push_notify`)
    - obsolete functionality from retained code (`cfc_webapp.py`)
    - tests for removed functionality (`TestGilesArchiver.py`, `TestStats.py`, `TestVisualize.py`)
    
    Move the `Profile` code into `trajectory_matching` in case we want to continue
    using it.

--- emission/analysis/modelling/tour_model/trajectory_matching/Profile.py
+++ emission/analysis/modelling/tour_model/trajectory_matching/Profile.py
@@ -23,40 +23,7 @@ def update_profiles(dummy_users=False):
     else:
         user_list = get_section_db().distinct('user_id')
     for user in user_list:
-        generate_user_home_work(user)
         generate_route_clusters(user)
-
-def generate_user_home_work(user):
-    user_home=detect_home(user)
-    # print user_home
-    zip_is_valid = _check_zip_validity(user_home, user)
-    logging.debug('starting for %s' % user)
-    if Profiles.find({'user_id':user}).count()==0:
-        profile_todo={'source':'Shankari','user_id': user,'home':user_home}
-        Profiles.insert(profile_todo)
-    else:
-        #TODO: make detect_home return something better than a N/A string
-        Profiles.update({"$and":[{'source':'Shankari'},
-                                     {'user_id':user}]},{"$set":{'home':user_home}})
-    user_work=detect_work_office(user)
-    Profiles.update({"$and":[{'source':'Shankari'},{'user_id':user}]},{"$set":{'work_place':user_work}})
-    user_zip=get_userZipcode(user, zip_is_valid)
-    Profiles.update({"$and":[{'source':'Shankari'},{'user_id':user}]},{"$set":{'zip':user_zip}})
-    if user_zip!='N/A':
-        geoinfo= Geocoder.geocode(user_zip)
-        # geocoder returns data in lat,lng format.
-        # we convert to lng,lat internally, since that is the format that the
-        # rest of our code is in
-        zipCen=[geoinfo[0].coordinates[1],geoinfo[0].coordinates[0]]
-    else:
-        zipCen='N/A'
-    Profiles.update({"$and":[{'source':'Shankari'},{'user_id':user}]},{"$set":{'zip_centroid':zipCen}})
-
-
-    for day in range(1,6):
-        key='work'+str(day)
-        Profiles.update({"$and":[{'source':'Shankari'},
-                                     {'user_id':user}]},{"$set":{key:detect_daily_work_office(user,day)}})
 
 def generate_route_clusters(user):
     print "In profile, generating route clusters for %s" % user
