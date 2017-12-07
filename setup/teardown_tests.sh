231c7ab27667ea836726a52be1fe5e0a58bb4663
K. Shankari <shankari@eecs.berkeley.edu>
2017-12-07 15:06:20 -0800

    Couple of teardown checks to make them work without user interaction

--- setup/teardown_tests.sh
+++ setup/teardown_tests.sh
@@ -2,8 +2,8 @@
 
 echo "Removing environment from "${CONDA_TEMP_PREFIX}
 source deactivate ${CONDA_TEMP_PREFIX}
-conda env remove --prefix ${CONDA_TEMP_PREFIX}
+conda env remove --yes --prefix ${CONDA_TEMP_PREFIX} 
 unset CONDA_TEMP_PREFIX
 rm conf/net/int_service/giles_conf.json
-rm conf/net/ext_service/habitica_conf.json
-rm conf/net/ext_service/push_conf.json
+rm conf/net/ext_service/habitica.json
+rm conf/net/ext_service/push.json
