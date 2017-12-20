310d045e8fc4eeb7524f4967ef297903d25a0b6d
K. Shankari <shankari@eecs.berkeley.edu>
2017-12-20 07:34:50 -0800

    Use the actual mappings instead of test mappings
    The test mappings were for local testing.
    Actual mappings from:
    https://github.com/e-mission/e-mission-server/issues/530#issuecomment-351561385

--- bin/historical/migrations/reset_test_phone_emails.py
+++ bin/historical/migrations/reset_test_phone_emails.py
@@ -9,5 +9,18 @@ def map_uuid_to_label(uuid, label):
     assert(ur.modified_count == 1)
 
 if __name__ == '__main__':
-    map_uuid_to_label(UUID("04bc05b3-4c2d-4138-bfbb-7eb45dce709a"), "ucb.sdb.iphone.3")
-    map_uuid_to_label(UUID("1c8fac56-6157-4a5e-aa55-03481cad1376"), "ucb.sdb.iphone.4")
+    map_uuid_to_label(UUID("e471711e-bd14-3dbe-80b6-9c7d92ecc296"), "ucb.sdb.android.1")
+    map_uuid_to_label(UUID("fd7b4c2e-2c8b-3bfa-94f0-d1e3ecbd5fb7"), "ucb.sdb.android.2")
+    map_uuid_to_label(UUID("86842c35-da28-32ed-a90e-2da6663c5c73"), "ucb.sdb.android.3")
+    map_uuid_to_label(UUID("3bc0f91f-7660-34a2-b005-5c399598a369"), "ucb.sdb.android.4")
+
+    map_uuid_to_label(UUID("079e0f1a-c440-3d7c-b0e7-de160f748e35"), "ucb.sdb.iphone.1")
+    map_uuid_to_label(UUID("c76a0487-7e5a-3b17-a449-47be666b36f6"), "ucb.sdb.iphone.2")
+    map_uuid_to_label(UUID("c528bcd2-a88b-3e82-be62-ef4f2396967a"), "ucb.sdb.iphone.3")
+    map_uuid_to_label(UUID("95e70727-a04e-3e33-b7fe-34ab19194f8b"), "ucb.sdb.iphone.4")
+
+    map_uuid_to_label(UUID("70968068-dba5-406c-8e26-09b548da0e4b"), "nexus7itu01")
+    map_uuid_to_label(UUID("6561431f-d4c1-4e0f-9489-ab1190341fb7"), "nexus7itu02")
+
+    map_uuid_to_label(UUID("92cf5840-af59-400c-ab72-bab3dcdf7818"), "motoeitu01")
+    map_uuid_to_label(UUID("93e8a1cc-321f-4fa9-8c3c-46928668e45d"), "motoeitu02")
