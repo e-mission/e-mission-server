f8167370bee1906f8c4f7f224ab02bc991ef0bf3
Jack Sullivan <jack@Sullivan-Family-iMac.local>
2017-12-23 12:24:15 -0800

    updateTiers

--- emission/core/wrapper/tiersys.py
+++ emission/core/wrapper/tiersys.py
@@ -4,24 +4,37 @@ class TierSys:
     self.tiers = {}
     for i in range(1, 6):
       self.addTier(i)
-
-  def updateTiers(self):
     
   def addTier(self, rank):
       self.tiers[rank] = Tier(rank)
 
-  def computeRanks(self, n):
+  def computeRanks(self, last_ts, n):
     #TODO: FINISH
-      """get all users from DB
+      """
+      Get all users from DB
       sort them by carbon metric
       compute percentiles
       set boundaries
-      return list of n lists for n ranks"""
+      return list of n lists for n ranks, 
+      with the tiers sorted in descending order."""
       ret = []
       num_users = #GET NUM USERS
       boundary = int(round(num_users / n)) #rounds down to nearest boundary, make first tier have all extra remainder
-  def computeCarbon(self, uuid):
+  
+  def computeCarbon(self, uuid, last_ts):
       """
+      Computers carbon metric for specified user.
+      Formula is (Actual CO2 + penalty) / distance travelled
+      """
+      carbon_val = 
+      penalty_val = self.computePenalty(t_mode)
+      dist_travelled = 
+      return (carbon_val + penalty_val) / dist_travelled
+
+  def computePenalty(self, t_mode):
+      """
+      Linear penalty functions are created depending on
+      transportation mode:
       car: 50 mile threshold
       bus: 25 mile threshold
       cycling: 5 mile threshold
@@ -31,3 +44,11 @@ class TierSys:
       each trip, aggregate carbon metric
       values to get their total penalty
       """
+
+  def updateTiers(self, last_ts):
+      updated_user_tiers = self.computeRanks(last_ts, 5)
+      for rank in range(1, len(updated_user_tiers) + 1):
+        self.addTier(rank)
+        tier_users = updated_user_tiers[rank-1]
+        self.tiers[rank].setUsers(tier_users)
+
