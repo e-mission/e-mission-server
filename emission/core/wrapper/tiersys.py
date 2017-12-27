3c2f2fc1139c7cca0e6c4397fda2ba58ef7ae8a1
Jack Sullivan <jack.sullivan@gmail.com>
2017-12-26 21:58:36 -0800

    computeCarbon functionality

--- emission/core/wrapper/tiersys.py
+++ emission/core/wrapper/tiersys.py
@@ -1,3 +1,8 @@
+import emission.core.get_database as edb
+import pandas as pd
+from uuid import UUID
+import emission.storage.timeseries.abstract_timeseries as esta
+
 class TierSys:
   def __init__(self, tsid):
     self.tsid = tsid
@@ -17,15 +22,32 @@ class TierSys:
       set boundaries
       return list of n lists for n ranks, 
       with the tiers sorted in descending order."""
-      ret = []
-      num_users = #GET NUM USERS
-      boundary = int(round(num_users / n)) #rounds down to nearest boundary, make first tier have all extra remainder
-  
+      user_carbon_map = {} # Map from user_id to carbon val.
+      num_users = len(all_users.iloc)
+      all_users = pd.DataFrame(list(edb.get_uuid_db().find({}, {"user_email":1, "uuid": 1, "_id": 0})))
+      for i in range(1, num_users+1):
+          user_id = all_users.iloc[i].UUID
+          user_carbon_map[user_id] = self.computeCarbon(user_id, last_ts)
+
+      # Sort and partition users by carbon metric.    
+      user_carbon_tuples_sorted = sorted(user_carbon_map.iteritems(), key=lambda (k,v): (v,k)) # Sorted list by value of dict tuples.
+      user_carbon_sorted = [i[0] for i in user_carbon_tuples_sorted] # Extract only the user ids.
+      boundary = int(round(num_users / n)) # Floors to nearest boundary, make first tier have all extra remainder.
+      list_of_tiers = []
+      for i in range(0, n):
+        list_of_tiers[i] = user_carbon_sorted[i*boundary:(i+1)*boundary]
+      return list_of_tiers
+
   def computeCarbon(self, uuid, last_ts):
       """
       Computers carbon metric for specified user.
       Formula is (Actual CO2 + penalty) / distance travelled
       """
+      ts = esta.TimeSeries.get_time_series(user_id)
+
+      # Get all cleaned trips for the user.
+      entry_it = ts.find_entries(["analysis/cleaned_trip"], time_query=None)
+
       carbon_val = 
       penalty_val = self.computePenalty(t_mode)
       dist_travelled = 
