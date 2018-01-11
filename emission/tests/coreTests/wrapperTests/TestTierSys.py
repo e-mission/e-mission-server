b7a345da41dd15ca3f2cb30884d9083010b6e089
AndyY <yan.andy4@gmail.com>
2018-01-10 17:50:34 -0800

    fixed carbonTest

--- emission/tests/coreTests/wrapperTests/TestTierSys.py
+++ emission/tests/coreTests/wrapperTests/TestTierSys.py
@@ -28,10 +28,10 @@ class TestTierSys(unittest.TestCase):
       IN_VEHICLE = 0, BICYCLING = 1, ON_FOOT = 2, STILL = 3, UNKNOWN = 4, TILTING = 5, WALKING = 7, RUNNING = 8
       """
       ts = TierSys(0)
-      d = {'sensed_mode': [0, 1, 0, 2, 3, 4, 5, 6, 7, 8, 1], 'distance': [3.32, 4.45, 0.63, 1.214, 5.43, 0, 1.3, 65.2, 32.2, 31.3, 8.31]}
-      #should be 37.5 - 3.32 + 37.5 - 0.63 + 5 - 4.45 + 0...
+      d = {'sensed_mode': [0, 1, 0, 2, 3, 4, 5, 6, 7, 8, 1], 'distance': [3320, 4450, 630, 12140, 5430, 0, 1300, 65200, 32200, 31300, 8310]}
       penalty_df = pd.DataFrame(data=d)
-      self.assertEqual(ts.computePenalty(penalty_df), 71.6, "Many inputs penalty is not 71.6")
+      penalty = (37.5 * 1609.344/1000) - 3.320 + (37.5 * 1609.344/1000) - 0.630
+      self.assertEqual(ts.computePenalty(penalty_df), penalty, "Many inputs penalty is not correct")
 
       d = {'sensed_mode': [], 'distance': []}
       penalty_df = pd.DataFrame(data=d)
@@ -57,13 +57,29 @@ class TestTierSys(unittest.TestCase):
   def testComputeCarbon(self):
       etc.setupRealExample(self, "emission/tests/data/real_examples/shankari_2015-07-22")
       etc.runIntakePipeline(self.testUUID)
+      #total dist: 51630.268529
+      #carbonfootprint:
+      #penalty:
+      tiersys = TierSys(0)
+      time = arrow.Arrow(2010,5,1).timestamp
 
-      self.ts = esta.TimeSeries.get_time_series(self.testUUID)
-      cs_df = self.ts.get_data_df("analysis/cleaned_section", time_query=None)
-      pd.set_option('display.max_columns', None)
-      print(cs_df.shape)
-      print(cs_df[['sensed_mode', 'distance']])
+      totalDist = 1354.505326 + 976.425010 + 7099.545921 + 378.832636 + \
+      3435.018524 + 1243.291266 + 7854.830970 + 703.250545 + 447.809170 + \
+      1023.561199 + 1149.518965 + 7946.758593 + 603.111034 + 885.495073 + \
+      1101.908416 + 1190.394822 + 1263.283264 + 7365.816160 + 1725.298455 + \
+      887.985382 + 2993.627798
+      footprint = float((92 + 287)/2)/float(1609) * float((7099.545921 + 7854.830970 + 7946.758593 + 7365.816160))/float(1000)
+      penalty = float(37.5*4*1609.344)/float(1000) - float((7099.545921 + 7854.830970 + 7946.758593 + \
+      7365.816160))/float(1000)
 
+      ts = esta.TimeSeries.get_time_series(self.testUUID)
+      cs_df = ts.get_data_df("analysis/cleaned_section")
+
+      self.assertEqual(int(tiersys.computeFootprint(cs_df) * 10000), int(footprint * 10000), "footprint value is not correct")
+      self.assertEqual(int(tiersys.computePenalty(cs_df) * 10000), int(penalty * 10000), "penalty value is not correct")
+
+      val = float(footprint + penalty)/float(totalDist)
+      self.assertEqual(int(tiersys.computeCarbon(self.testUUID, time) * 10000), int(val * 10000), "computeCarbon Fails")
       return
 
   def testGetLatest(self):
@@ -104,7 +120,6 @@ class TestTierSys(unittest.TestCase):
       for a in TierSys.getLatest():
           self.assertEqual(a['tiers'][0], {'rank': 1, 'uuids': [4, 5, 6]}, "Did not get latest Tier")
           self.assertEqual(a['tiers'][1], {'rank': 2, 'uuids': [1, 2, 3]}, "Did not get latest Tier")
-
       return
 
 if __name__ == '__main__':
