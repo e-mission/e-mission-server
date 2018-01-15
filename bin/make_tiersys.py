8e3c949e2d24dfbbd86f0dd35ca8cf4a0164ed66
AndyY <yan.andy4@gmail.com>
2018-01-15 15:19:49 -0800

    add make tiersys script

---
+++ bin/make_tiersys.py
@@ -0,0 +1,8 @@
+from emission.core.wrapper.tiersys import TierSys
+import arrow
+
+if __name__ == '__main__':
+    ts = TierSys(0)
+    time = arrow.utcnow().shift(weeks=-1)
+    ts.updateTiers(time)
+    ts.saveTiers()
