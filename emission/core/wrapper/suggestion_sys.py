dde4116e2de1b52a79970806e9ce4e92d2c965f4
jesbu1 <jessezhang5@gmail.com>
2018-01-18 14:20:24 -0800

    Suggestionsys improvements

--- emission/core/wrapper/suggestion_sys.py
+++ emission/core/wrapper/suggestion_sys.py
@@ -1,7 +1,8 @@
 import emission.storage.timeseries.abstract_timeseries as esta
 import pandas as pd
 from uuid import UUID
-
+import urllib
+ACCESS_TOKEN = 'AIzaSyAbnpsty2SAzEX9s1VVIdh5pTHUPMjn3lQ' #GOOGLE MAPS ACCESS TOKEN
 """
 def calculate_suggestions():
     #For each person, create their most recent suggestion
@@ -12,6 +13,19 @@ def calculate_suggestions():
         user_id = row['uuid']
         user_carbon_map[user_id] = self.computeCarbon(user_id, TierSys.getLatest()['created_at'])
 """
+
+def build_URL(search_text='',types_text=''):
+    """
+    Builds the url for a Google Maps business request from latitude and longitude
+    """
+    base_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+    key_string = '?key=' + ACCESS_TOKEN
+    query_string = '&query=' + urllib.parse.quote(search_text)
+    type_string = ''
+    if types_text != '':
+        type_string = '&types='+urllib.parse.quote(types_text)
+    url = base_url+key_string+query_string+type_string
+    return url
 
 def calculate_single_suggestion(uuid):
     #Given a single UUID, create a suggestion for them
@@ -25,7 +39,13 @@ def calculate_single_suggestion(uuid):
     #Go in reverse order because we check by most recent trip
     for i in range(len(cleaned_sections), -1, -1):
         distance_in_miles = cleaned_sections.iloc[i]["distance"] * 0.000621371
-        mode = cleaned-sections.iloc[i]["sensed_mode"]
+        mode = cleaned_sections.iloc[i]["sensed_mode"]
+        start_loc = cleaned_sections.iloc[i]["start_loc"]["coordinates"]
+        start_lat = start_loc[0]
+        start_lon = start_loc[1]
+        end_loc = cleaned_sections.iloc[i]["end_loc"]["coordinates"]
+        end_lat = end_loc[0]
+        end_lon = end_loc[1]
         if mode == 0 and distance => 5 and distance <= 15:
             #Suggest bus if it is car and distance between 5 and 15
             #TODO: Change ret_obj and figure out how to change lat and lon to places
