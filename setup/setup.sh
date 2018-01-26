c5799449d456e7431478d761eb7c7df30fe5d9c0
Rachel <omrachel@gmail.com>
2018-01-25 22:53:58 -0800

    Update setup.sh
    adding Miniconda3 path

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,3 @@
-conda env update --name emission --file setup/environment36.yml
+conda env update -p="C:\Users\rachel_om\Miniconda3\python.exe" --name emission --file setup/environment36.yml
 source activate emission
 pip install six --upgrade
