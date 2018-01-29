74a8981b7937ce8e9b0e82567e409d27368ffbf4
jesbu1 <jessezhang5@gmail.com>
2018-01-28 17:58:39 -0800

    fixed typo in cfc webapp

--- emission/net/api/cfc_webapp.py
+++ emission/net/api/cfc_webapp.py
@@ -47,6 +47,7 @@ from emission.core.wrapper.client import Client
 from emission.core.wrapper.user import User
 from emission.core.wrapper.tiersys import TierSys
 from emission.core.get_database import get_uuid_db, get_mode_db
+import emission.core.wrapper.suggestion_sys as suggsys
 import emission.core.wrapper.motionactivity as ecwm
 import emission.storage.timeseries.timequery as estt
 import emission.storage.timeseries.tcquery as esttc
