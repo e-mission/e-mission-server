a25a803c344d66348fead2f494ef9dd770820755
Jesse Zhang <jessezhang@berkeley.edu>
2018-02-20 20:17:27 +0000

    Fixed spacing

--- emission/core/wrapper/polarbear.py
+++ emission/core/wrapper/polarbear.py
@@ -69,9 +69,9 @@ def getAllBearsInTier(user_id):
 		if uuid != user_id:
 			userattrs = getPolarBearattr(uuid)
 		if userattrs != None:
-            currUsername = userattrs['username']
-            allUsers['otherBears'][currUsername] = {}
-            allUsers['otherBears'][currUsername]['username'] = currUsername
+			currUsername = userattrs['username']
+			allUsers['otherBears'][currUsername] = {}
+			allUsers['otherBears'][currUsername]['username'] = currUsername
 			allUsers['otherBears'][currUsername]['happiness'] = userattrs['happiness']
 			allUsers['otherBears'][currUsername]['size'] = userattrs['size']
 		else:
