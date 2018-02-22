bf4cfaaba5bbeb2fffe1e9b227fadb78d15605bf
K. Shankari <shankari@eecs.berkeley.edu>
2018-02-22 06:00:29 -0800

    Change the default configuration to firebase
    Since it is the only one that we currently support

--- emission/tests/netTests/TestPush.py
+++ emission/tests/netTests/TestPush.py
@@ -47,15 +47,14 @@ class TestPush(unittest.TestCase):
         shutil.copyfile("%s.sample" % self.push_conf_path,
                         self.push_conf_path)
         open(self.push_conf_path, "w").write(json.dumps({
-            "provider": "ionic",
+            "provider": "firebase",
             "server_auth_token": "firebase_api_key"
         }))
         logging.debug("Finished setting up %s" % self.push_conf_path)
         logging.debug("Current values are %s" % json.load(open(self.push_conf_path)))
 
     def tearDown(self):
-        # os.remove(self.push_conf_path)
-        pass
+        os.remove(self.push_conf_path)
 
     def testGetInterface(self):
         #import emission.net.ext_service.push.notify_interface_impl.firebase as pnif
