236f89c9b721e0f7ad435abbb8f52a6bd0fe67cd
K. Shankari <shankari@eecs.berkeley.edu>
2018-02-24 23:45:39 -0800

    Add a new script to remove only the inference modes
    So that we can experiment with other options such as different feature
    selection, etc

--- emission/analysis/classification/inference/mode/pipeline.py
+++ emission/analysis/classification/inference/mode/pipeline.py
@@ -23,6 +23,7 @@ import emission.core.get_database as edb
 import emission.core.wrapper.entry as ecwe
 import emission.core.wrapper.modeprediction as ecwm
 import emission.core.wrapper.motionactivity as ecwma
+import emission.core.wrapper.pipelinestate as ecwp
 
 from uuid import UUID
 
@@ -48,7 +49,28 @@ def predict_mode(user_id):
         logging.exception("Error while inferring modes, timestamp is unchanged")
         epq.mark_mode_inference_failed(user_id)
 
-# Delete the objects created by this pipeline step
+# Delete the objects created by this pipeline step (across users)
+def del_all_objects(is_dry_run):
+    del_query = {}
+    del_query.update({"metadata.key": {"$in": ["inference/prediction", "analysis/inferred_section"]}})
+    logging.info("About to delete %d entries" 
+        % edb.get_analysis_timeseries_db().find(del_query).count())
+    logging.info("About to delete entries with keys %s" 
+        % edb.get_analysis_timeseries_db().find(del_query).distinct("metadata.key"))
+
+    del_pipeline_query = {"pipeline_stage": ecwp.PipelineStages.MODE_INFERENCE.value}
+    logging.info("About to delete pipeline entries for stage %s" %
+        ecwp.PipelineStages.MODE_INFERENCE)
+
+    if is_dry_run:
+        logging.info("this is a dry-run, returning from del_objects_after without modifying anything")
+    else:
+        result = edb.get_analysis_timeseries_db().delete_many(del_query)
+        logging.info("this is not a dry-run, result of deleting analysis entries is %s" % result.raw_result)
+        result = edb.get_pipeline_state_db().delete_many(del_pipeline_query)
+        logging.info("this is not a dry-run, result of deleting pipeline state is %s" % result.raw_result)
+
+# Delete the objects created by this pipeline step (for a particular user)
 def del_objects_after(user_id, reset_ts, is_dry_run):
     del_query = {}
     # handle the user
@@ -58,6 +80,12 @@ def del_objects_after(user_id, reset_ts, is_dry_run):
     # all objects inserted here have start_ts and end_ts and are trip-like
     del_query.update({"data.start_ts": {"$gt": reset_ts}})
     logging.debug("After all updates, del_query = %s" % del_query)
+
+    reset_pipeline_query = {"pipeline_stage": ecwp.PipelineStages.MODE_INFERENCE.value}
+    reset_pipeline_update = {'$set': {'last_processed_ts': reset_ts + FUZZ_FACTOR}}
+    logging.info("About to reset stage %s to %s" 
+        % (ecwp.PipelineStages.MODE_INFERENCE, reset_ts))
+    
 
     logging.info("About to delete %d entries" 
         % edb.get_analysis_timeseries_db().find(del_query).count())
