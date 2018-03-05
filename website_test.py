397f9b1fe6d14a1fe5bdcce43952a68964191099
jesbu1 <jessezhang5@gmail.com>
2018-03-05 12:14:30 -0800

    making script more generalizeable

--- website_test.py
+++ website_test.py
@@ -1,15 +1,17 @@
 """
 must install selenium (pip install selenium)
-also install PhantomJS (brew/npm), and replace with executable_path
+also install PhantomJS (brew/npm), and replace with path
 """
 from selenium import webdriver
 info, control, emotion = 0, 0, 0
 trials = 1000
 path = '/Users/Jesse/Desktop/node_modules/phantomjs/lib/phantom/bin/phantomjs'
 driver = webdriver.PhantomJS(executable_path=path)
+link = 'https://tripaware.eecs.berkeley.edu'
+element_id = 'groupLink'
 for _ in range(trials):
-    driver.get('https://tripaware.eecs.berkeley.edu')
-    p_element = driver.find_element_by_id(id_='groupLink')
+    driver.get(link)
+    p_element = driver.find_element_by_id(id_ = element_id)
     if 'urap2017information' in p_element.get_attribute('href'):
         info += 1
     elif 'urap2017emotion' in p_element.get_attribute('href'):
