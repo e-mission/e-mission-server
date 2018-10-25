69765f66a73003e794a5e051b48480d76fa5aba9
Alvin <alvin.ghouas@gmail.com>
2018-10-25 12:42:54 -0700

    Add comments

--- emission/simulation/client.py
+++ emission/simulation/client.py
@@ -1,8 +1,6 @@
 from abc import ABC, abstractmethod
 from emission.simulation.fake_user import FakeUser
 from emission.simulation.error import AddressNotFoundError
-#TODO: the trip planner should be kept in the FakeUser class, not in the client
-from emission.net.ext_service.otp.otp import OTP
 
 class Client(ABC):
     def __init__(self):
@@ -18,18 +16,15 @@ class Client(ABC):
 class EmissionFakeDataGenerator(Client):
     def __init__(self):
         self._user_factory = FakeUser
-        self._trip_planer_client = OTP
 
     def create_fake_user(self, config):
-        #parse the config
+        #TODO: parse the config object
         return self._user_factory(config)
 
     def _parse_user_config(self, config):
-
-        #check that config has initial state location and that the address is in the addresses list 
-
+        #TODO: This function shoudl be used to parser user config object and check that the paramaters are valid.
         try: 
-            addresses = config['addresses']
+            locations = config['locations']
         except KeyError:
             print("You must specify a set of addresses")
             raise AddressNotFoundError
@@ -40,6 +35,6 @@ class EmissionFakeDataGenerator(Client):
         #        message = ("%s, is not supported by the Trip Planer", address) 
         #        raise AddressNotFoundError(message, address)
 
-        #check that all teh transition probabilites for every address ads up to one
+        #check that all teh transition probabilites for every address adds up to one
 
         
