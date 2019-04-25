bb2659acdb85be21ee8a8f9f99de42b54e87df06
K. Shankari <shankari@eecs.berkeley.edu>
2019-04-25 09:45:48 -0700

    Make it easier to test individual candidate functions
    Enhance the test harness to compare the performance of various implementations
    of the same algorithm. In particular:
    - add a list of candidate implementations for each step in the pipeline
    - allow users to choose which candidates they want to test
    - default to testing all candidates if the user does not specify anything
    
    We also keep track of the results for each step and print out a comparison at the end
    
    Also, refactor the existing suggestion system to allow us to compare the
    various choices (e.g. nominatim v/s google v/s yelp?) for reverse geocoding and
    search by name v/s search by address for category lookup v/s something even
    more awesome?
    
    Testing done:
    
    With no categories specified (which also ensures that the refactoring of the
    suggestion system did not did not have a regression by testing
    `category_of_business_nominatim`, which returned the same result as last night)
    
    ```
    ./e-mission-py.bash emission/integrationTests/suggestionsys/algorithm_test_harness.py category_of_business_nominatim
    INFO:root:candidate: category_of_business_nominatim, accuracy = 4.0
    INFO:root:candidate: category_from_name_wrapper, accuracy = 6.0
    INFO:root:candidate: category_from_address_wrapper, accuracy = 4.0
    INFO:root:candidate: category_of_business_awesome, accuracy = 6.0
    ```
    
    With categories specified
    
    ```
    ./e-mission-py.bash emission/integrationTests/suggestionsys/algorithm_test_harness.py category_of_business_nominatim --candidates category_of_business_awesome
    INFO:root:candidate: category_of_business_awesome, accuracy = 6.0
    ```

--- emission/integrationTests/suggestionsys/algorithm_test_harness.py
+++ emission/integrationTests/suggestionsys/algorithm_test_harness.py
@@ -7,30 +7,30 @@ import json
 
 import emission.core.wrapper.suggestion_sys as sugg
 
-def test_find_destination_business(params):
+def test_find_destination_business(cfn, params):
     lat, lon = sugg.geojson_to_lat_lon_separated(params["loc"])
-    result = sugg.find_destination_business(lat, lon)
+    result = cfn(lat, lon)
     return list(result)
 
-def test_category_of_business_nominatim(params):
+def test_category_of_business_nominatim(cfn, params):
     lat, lon = sugg.geojson_to_lat_lon_separated(params["loc"])
-    result = sugg.category_of_business_nominatim(lat, lon)
+    result = cfn(lat, lon)
     return result
 
-def test_calculate_yelp_server_suggestion_for_locations(params):
+def test_calculate_yelp_server_suggestion_for_locations(cfn, params):
     start_loc = params["start_loc"]
     end_loc = params["end_loc"]
     distance_in_miles = sugg.distance(sugg.geojson_to_latlon(start_loc), sugg.geojson_to_latlon(end_loc))
     distance_in_meters = distance_in_miles / 0.000621371
     logging.debug("distance in meters = %s" % distance_in_meters)
     # calculation function expects distance in meters
-    result = sugg.calculate_yelp_server_suggestion_for_locations(start_loc, end_loc, distance_in_meters)
+    result = cfn(start_loc, end_loc, distance_in_meters)
     return result.get('businessid', None)
 
-def test_single_instance(test_fn, instance):
+def test_single_instance(test_fn, cfn, instance):
     logging.debug("-----" + instance["test_name"] + "------")
     param = instance["input"]
-    result = test_fn(param)
+    result = test_fn(cfn, param)
     exp_output = instance["output"]
     if exp_output == result:
         return True
@@ -49,6 +49,23 @@ TEST_WRAPPER_MAP = {
     "calculate_yelp_server_suggestion_for_locations": test_calculate_yelp_server_suggestion_for_locations
 }
 
+CANDIDATE_ALGORITHMS = {
+    "find_destination_business": [
+        sugg.find_destination_business_google,
+        sugg.find_destination_business_yelp,
+        sugg.find_destination_business_nominatim
+    ],
+    "category_of_business_nominatim": [
+        sugg.category_of_business_nominatim,
+        sugg.category_from_name_wrapper,
+        sugg.category_from_address_wrapper,
+        sugg.category_of_business_awesome
+    ],
+    "calculate_yelp_server_suggestion_for_locations": [
+        sugg.calculate_yelp_server_suggestion_for_locations
+    ]
+}
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -57,6 +74,8 @@ if __name__ == '__main__':
     parser.add_argument("algorithm",
         choices=TEST_WRAPPER_MAP.keys(),
         help="the algorithm to test")
+    parser.add_argument("--candidates", nargs="*",
+        help="the candidate implementations of the algorithm; see suggestion_sys for details")
     parser.add_argument("-f", "--infile",
         help="the file that has the inputs and expected outputs. default is emission/integrationTests/suggestionsys/{algorithm}.dataset.json")
 
@@ -73,17 +92,39 @@ if __name__ == '__main__':
     test_fn = TEST_WRAPPER_MAP[args.algorithm]
     logging.info("Mapped algorithm %s -> %s" % (args.algorithm, test_fn))
 
-    dataset = json.load(open(args.infile))
-    successfulTests = 0
-    failedTests = 0
-    for instance in dataset:
-        result = test_single_instance(test_fn, instance)
-        if result:
-            successfulTests = successfulTests + 1
-        else:
-            failedTests = failedTests + 1
-        logging.debug("After instance %s, successfulTests = %d, failedTests = %d"
-            % (instance["test_name"], successfulTests, failedTests))
+    all_candidate_fn_list = CANDIDATE_ALGORITHMS[args.algorithm]
+    all_candidate_name_list = [c.__name__ for c in CANDIDATE_ALGORITHMS[args.algorithm]]
+    if args.candidates is None:
+        args.candidates = all_candidate_name_list
 
-    logging.info("Test complete, overall accuracy = %s " % ((successfulTests * 100) / (successfulTests + failedTests)))
+    logging.info("specified candidates = %s" % args.candidates)
+    invalid_candidate_fn_names = [c for c in args.candidates
+        if c not in all_candidate_name_list]
+    if len(invalid_candidate_fn_names) > 0:
+        print("Did not find candidate algorithms %s" % invalid_candidate_fn_names)
+        exit(1)
+
+    candidate_fns = [fn for fn in all_candidate_fn_list if fn.__name__ in args.candidates]
+    logging.info("Comparing candidate functions %s" % candidate_fns)
+
+    dataset = json.load(open(args.infile))
+    cfn2resultlist= []
+    for cfn in candidate_fns:
+        successfulTests = 0
+        failedTests = 0
+        for instance in dataset:
+            result = test_single_instance(test_fn, cfn, instance)
+            if result:
+                successfulTests = successfulTests + 1
+            else:
+                failedTests = failedTests + 1
+            logging.debug("For candidate %s, after instance %s, successfulTests = %d, failedTests = %d"
+                % (cfn.__name__, instance["test_name"], successfulTests, failedTests))
+        cfn2resultlist.append((cfn.__name__, successfulTests, failedTests))
+        logging.info("Testing candidate %s complete, overall accuracy = %s " %
+            (cfn.__name__, (successfulTests * 100) / (successfulTests + failedTests)))
+
+    logging.info("Test complete, comparison results = ")
+    for cfn_name, successfulTests, failedTests in cfn2resultlist:
+        logging.info("candidate: %s, accuracy = %s" % (cfn_name, (successfulTests * 100) / (successfulTests + failedTests)))
 
