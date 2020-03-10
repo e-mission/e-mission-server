a82fbedd5a3190c1f8aaf73a2bd5c3256d5b37dd
K. Shankari <shankari@eecs.berkeley.edu>
2020-03-09 18:33:35 -0700

    Fix the setup and teardown tests
    To disable habitica since we have shutdown the server

--- setup/teardown_tests.sh
+++ setup/teardown_tests.sh
@@ -4,6 +4,6 @@ echo "Removing environment from "${CONDA_TEMP_PREFIX}
 source deactivate ${CONDA_TEMP_PREFIX}
 conda env remove --yes --prefix ${CONDA_TEMP_PREFIX} 
 unset CONDA_TEMP_PREFIX
-rm conf/net/int_service/giles_conf.json
-rm conf/net/ext_service/habitica.json
+# rm conf/net/ext_service/habitica.json
 rm conf/net/ext_service/push.json
+rm seed_model.json
