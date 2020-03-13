85c9150f5acb520d20d8e96f409534e5bcc20894
K. Shankari <shankari@eecs.berkeley.edu>
2020-03-13 08:38:30 -0700

    Switch to conda activate/deactivate
    + remove python and apt-update to reduce space used

--- setup/setup_tests.sh
+++ setup/setup_tests.sh
@@ -6,7 +6,7 @@ echo "Make sure you are running this with conda >=4.2"
 echo "Check with 'conda -V'"
 echo "Upgrade with 'conda update conda' from the *root* environment"
 conda env create --prefix ${CONDA_TEMP_PREFIX} --file setup/environment36.nomkl.yml
-source activate ${CONDA_TEMP_PREFIX}
+conda activate ${CONDA_TEMP_PREFIX}
 # python bin/deploy/habitica_conf.py
 python bin/deploy/push_conf.py
 python bin/deploy/model_copy.py
--- setup/teardown_tests.sh
+++ setup/teardown_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Removing environment from "${CONDA_TEMP_PREFIX}
-source deactivate ${CONDA_TEMP_PREFIX}
+conda deactivate ${CONDA_TEMP_PREFIX}
 conda env remove --yes --prefix ${CONDA_TEMP_PREFIX} 
 unset CONDA_TEMP_PREFIX
 # rm conf/net/ext_service/habitica.json
