e66fb3857c5286c027d26e52840cdae67e528e03
K. Shankari <shankari@eecs.berkeley.edu>
2020-03-16 23:06:22 -0700

    Remove the prefix creation for every run
    Since we are moving to a model where we run the tests in separate containers anyway

--- setup/setup_tests.sh
+++ setup/setup_tests.sh
@@ -1,15 +1,8 @@
 #!/bin/bash 
 
-export CONDA_TEMP_PREFIX=$(mktemp -d)
-echo "Deploying environment in "${CONDA_TEMP_PREFIX}
-echo "Make sure you are running this with conda >=4.2"
-echo "Check with 'conda -V'"
-echo "Upgrade with 'conda update conda' from the *root* environment"
-
-
 echo "Setting up blank environment"
-conda create --prefix ${CONDA_TEMP_PREFIX} python=3.6
-source activate ${CONDA_TEMP_PREFIX}
+conda create --name emissiontest python=3.6
+source activate emissiontest
 
 echo "Downloading packages"
 curl -o /tmp/cachetools-2.1.0-py_0.tar.bz2 -L https://anaconda.org/conda-forge/cachetools/2.1.0/download/noarch/cachetools-2.1.0-py_0.tar.bz2
@@ -22,7 +15,7 @@ curl -o /tmp/pyasn1-modules-0.2.7-py_0.tar.bz2 -L https://anaconda.org/conda-for
 echo "Installing manually downloaded packages"
 conda install /tmp/*.bz2
 
-conda env update --file setup/environment36.yml
+conda env update --name emissiontest --file setup/environment36.yml
 # python bin/deploy/habitica_conf.py
 python bin/deploy/push_conf.py
 python bin/deploy/model_copy.py
