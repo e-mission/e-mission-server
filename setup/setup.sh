def5d54073840110b6f8fede3e45afe81c963c0e
K. Shankari <shankari@eecs.berkeley.edu>
2020-03-16 22:22:51 -0700

    Use source activate to be consistent

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,7 +8,7 @@
 
 echo "Setting up blank environment"
 conda create --name emission python=3.6
-conda activate emission
+source activate emission
 
 echo "Downloading packages"
 curl -o /tmp/cachetools-2.1.0-py_0.tar.bz2 -L https://anaconda.org/conda-forge/cachetools/2.1.0/download/noarch/cachetools-2.1.0-py_0.tar.bz2
