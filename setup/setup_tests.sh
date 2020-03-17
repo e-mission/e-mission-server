f37a5c40f700b98c70b6513ab4c08c08f2783602
K. Shankari <shankari@eecs.berkeley.edu>
2020-03-16 20:33:59 -0700

    Manually install some packages from conda-forge
    To workaround
    https://github.com/e-mission/e-mission-docs/issues/511
    
    The solution is in
    https://github.com/e-mission/e-mission-docs/issues/511#issuecomment-599669134

--- setup/setup_tests.sh
+++ setup/setup_tests.sh
@@ -5,8 +5,24 @@ echo "Deploying environment in "${CONDA_TEMP_PREFIX}
 echo "Make sure you are running this with conda >=4.2"
 echo "Check with 'conda -V'"
 echo "Upgrade with 'conda update conda' from the *root* environment"
-conda env create --prefix ${CONDA_TEMP_PREFIX} --file setup/environment36.yml
+
+
+echo "Setting up blank environment"
+conda create --prefix ${CONDA_TEMP_PREFIX} python=3.6
 source activate ${CONDA_TEMP_PREFIX}
+
+echo "Downloading packages"
+curl -o /tmp/cachetools-2.1.0-py_0.tar.bz2 -L https://anaconda.org/conda-forge/cachetools/2.1.0/download/noarch/cachetools-2.1.0-py_0.tar.bz2
+curl -o /tmp/geojson-2.3.0-py_0.tar.bz2 -L https://anaconda.org/conda-forge/geojson/2.3.0/download/noarch/geojson-2.3.0-py_0.tar.bz2
+curl -o /tmp/jsonpickle-0.9.6-py_1.tar.bz2 -L https://anaconda.org/conda-forge/jsonpickle/0.9.6/download/noarch/jsonpickle-0.9.6-py_1.tar.bz2
+curl -o /tmp/more-itertools-8.2.0-py_0.tar.bz2 -L https://anaconda.org/conda-forge/more-itertools/8.2.0/download/noarch/more-itertools-8.2.0-py_0.tar.bz2
+curl -o /tmp/pyasn1-0.4.8-py_0.tar.bz2 -L https://anaconda.org/conda-forge/pyasn1/0.4.8/download/noarch/pyasn1-0.4.8-py_0.tar.bz2
+curl -o /tmp/pyasn1-modules-0.2.7-py_0.tar.bz2 -L https://anaconda.org/conda-forge/pyasn1-modules/0.2.7/download/noarch/pyasn1-modules-0.2.7-py_0.tar.bz2
+
+echo "Installing manually downloaded packages"
+conda install /tmp/*.bz2
+
+conda env update --file setup/environment36.yml
 # python bin/deploy/habitica_conf.py
 python bin/deploy/push_conf.py
 python bin/deploy/model_copy.py
