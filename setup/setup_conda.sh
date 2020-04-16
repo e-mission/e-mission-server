82448f5261d1b17f1dab8189e6b7b7cce5cfa01b
K. Shankari <shankari@eecs.berkeley.edu>
2020-04-16 15:49:03 -0700

    Add exit codes so we can see why this is failing

--- setup/setup_conda.sh
+++ setup/setup_conda.sh
@@ -2,6 +2,7 @@ EXP_CONDA_VER=4.5.12
 
 PLATFORM=$1
 echo "Installing for platform $PLATFORM"
+echo $?
 
 if [ -z $PLATFORM ]; then
     echo "Usage: setup_conda.sh <platform>"
@@ -9,10 +10,16 @@ if [ -z $PLATFORM ]; then
     echo "   For Windows, manually download and install https://repo.anaconda.com/miniconda/Miniconda3-$EXP_CONDA_VER-Windows-x86_64.exe"
 else
     curl -o miniconda.sh -L https://repo.continuum.io/miniconda/Miniconda3-$EXP_CONDA_VER-$PLATFORM.sh;
+    echo $?
     bash miniconda.sh -b -p $HOME/miniconda
+    echo $?
     source "$HOME/miniconda/etc/profile.d/conda.sh"
+    echo $?
     hash -r
+    echo $?
     conda config --set always_yes yes --set changeps1 no
+    echo $?
     # Useful for debugging any issues with conda
     conda info -a
+    echo $?
 fi
