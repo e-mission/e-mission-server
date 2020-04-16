6a7a2f7ad4494aff8de27ccc7678b1843faa4906
shankari <shankari@eecs.berkeley.edu>
2020-04-16 16:51:02 -0700

    Multiple fixes to get conda setup working in more environments (#741)
    * Multiple fixes to get conda setup working in more environments
    
    My personal laptop had a version of conda even older than the docker container.
    So old that it only supported `source activate` and not `conda activate`.
    So we used `source activate` in the setup script and checked for the CI
    variable before using it in `conda activate`.
    
    This:
    - supports `setup_conda` on multiple platforms
    - upgrades all setup scripts to use `conda install`
    - stops exiting from `check_for_conda` so it doesn't kill the shell
    - updates the manual install test to also include osx, and to add osx to the
      list of oses that we test against
        - also pass in the platform correctly per OS so that the installation will
          happen correctly
    
    * Mongodb install action only works on ubuntu
    
    So we test install only ubuntu + osx
    and go back to linux only for install + tests
    
    This should also catch any inconsistencies between the dev setup and the test
    setup scripts (similar to https://github.com/e-mission/e-mission-docs/issues/513#issuecomment-614913783)
    
    * Remove miniconda check since osx doesn't have miniconda preinstalled
    
    * Add exit codes so we can see why this is failing
    
    * Checking to see if the error is with which conda
    
    * Check the conda path only on linux
    
    Since it inexplicably fails on OSX
    + remove exit 1 since it is confusing in source scripts
    
    * Final fixes
    
    - switch to conda on teardown as well
    - actually switch to the environment after installing it to ensure that it exists
    
    * Some more clarity into what we are doing
    
    So that there is more indication of what failed if something fails

--- setup/setup.sh
+++ setup/setup.sh
@@ -9,7 +9,7 @@ source setup/checks/check_for_conda.sh
 
 echo "Setting up blank environment"
 conda create --name emission python=3.6
-source activate emission
+conda activate emission
 
 echo "Downloading packages"
 curl -o /tmp/cachetools-2.1.0-py_0.tar.bz2 -L https://anaconda.org/conda-forge/cachetools/2.1.0/download/noarch/cachetools-2.1.0-py_0.tar.bz2
--- setup/setup_conda.sh
+++ setup/setup_conda.sh
@@ -1,9 +1,18 @@
 EXP_CONDA_VER=4.5.12
 
-wget https://repo.continuum.io/miniconda/Miniconda3-$EXP_CONDA_VER-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
-source "$HOME/miniconda/etc/profile.d/conda.sh"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-# Useful for debugging any issues with conda
-conda info -a
+PLATFORM=$1
+echo "Installing for platform $PLATFORM"
+
+if [ -z $PLATFORM ]; then
+    echo "Usage: setup_conda.sh <platform>"
+    echo "   Platform options are Linux-x86_64, MacOSX-x86_64"
+    echo "   For Windows, manually download and install https://repo.anaconda.com/miniconda/Miniconda3-$EXP_CONDA_VER-Windows-x86_64.exe"
+else
+    curl -o miniconda.sh -L https://repo.continuum.io/miniconda/Miniconda3-$EXP_CONDA_VER-$PLATFORM.sh;
+    bash miniconda.sh -b -p $HOME/miniconda
+    source "$HOME/miniconda/etc/profile.d/conda.sh"
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    # Useful for debugging any issues with conda
+    conda info -a
+fi
