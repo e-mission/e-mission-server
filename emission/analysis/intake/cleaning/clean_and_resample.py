b63d4f090217526997942368e29ace17b0457a6c
K. Shankari <shankari@eecs.berkeley.edu>
2020-05-05 21:18:40 -0700

    Fix one more location

--- emission/analysis/intake/cleaning/clean_and_resample.py
+++ emission/analysis/intake/cleaning/clean_and_resample.py
@@ -31,7 +31,7 @@ import emission.analysis.config as eac
 import emission.storage.decorations.timeline as esdtl
 import emission.storage.decorations.trip_queries as esdtq
 import emission.storage.decorations.analysis_timeseries_queries as esda
-import emission.storage.decorations.local_date_queries as esdl
+import emission.core.wrapper.localdate as ecwld
 import emission.storage.decorations.place_queries as esdp
 
 import emission.storage.timeseries.abstract_timeseries as esta
@@ -609,7 +609,7 @@ def _add_start_point(filtered_loc_df, raw_start_place, ts):
     logging.debug("After copy, new data = %s" % new_first_point_data)
     new_first_point_data["ts"] = new_start_ts
     tz = raw_start_place_enter_loc_entry.data.local_dt.timezone
-    new_first_point_data["local_dt"] = esdl.get_local_date(new_start_ts, tz)
+    new_first_point_data["local_dt"] = ecwld.LocalDate.get_local_date(new_start_ts, tz)
     new_first_point_data["fmt_time"] = arrow.get(new_start_ts).to(tz).isoformat()
     new_first_point = ecwe.Entry.create_entry(curr_first_point.user_id,
                             "background/filtered_location",
@@ -716,7 +716,7 @@ def _overwrite_from_loc_row(filtered_section_data, fixed_loc, prefix):
 
 def _overwrite_from_timestamp(filtered_trip_like, prefix, ts, tz, loc):
     filtered_trip_like[prefix+"_ts"] = float(ts)
-    filtered_trip_like[prefix+"_local_dt"] = esdl.get_local_date(ts, tz)
+    filtered_trip_like[prefix+"_local_dt"] = ecwld.LocalDate.get_local_date(ts, tz)
     filtered_trip_like[prefix+"_fmt_time"] = arrow.get(ts).to(tz).isoformat()
     filtered_trip_like[prefix+"_loc"] = loc
 
@@ -790,7 +790,7 @@ def resample(filtered_loc_df, interval):
     lng_new = lng_fn(ts_new)
     alt_new = altitude_fn(ts_new)
     tz_new = [_get_timezone(ts, tz_ranges_df) for ts in ts_new]
-    ld_new = [esdl.get_local_date(ts, tz) for (ts, tz) in zip(ts_new, tz_new)]
+    ld_new = [ecwld.LocalDate.get_local_date(ts, tz) for (ts, tz) in zip(ts_new, tz_new)]
     loc_new = [gj.Point((lng, lat)) for (lng, lat) in zip(lng_new, lat_new)]
     fmt_time_new = [arrow.get(ts).to(tz).isoformat() for
                         (ts, tz) in zip(ts_new, tz_new)]
