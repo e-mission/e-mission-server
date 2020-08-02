6db3b8c633e21696d11b3c1e93204a9bf1bc1d36
Shankari <shankari@eecs.berkeley.edu>
2020-08-01 17:10:22 -0700

    Upgrade the setup scripts to support multiple instances of miniconda at the same time

--- setup/setup_conda.sh
+++ setup/setup_conda.sh
@@ -1,18 +1,21 @@
-EXP_CONDA_VER=4.5.12
+EXP_CONDA_VER=$1
+PLATFORM=$2
+echo "Installing for version $EXP_CONDA_VER and platform $PLATFORM"
 
-PLATFORM=$1
-echo "Installing for platform $PLATFORM"
-
-if [ -z $PLATFORM ]; then
-    echo "Usage: setup_conda.sh <platform>"
+if [[ -z $EXP_CONDA_VER || -z $PLATFORM ]]; then
+    echo "Usage: setup_conda.sh <version> <platform>"
     echo "   Platform options are Linux-x86_64, MacOSX-x86_64"
     echo "   For Windows, manually download and install https://repo.anaconda.com/miniconda/Miniconda3-$EXP_CONDA_VER-Windows-x86_64.exe"
 else
+    INSTALL_PREFIX=$HOME/miniconda-$EXP_CONDA_VER
+    SOURCE_SCRIPT="$HOME/miniconda-$EXP_CONDA_VER/etc/profile.d/conda.sh"
+    
     curl -o miniconda.sh -L https://repo.continuum.io/miniconda/Miniconda3-$EXP_CONDA_VER-$PLATFORM.sh;
-    bash miniconda.sh -b -p $HOME/miniconda
-    source "$HOME/miniconda/etc/profile.d/conda.sh"
+    bash miniconda.sh -b -p $INSTALL_PREFIX
+    source $SOURCE_SCRIPT
     hash -r
-    conda config --set always_yes yes --set changeps1 no
+    conda config --set always_yes yes
     # Useful for debugging any issues with conda
     conda info -a
+    echo "Successfully installed at $INSTALL_PREFIX. Please run 'source $SOURCE_SCRIPT' in every terminal where you want to use conda" 
 fi
