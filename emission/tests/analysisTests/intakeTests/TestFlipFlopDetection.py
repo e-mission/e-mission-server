2c15ea93f3e27bd7d90b01c930d45ab6fae7842a
Shankari <shankari@eecs.berkeley.edu>
2020-08-18 22:08:08 -0700

    Move out the flip flop detection tests to their own file
    The flip flop detection code is in its own file - why not the tests?
    This also makes it easier to run the tests without loading the real data and running the accuracy filter

---
+++ emission/tests/analysisTests/intakeTests/TestFlipFlopDetection.py
@@ -0,0 +1,109 @@
+import unittest
+import attrdict as ad
+
+# our imports
+import emission.core.wrapper.motionactivity as ecwm
+import emission.analysis.intake.segmentation.section_segmentation_methods.flip_flop_detection as eaissf
+
+# Test imports
+import emission.tests.common as etc
+
+class TestFlipFlopDetection(unittest.TestCase):
+    def test_GetStreakOne(self):
+        ffd = eaissf.FlipFlopDetection([], None)
+        flip_flop_list = [False, False, False, True, True, False, False, False]
+        sss_list = ffd.get_streaks(flip_flop_list)
+        self.assertEqual(sss_list, [(3, 4)])
+
+    def test_GetStreakMixed(self):
+        ffd = eaissf.FlipFlopDetection([], None)
+        flip_flop_list = [False, False, True, False, False, False, False, True, False, True, False, True, True, False, False]
+        sss_list = ffd.get_streaks(flip_flop_list)
+        self.assertEqual(sss_list, [(2,2), (7,7), (9,9), (11, 12)])
+
+    def test_GetStreakLast(self):
+        ffd = eaissf.FlipFlopDetection([], None)
+        flip_flop_list = [False, False, True, False, False, False, False, True, False, True, False, True, True]
+        sss_list = ffd.get_streaks(flip_flop_list)
+        self.assertEqual(sss_list, [(2,2), (7,7), (9,9), (11,11)])
+
+    def test_MergeStreaksPass1(self):
+        ffd = eaissf.FlipFlopDetection([], None)
+        unmerged_change_list = [({'idx': 'a'}, {'idx': 'b'}),
+                                ({'idx': 'b'}, {'idx': 'c'}),
+                                ({'idx': 'c'}, {'idx': 'd'}),
+                                ({'idx': 'd'}, {'idx': 'e'}),
+                                ({'idx': 'e'}, {'idx': 'f'}),
+                                ({'idx': 'f'}, {'idx': 'g'}),
+                                ({'idx': 'g'}, {'idx': 'h'}),
+                                ({'idx': 'h'}, {'idx': 'i'})]
+        forward_merged_list = [
+            ad.AttrDict({"start": 1, "end": 2, "final_mode": ecwm.MotionTypes.IN_VEHICLE}),
+            ad.AttrDict({"start": 6, "end": 6, "final_mode": ecwm.MotionTypes.WALKING})
+        ]
+        backward_merged_list = [
+            ad.AttrDict({"start": 4, "end": 4, "final_mode": ecwm.MotionTypes.BICYCLING})
+        ]
+        ret_list = ffd.merge_streaks_pass_1(unmerged_change_list, forward_merged_list,
+                                 backward_merged_list, [])
+        self.assertEqual(ret_list, [({'idx': 'a'}, {'idx': 'd'}),
+                                    ({'idx': 'd'}, {'idx': 'e'}),
+                                    ({'idx': 'e'}, {'idx': 'h'}),
+                                    ({'idx': 'h'}, {'idx': 'i'})])
+
+        unmerged_change_list = [({'idx': 'a'}, {'idx': 'b'}),
+                                ({'idx': 'b'}, {'idx': 'c'}),
+                                ({'idx': 'c'}, {'idx': 'd'}),
+                                ({'idx': 'd'}, {'idx': 'e'}),
+                                ({'idx': 'e'}, {'idx': 'f'}),
+                                ({'idx': 'f'}, {'idx': 'g'}),
+                                ({'idx': 'g'}, {'idx': 'h'}),
+                                ({'idx': 'h'}, {'idx': 'i'}),
+                                ({'idx': 'k'}, {'idx': 'l'}),
+                                ({'idx': 'm'}, {'idx': 'n'}),
+                                ({'idx': 'o'}, {'idx': 'p'})]
+
+        forward_merged_list = [
+            ad.AttrDict({"start": 1, "end": 2, "final_mode": ecwm.MotionTypes.IN_VEHICLE}),
+            ad.AttrDict({"start": 6, "end": 6, "final_mode": ecwm.MotionTypes.WALKING})
+        ]
+        backward_merged_list = [
+            ad.AttrDict({"start": 4, "end": 4, "final_mode": ecwm.MotionTypes.BICYCLING})
+        ]
+        new_merged_list = [
+            ad.AttrDict({"start": 8, "end": 10, "final_mode": ecwm.MotionTypes.BICYCLING})
+        ]
+        ret_list = ffd.merge_streaks_pass_1(unmerged_change_list, forward_merged_list,
+                                 backward_merged_list, new_merged_list)
+        self.assertEqual(ret_list, [({'idx': 'a'}, {'idx': 'd'}),
+                                    ({'idx': 'd'}, {'idx': 'e'}),
+                                    ({'idx': 'e'}, {'idx': 'h'}),
+                                    ({'idx': 'h'}, {'idx': 'i'}),
+                                    ({'idx': 'k', 'type': ecwm.MotionTypes.BICYCLING}, {'idx': 'p'})])
+
+    def test_MergeStreaksPass2(self):
+        ffd = eaissf.FlipFlopDetection([], None)
+        unmerged_change_list = [
+            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
+                ad.AttrDict({'type': ecwm.MotionTypes.IN_VEHICLE})),
+            (ad.AttrDict({'type': ecwm.MotionTypes.IN_VEHICLE}),
+                ad.AttrDict({'type': ecwm.MotionTypes.WALKING})),
+            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
+                ad.AttrDict({'type': ecwm.MotionTypes.WALKING})),
+            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
+                ad.AttrDict({'type': ecwm.MotionTypes.WALKING})),
+            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
+                ad.AttrDict({'type': ecwm.MotionTypes.WALKING}))]
+
+        ret_list = ffd.merge_streaks_pass_2(unmerged_change_list)
+        self.assertEqual(ret_list, [
+            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
+                ad.AttrDict({'type': ecwm.MotionTypes.IN_VEHICLE})),
+            (ad.AttrDict({'type': ecwm.MotionTypes.IN_VEHICLE}),
+                ad.AttrDict({'type': ecwm.MotionTypes.WALKING})),
+            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
+                ad.AttrDict({'type': ecwm.MotionTypes.WALKING}))])
+
+if __name__ == '__main__':
+    etc.configLogging()
+    unittest.main()
--- emission/tests/analysisTests/intakeTests/TestSectionSegmentation.py
+++ emission/tests/analysisTests/intakeTests/TestSectionSegmentation.py
@@ -13,7 +13,6 @@ import json
 import bson.json_util as bju
 import uuid
 import os
-import attrdict as ad
 
 # Our imports
 import emission.core.get_database as edb
@@ -27,7 +26,6 @@ import emission.core.wrapper.rawplace as ecwrp
 import emission.core.wrapper.pipelinestate as ecwp
 
 import emission.analysis.intake.segmentation.section_segmentation_methods.smoothed_high_confidence_motion as shcm
-import emission.analysis.intake.segmentation.section_segmentation_methods.flip_flop_detection as eaissf
 import emission.analysis.intake.segmentation.section_segmentation as eaiss
 
 import emission.analysis.intake.segmentation.trip_segmentation as eaist
@@ -234,101 +232,6 @@ class TestSectionSegmentation(unittest.TestCase):
         # The expected value was copy-pasted from the debug statement above
         self.assertEqual(sections_stops,
                          [(0, 0), (8, 7)])
-
-    def test_GetStreakOne(self):
-        ffd = eaissf.FlipFlopDetection([], None)
-        flip_flop_list = [False, False, False, True, True, False, False, False]
-        sss_list = ffd.get_streaks(flip_flop_list)
-        self.assertEqual(sss_list, [(3, 4)])
-
-    def test_GetStreakMixed(self):
-        ffd = eaissf.FlipFlopDetection([], None)
-        flip_flop_list = [False, False, True, False, False, False, False, True, False, True, False, True, True, False, False]
-        sss_list = ffd.get_streaks(flip_flop_list)
-        self.assertEqual(sss_list, [(2,2), (7,7), (9,9), (11, 12)])
-
-    def test_GetStreakLast(self):
-        ffd = eaissf.FlipFlopDetection([], None)
-        flip_flop_list = [False, False, True, False, False, False, False, True, False, True, False, True, True]
-        sss_list = ffd.get_streaks(flip_flop_list)
-        self.assertEqual(sss_list, [(2,2), (7,7), (9,9), (11,11)])
-
-    def test_MergeStreaksPass1(self):
-        ffd = eaissf.FlipFlopDetection([], None)
-        unmerged_change_list = [({'idx': 'a'}, {'idx': 'b'}),
-                                ({'idx': 'b'}, {'idx': 'c'}),
-                                ({'idx': 'c'}, {'idx': 'd'}),
-                                ({'idx': 'd'}, {'idx': 'e'}),
-                                ({'idx': 'e'}, {'idx': 'f'}),
-                                ({'idx': 'f'}, {'idx': 'g'}),
-                                ({'idx': 'g'}, {'idx': 'h'}),
-                                ({'idx': 'h'}, {'idx': 'i'})]
-        forward_merged_list = [
-            ad.AttrDict({"start": 1, "end": 2, "final_mode": ecwm.MotionTypes.IN_VEHICLE}),
-            ad.AttrDict({"start": 6, "end": 6, "final_mode": ecwm.MotionTypes.WALKING})
-        ]
-        backward_merged_list = [
-            ad.AttrDict({"start": 4, "end": 4, "final_mode": ecwm.MotionTypes.BICYCLING})
-        ]
-        ret_list = ffd.merge_streaks_pass_1(unmerged_change_list, forward_merged_list,
-                                 backward_merged_list, [])
-        self.assertEqual(ret_list, [({'idx': 'a'}, {'idx': 'd'}),
-                                    ({'idx': 'd'}, {'idx': 'e'}),
-                                    ({'idx': 'e'}, {'idx': 'h'}),
-                                    ({'idx': 'h'}, {'idx': 'i'})])
-
-        unmerged_change_list = [({'idx': 'a'}, {'idx': 'b'}),
-                                ({'idx': 'b'}, {'idx': 'c'}),
-                                ({'idx': 'c'}, {'idx': 'd'}),
-                                ({'idx': 'd'}, {'idx': 'e'}),
-                                ({'idx': 'e'}, {'idx': 'f'}),
-                                ({'idx': 'f'}, {'idx': 'g'}),
-                                ({'idx': 'g'}, {'idx': 'h'}),
-                                ({'idx': 'h'}, {'idx': 'i'}),
-                                ({'idx': 'k'}, {'idx': 'l'}),
-                                ({'idx': 'm'}, {'idx': 'n'}),
-                                ({'idx': 'o'}, {'idx': 'p'})]
-
-        forward_merged_list = [
-            ad.AttrDict({"start": 1, "end": 2, "final_mode": ecwm.MotionTypes.IN_VEHICLE}),
-            ad.AttrDict({"start": 6, "end": 6, "final_mode": ecwm.MotionTypes.WALKING})
-        ]
-        backward_merged_list = [
-            ad.AttrDict({"start": 4, "end": 4, "final_mode": ecwm.MotionTypes.BICYCLING})
-        ]
-        new_merged_list = [
-            ad.AttrDict({"start": 8, "end": 10, "final_mode": ecwm.MotionTypes.BICYCLING})
-        ]
-        ret_list = ffd.merge_streaks_pass_1(unmerged_change_list, forward_merged_list,
-                                 backward_merged_list, new_merged_list)
-        self.assertEqual(ret_list, [({'idx': 'a'}, {'idx': 'd'}),
-                                    ({'idx': 'd'}, {'idx': 'e'}),
-                                    ({'idx': 'e'}, {'idx': 'h'}),
-                                    ({'idx': 'h'}, {'idx': 'i'}),
-                                    ({'idx': 'k', 'type': ecwm.MotionTypes.BICYCLING}, {'idx': 'p'})])
-
-    def test_MergeStreaksPass2(self):
-        ffd = eaissf.FlipFlopDetection([], None)
-        unmerged_change_list = [
-            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
-                ad.AttrDict({'type': ecwm.MotionTypes.IN_VEHICLE})),
-            (ad.AttrDict({'type': ecwm.MotionTypes.IN_VEHICLE}),
-                ad.AttrDict({'type': ecwm.MotionTypes.WALKING})),
-            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
-                ad.AttrDict({'type': ecwm.MotionTypes.WALKING})),
-            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
-                ad.AttrDict({'type': ecwm.MotionTypes.WALKING})),
-            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
-                ad.AttrDict({'type': ecwm.MotionTypes.WALKING}))]
-
-        ret_list = ffd.merge_streaks_pass_2(unmerged_change_list)
-        self.assertEqual(ret_list, [
-            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
-                ad.AttrDict({'type': ecwm.MotionTypes.IN_VEHICLE})),
-            (ad.AttrDict({'type': ecwm.MotionTypes.IN_VEHICLE}),
-                ad.AttrDict({'type': ecwm.MotionTypes.WALKING})),
-            (ad.AttrDict({'type': ecwm.MotionTypes.WALKING}),
-                ad.AttrDict({'type': ecwm.MotionTypes.WALKING}))])
 
 if __name__ == '__main__':
     etc.configLogging()
