f72ecb5c9bb55e67a0e2c54214cc1655b79e2356
Shankari <shankari@eecs.berkeley.edu>
2020-09-14 20:45:46 -0700

    handle the case where `last_ts_processed` is None
    got a TypeError here when we were experimenting with loading data for app store
    review
    
    Testing done:
    - Reran the pipeline; the error went away

--- emission/analysis/intake/segmentation/trip_segmentation.py
+++ emission/analysis/intake/segmentation/trip_segmentation.py
@@ -165,6 +165,8 @@ def get_last_ts_processed(filter_methods):
             if last_ts_processed is None or method.last_ts_processed > last_ts_processed:
                 last_ts_processed = method.last_ts_processed
                 logging.debug("Set last_ts_processed = %s from method %s" % (last_ts_processed, method))
+        except TypeError as e:
+            logging.debug("Processing method %s got error %s, skipping" % (method, e))
         except AttributeError as e:
             logging.debug("Processing method %s got error %s, skipping" % (method, e))
     logging.info("Returning last_ts_processed = %s" % last_ts_processed)
