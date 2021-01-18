64db3bf7c363f52cfeb96289db200ae647b87e9f
Shankari <shankari@eecs.berkeley.edu>
2021-01-17 21:13:57 -0800

    Server side changes to support infinite scroll
    + support querying by other time keys instead of only `metadata.write_ts`. This
    ensures that we can accurately query for analysed results as well, since they
    can be generated at any time after the raw data is collected.
    + support returning both the max and min value of any data field
        - Change the existing call to get_max_value_for_field to use it
        - Add a new method to the API that gets the pipeline range, not just the last entry
    + use this to figure out when to properly stop the infinite scroll

--- emission/net/api/pipeline.py
+++ emission/net/api/pipeline.py
@@ -6,10 +6,28 @@ from future import standard_library
 standard_library.install_aliases()
 from builtins import *
 import logging
+import pymongo
 
 import emission.storage.pipeline_queries as esp
+import emission.storage.timeseries.abstract_timeseries as esta
 
 def get_complete_ts(user_id):
     complete_ts = esp.get_complete_ts(user_id)
     logging.debug("Returning complete_ts = %s" % complete_ts)
     return complete_ts
+
+def get_range(user_id):
+    ts = esta.TimeSeries.get_time_series(user_id)
+    start_ts = ts.get_first_value_for_field("analysis/confirmed_trip", "data.start_ts", pymongo.ASCENDING)
+    if start_ts == -1:
+        start_ts = ts.get_first_value_for_field("analysis/cleaned_trip", "data.start_ts", pymongo.ASCENDING)
+
+    end_ts = ts.get_first_value_for_field("analysis/confirmed_trip", "data.end_ts", pymongo.DESCENDING)
+    if end_ts == -1:
+        end_ts = ts.get_first_value_for_field("analysis/cleaned_trip", "data.end_ts", pymongo.DESCENDING)
+
+    complete_ts = get_complete_ts(user_id)
+    if (end_ts != (complete_ts - esp.END_FUZZ_AVOID_LTE)):
+        logging.exception("end_ts %s != complete_ts no fuzz %s" % (end_ts, (complete_ts - esp.END_FUZZ_AVOID_LTE)))
+
+    return (start_ts, end_ts)
