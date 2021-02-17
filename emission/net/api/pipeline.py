1c113c005b260a13bd93c33bf9b033184c2f0ea5
Shankari <shankari@eecs.berkeley.edu>
2021-02-16 17:25:06 -0800

    Return None for the range if there are no analysis results
    This allows us to not go into an infinite scroll in case the user doesn't have
    any analysis results.
    
    Without this fix, we returned (-1, -1), so the phone code assumed that was
    valid and created the timestamp range by subtracting a week from it. So we kept
    going into negative timestamp ranges and got into an infinite loop.
    
    This returns None, so the `if (pipeline_range.end_ts)` check actually works

--- emission/net/api/pipeline.py
+++ emission/net/api/pipeline.py
@@ -21,13 +21,20 @@ def get_range(user_id):
     start_ts = ts.get_first_value_for_field("analysis/confirmed_trip", "data.start_ts", pymongo.ASCENDING)
     if start_ts == -1:
         start_ts = ts.get_first_value_for_field("analysis/cleaned_trip", "data.start_ts", pymongo.ASCENDING)
+    if start_ts == -1:
+        start_ts = None
 
     end_ts = ts.get_first_value_for_field("analysis/confirmed_trip", "data.end_ts", pymongo.DESCENDING)
     if end_ts == -1:
         end_ts = ts.get_first_value_for_field("analysis/cleaned_trip", "data.end_ts", pymongo.DESCENDING)
+    if end_ts == -1:
+        end_ts = None
 
     complete_ts = get_complete_ts(user_id)
-    if (end_ts != (complete_ts - esp.END_FUZZ_AVOID_LTE)):
-        logging.exception("end_ts %s != complete_ts no fuzz %s" % (end_ts, (complete_ts - esp.END_FUZZ_AVOID_LTE)))
+    if complete_ts is not None and end_ts is not None\
+        and (end_ts != (complete_ts - esp.END_FUZZ_AVOID_LTE)):
+        logging.exception("end_ts %s != complete_ts no fuzz %s" %
+            (end_ts, (complete_ts - esp.END_FUZZ_AVOID_LTE)))
 
+    logging.debug("Returning range (%s, %s)" % (start_ts, end_ts))
     return (start_ts, end_ts)
