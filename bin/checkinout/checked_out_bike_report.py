9e48514f553fbe6d3413ff2ad78ac877502bb559
Shankari <shankari@eecs.berkeley.edu>
2021-05-06 08:26:01 -0700

    Handle the case with no checked out bikes
    Doh!

--- bin/checkinout/checked_out_bike_report.py
+++ bin/checkinout/checked_out_bike_report.py
@@ -9,6 +9,9 @@ import emission.core.get_database as edb
 def display_checked_out_list(tz):
     all_list = list(edb.get_checkinout_db().find())
     all_df = pd.DataFrame(all_list)
+    if len(all_df) == 0:
+        print("No checked out bikes")
+        return
     all_df["fmt_date"] = all_df.ts.apply(lambda ts: arrow.get(ts).to(tz))
     all_df.drop(["_id", "ts", "status"], axis=1, inplace=True)
     all_df
