e-mission/e-mission-server

commit 16441ed9e1369ff923957bd80722bb11204864d6

monir modification in pipeline and similarity

emission/analysis/modelling/tour_model/cluster_pipeline.py
@@ -67,7 +67,7 @@ def cluster(data, nBins):
     max = int(math.ceil(1.5 * nBins))
     feat.cluster(min_clusters=min, max_clusters=max)
     logging.debug('number of clusters: %d' % feat.clusters)
-    return feat.clusters, feat.labels, feat.data
+    return feat.clusters, feat.labels, feat.data, feat.points
 
 #prepare the data for the tour model
 def cluster_to_tour_model(data, labels):

emission/analysis/modelling/tour_model/similarity.py
@@ -96,7 +96,7 @@ class similarity(object):
     #delete lower portion of bins
     def delete_bins(self):
         self.calc_cutoff_bins()
-        for i in range(len(self.bins) - num):
+        for i in range(len(self.bins) - self.num):
             self.bins.pop()
         newdata = []
         for bin in self.bins:

commit de95fc2fba304e3b60270367179741b3ad651e77

Fix regression introduced in 16441ed9e
In 16441ed9e, @corinne-hcr introduced an additional return value from `cluster`.
While she changed the call location in her notebook, unfortunately, she did not
change the call location in the cluster pipeline driver.

This lead to the error in
https://github.com/e-mission/e-mission-server/pull/815#issue-653177984

```
Traceback (most recent call last):
  File "emission/analysis/modelling/tour_model/cluster_pipeline.py", line 99, in <module>
    main(uuid=uuid)
  File "emission/analysis/modelling/tour_model/cluster_pipeline.py", line 88, in main
    n, labels, data = cluster(data, len(bins))
ValueError: too many values to unpack (expected 3)
```

Testing done:
After the fix, there is no error

```
2021-05-25 21:35:21,704:DEBUG:in representatives, endpoints have len = 4
2021-05-25 21:35:21,704:DEBUG:For cluster 1, centers are [  37.39335332 -122.08335353   37.38773225 -122.08463903]
2021-05-25 21:35:21,705:DEBUG:number of locations: 4
```

emission/analysis/modelling/tour_model/cluster_pipeline.py
@@ -85,7 +85,7 @@ def main(uuid=None):
     data = read_data(uuid)
     logging.debug("len(data) is %d" % len(data))
     data, bins = remove_noise(data, 300)
-    n, labels, data = cluster(data, len(bins))
+    n, labels, data, points = cluster(data, len(bins))
     tour_dict = cluster_to_tour_model(data, labels)
     return tour_dict
 