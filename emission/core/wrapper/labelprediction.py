0f1ed52e7525ea5f41d63642bca1eaeb9d5af8ba
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2021-07-01 12:36:57 -0600

    Add comments explaining prediction data structure

--- emission/core/wrapper/labelprediction.py
+++ emission/core/wrapper/labelprediction.py
@@ -1,9 +1,16 @@
 # Based on modeprediction.py
 import emission.core.wrapper.wrapperbase as ecwb
 
+# The "prediction" data structure is a list of label possibilities, each one consisting of a set of labels and a probability:
+# [
+#    {"labels": {"labeltype1": "labelvalue1", "labeltype2": "labelvalue2"}, "p": 0.61},
+#    {"labels": {"labeltype1": "labelvalue3", "labeltype2": "labelvalue4"}, "p": 0.27},
+#    ...
+# ]
+
 class Labelprediction(ecwb.WrapperBase):
     props = {"trip_id":   ecwb.WrapperBase.Access.WORM,  # the trip that this is part of
-            "prediction": ecwb.WrapperBase.Access.WORM,  # What we predict
+            "prediction": ecwb.WrapperBase.Access.WORM,  # What we predict -- see above
             "start_ts":   ecwb.WrapperBase.Access.WORM,  # start time for the prediction, so that it can be captured in time-based queries, e.g. to reset the pipeline
             "end_ts":     ecwb.WrapperBase.Access.WORM,  # end time for the prediction, so that it can be captured in time-based queries, e.g. to reset the pipeline
     }
