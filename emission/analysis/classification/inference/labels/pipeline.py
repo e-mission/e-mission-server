d0136fc8c041739bf2809f74b3e8b44846957785
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2021-07-01 14:57:35 -0600

    Restructure label inference pipeline to accommodate multiple algorithms
    + The label inference pipeline.py now handles multiple algorithms less
      awkwardly and has a place for combination of multiple predictions into
      an ensemble.
    + The pipeline now stores each algorithm's prediction and a final
      (perhaps ensemble) prediction in different places
    
    Tested by confirming that client-side behavior is the same as before.

--- emission/analysis/classification/inference/labels/pipeline.py
+++ emission/analysis/classification/inference/labels/pipeline.py
@@ -1,5 +1,7 @@
 # Standard imports
 import logging
+import random
+import copy
 
 # Our imports
 import emission.storage.pipeline_queries as epq
@@ -20,6 +22,77 @@ def infer_labels(user_id):
         logging.exception("Error while inferring labels, timestamp is unchanged")
         epq.mark_label_inference_failed(user_id)
 
+# A set of placeholder predictors to allow pipeline development without a real inference algorithm.
+# For the moment, the system is configured to work with two labels, "mode_confirm" and
+# "purpose_confirm", so I'll do that.
+
+# The first placeholder scenario represents a case where it is hard to distinguish between
+# biking and walking (e.g., because the user is a very slow biker) and hard to distinguish
+# between work and shopping at the grocery store (e.g., because the user works at the
+# grocery store), but whenever the user bikes to the location it is to work and whenever
+# the user walks to the location it is to shop (e.g., because they don't have a basket on
+# their bike), and the user bikes to the location four times more than they walk there.
+# Obviously, it is a simplification.
+def placeholder_predictor_0(trip):
+    return [
+        {"labels": {"mode_confirm": "bike", "purpose_confirm": "work"}, "p": 0.8},
+        {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.2}
+    ]
+
+
+# The next placeholder scenario provides that same set of labels in 75% of cases and no
+# labels in the rest.
+def placeholder_predictor_1(trip):
+    return [
+        {"labels": {"mode_confirm": "bike", "purpose_confirm": "work"}, "p": 0.8},
+        {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.2}
+    ] if random.random() > 0.25 else []
+
+
+# This third scenario provides labels designed to test the soundness and resilience of
+# the client-side inference processing algorithms.
+trip_n = 0  # ugly use of globals for testing only
+def placeholder_predictor_2(trip):
+    global trip_n  # ugly use of globals for testing only
+    trip_n %= 6
+    trip_n += 1
+    return [
+        [
+
+        ],
+        [
+            {"labels": {"mode_confirm": "bike", "purpose_confirm": "work"}, "p": 0.8},
+            {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.2}
+        ],
+        [
+            {"labels": {"mode_confirm": "drove_alone"}, "p": 0.8},
+        ],
+        [
+            {"labels": {"mode_confirm": "bike", "purpose_confirm": "work"}, "p": 0.8},
+            {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.2}
+        ],
+        [
+            {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.45},
+            {"labels": {"mode_confirm": "walk", "purpose_confirm": "entertainment"}, "p": 0.35},
+            {"labels": {"mode_confirm": "drove_alone", "purpose_confirm": "work"}, "p": 0.15},
+            {"labels": {"mode_confirm": "shared_ride", "purpose_confirm": "work"}, "p": 0.05}
+        ],
+        [
+            {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.45},
+            {"labels": {"mode_confirm": "walk", "purpose_confirm": "entertainment"}, "p": 0.35},
+            {"labels": {"mode_confirm": "drove_alone", "purpose_confirm": "work"}, "p": 0.15},
+            {"labels": {"mode_confirm": "shared_ride", "purpose_confirm": "work"}, "p": 0.05}
+        ]
+    ][6-trip_n]
+
+# For each algorithm in ecwl.AlgorithmTypes that runs on a trip (e.g., not the ensemble, which
+# runs on the results of other algorithms), primary_algorithms specifies a corresponding
+# function to run
+primary_algorithms = {
+    # This can be edited to select a different placeholder predictor
+    ecwl.AlgorithmTypes.PLACEHOLDER: placeholder_predictor_1
+}
+
 # Code structure based on emission.analysis.classification.inference.mode.pipeline
 # and emission.analysis.classification.inference.mode.rule_engine
 class LabelInferencePipeline:
@@ -35,87 +108,38 @@ class LabelInferencePipeline:
         self.toPredictTrips = esda.get_entries(
             esda.CLEANED_TRIP_KEY, user_id, time_query=time_range)
         for trip in self.toPredictTrips:
-            prediction = predict_trip(trip)
+            results = self.compute_and_save_algorithms(trip)
+            self.compute_and_save_ensemble(trip, results)
+            if self._last_trip_done is None or self._last_trip_done.data.end_ts < trip.data.end_ts:
+                self._last_trip_done = trip
+    
+    # This is where the labels for a given trip are actually predicted.
+    # Though the only information passed in is the trip object, the trip object can provide the
+    # user_id and other potentially useful information.
+    def compute_and_save_algorithms(self, trip):
+        predictions = []
+        for algorithm_id, algorithm_fn in primary_algorithms.items():
+            prediction = algorithm_fn(trip)
             lp = ecwl.Labelprediction()
             lp.trip_id = trip.get_id()
+            lp.algorithm_id = algorithm_id
             lp.prediction = prediction
             lp.start_ts = trip.data.start_ts
             lp.end_ts = trip.data.end_ts
-            # Insert the Labelprediction into the database as its own independent document
-            self.ts.insert_data(self.user_id, esda.INFERRED_LABELS_KEY, lp)
-            if self._last_trip_done is None or self._last_trip_done.data.end_ts < trip.data.end_ts:
-                self._last_trip_done = trip
+            self.ts.insert_data(self.user_id, "inference/labels", lp)
+            predictions.append(lp)
+        return predictions
 
-# This is where the labels for a given trip are actually predicted.
-# Though the only information passed in is the trip object, the trip object can provide the
-# user_id and other potentially useful information.
-def predict_trip(trip):
-    return placeholder_prediction(trip)
+    # Combine all our predictions into a single ensemble prediction.
+    def compute_and_save_ensemble(self, trip, predictions):
+        il = ecwl.Labelprediction()
+        il.trip_id = trip.get_id()
+        il.algorithm_id = ecwl.AlgorithmTypes.ENSEMBLE
+        il.start_ts = trip.data.start_ts
+        il.end_ts = trip.data.end_ts
 
-# For testing only!
-trip_n = 0
-import random
-
-# A placeholder predictor to allow pipeline development without a real inference algorithm
-def placeholder_prediction(trip, scenario=2):
-    # For the moment, the system is configured to work with two labels, "mode_confirm" and
-    # "purpose_confirm", so I'll do that.
-
-    # For testing only!
-    global trip_n
-    trip_n %= 6
-    trip_n += 1
-
-    return [
-        # The first placeholder scenario represents a case where it is hard to distinguish between
-        # biking and walking (e.g., because the user is a very slow biker) and hard to distinguish
-        # between work and shopping at the grocery store (e.g., because the user works at the
-        # grocery store), but whenever the user bikes to the location it is to work and whenever
-        # the user walks to the location it is to shop (e.g., because they don't have a basket on
-        # their bike), and the user bikes to the location four times more than they walk there.
-        # Obviously, it is a simplification.
-        [
-            {"labels": {"mode_confirm": "bike", "purpose_confirm": "work"}, "p": 0.8},
-            {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.2}
-        ],
-
-        # The next placeholder scenario provides that same set of labels in 75% of cases and no
-        # labels in the rest.
-        [
-            {"labels": {"mode_confirm": "bike", "purpose_confirm": "work"}, "p": 0.8},
-            {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.2}
-        ]
-        if random.random() > 0.25 else [],
-
-        # This third scenario provides labels designed to test the soundness and resilience of
-        # the client-side inference processing algorithms.
-        [
-            [
-
-            ],
-            [
-                {"labels": {"mode_confirm": "bike", "purpose_confirm": "work"}, "p": 0.8},
-                {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.2}
-            ],
-            [
-                {"labels": {"mode_confirm": "drove_alone"}, "p": 0.8},
-            ],
-            [
-                {"labels": {"mode_confirm": "bike", "purpose_confirm": "work"}, "p": 0.8},
-                {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.2}
-            ],
-            [
-                {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.45},
-                {"labels": {"mode_confirm": "walk", "purpose_confirm": "entertainment"}, "p": 0.35},
-                {"labels": {"mode_confirm": "drove_alone", "purpose_confirm": "work"}, "p": 0.15},
-                {"labels": {"mode_confirm": "shared_ride", "purpose_confirm": "work"}, "p": 0.05}
-            ],
-            [
-                {"labels": {"mode_confirm": "walk", "purpose_confirm": "shopping"}, "p": 0.45},
-                {"labels": {"mode_confirm": "walk", "purpose_confirm": "entertainment"}, "p": 0.35},
-                {"labels": {"mode_confirm": "drove_alone", "purpose_confirm": "work"}, "p": 0.15},
-                {"labels": {"mode_confirm": "shared_ride", "purpose_confirm": "work"}, "p": 0.05}
-            ]
-        ][6-trip_n]
-    ][scenario]
-    
+        # As a placeholder, we just take the first prediction.
+        # TODO: implement a real combination algorithm.
+        il.prediction = copy.copy(predictions[0]["prediction"])
+        
+        self.ts.insert_data(self.user_id, "analysis/inferred_labels", il)
--- emission/core/wrapper/labelprediction.py
+++ emission/core/wrapper/labelprediction.py
@@ -1,5 +1,6 @@
 # Based on modeprediction.py
 import emission.core.wrapper.wrapperbase as ecwb
+import enum
 
 # The "prediction" data structure is a list of label possibilities, each one consisting of a set of labels and a probability:
 # [
@@ -8,14 +9,23 @@ import emission.core.wrapper.wrapperbase as ecwb
 #    ...
 # ]
 
+
+class AlgorithmTypes(enum.Enum):
+    ENSEMBLE = 0
+    PLACEHOLDER = 1
+
+
 class Labelprediction(ecwb.WrapperBase):
-    props = {"trip_id":   ecwb.WrapperBase.Access.WORM,  # the trip that this is part of
-            "prediction": ecwb.WrapperBase.Access.WORM,  # What we predict -- see above
-            "start_ts":   ecwb.WrapperBase.Access.WORM,  # start time for the prediction, so that it can be captured in time-based queries, e.g. to reset the pipeline
-            "end_ts":     ecwb.WrapperBase.Access.WORM,  # end time for the prediction, so that it can be captured in time-based queries, e.g. to reset the pipeline
+    props = {"trip_id":     ecwb.WrapperBase.Access.WORM,  # the trip that this is part of
+            "algorithm_id": ecwb.WrapperBase.Access.WORM,  # the algorithm that made this prediction
+            "prediction":   ecwb.WrapperBase.Access.WORM,  # What we predict -- see above
+            "start_ts":     ecwb.WrapperBase.Access.WORM,  # start time for the prediction, so that it can be captured in time-based queries, e.g. to reset the pipeline
+            "end_ts":       ecwb.WrapperBase.Access.WORM,  # end time for the prediction, so that it can be captured in time-based queries, e.g. to reset the pipeline
     }
 
-    enums = {}
+    enums = {
+        "algorithm_id": AlgorithmTypes
+    }
     geojson = {}
     local_dates = {}
 
