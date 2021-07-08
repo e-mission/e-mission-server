7095cceedc62c359a644f85be3aa797b474dc77b
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2021-07-08 12:52:45 -0600

    Use existing test procedures for TestLabelInferencePipeline

--- emission/tests/pipelineTests/TestLabelInferencePipeline.py
+++ emission/tests/pipelineTests/TestLabelInferencePipeline.py
@@ -2,17 +2,15 @@
 # and thus intentionally does not test the real inference algorithms themselves.
 import unittest
 import numpy as np
-from logging import config  # epi.run_intake_pipeline needs this to be done here
 import time
 
-import emission.core.wrapper.user as ecwu
-import emission.pipeline.intake_stage as epi
-import emission.pipeline.reset as epr
 import emission.analysis.classification.inference.labels.pipeline as eacilp
 import emission.core.wrapper.labelprediction as ecwl
 import emission.storage.decorations.analysis_timeseries_queries as esda
 import emission.storage.timeseries.timequery as estt
 import emission.storage.decorations.trip_queries as esdt
+import emission.core.get_database as edb
+import emission.tests.common as etc
 
 class TestLabelInferencePipeline(unittest.TestCase):
     # It is important that these functions be deterministic
@@ -23,26 +21,26 @@ class TestLabelInferencePipeline(unittest.TestCase):
 
     def setUp(self):
         np.random.seed(61297777)
-        self.user_id = ecwu.User.fromEmail("test_july_22").uuid
-        self.reset_pipeline()
+        self.reset_all()
+        etc.setupRealExample(self, "emission/tests/data/real_examples/shankari_2015-jul-22")
         self.run_pipeline(self.test_algorithms)
         time_range = estt.TimeQuery("metadata.write_ts", None, time.time())
-        self.cleaned_trips = esda.get_entries(esda.CLEANED_TRIP_KEY, self.user_id, time_query=time_range)
+        self.cleaned_trips = esda.get_entries(esda.CLEANED_TRIP_KEY, self.testUUID, time_query=time_range)
         self.cleaned_id_to_trip = {trip.get_id(): trip for trip in self.cleaned_trips}
-        self.inferred_trips = esda.get_entries(esda.INFERRED_TRIP_KEY, self.user_id, time_query=time_range)
+        self.inferred_trips = esda.get_entries(esda.INFERRED_TRIP_KEY, self.testUUID, time_query=time_range)
 
     def tearDown(self):
-        self.reset_pipeline()
+        self.reset_all()
 
     def run_pipeline(self, algorithms):
         default_primary_algorithms = eacilp.primary_algorithms
         eacilp.primary_algorithms = algorithms
-        epi.run_intake_pipeline("single", [self.user_id])
+        etc.runIntakePipeline(self.testUUID)
         eacilp.primary_algorithms = default_primary_algorithms
 
-    def reset_pipeline(self):
-        epr.reset_user_to_start(self.user_id, False)
-    
+    def reset_all(self):
+        etc.dropAllCollections(edb._get_current_db())
+
     # Tests that the fields from the cleaned trip are carried over into the inferred trip correctly
     def testPipelineIntegrity(self):
         self.assertEqual(len(self.inferred_trips), len(self.cleaned_trips))
@@ -56,7 +54,7 @@ class TestLabelInferencePipeline(unittest.TestCase):
     # Tests that each of the (test) algorithms runs and saves to the database correctly
     def testIndividualAlgorithms(self):
         for trip in self.inferred_trips:
-            entries = esdt.get_sections_for_trip("inference/labels", self.user_id, trip.get_id())
+            entries = esdt.get_sections_for_trip("inference/labels", self.testUUID, trip.get_id())
             self.assertEqual(len(entries), len(self.test_algorithms))
             for entry in entries:
                 self.assertEqual(entry["data"]["trip_id"], trip.get_id())
@@ -69,7 +67,7 @@ class TestLabelInferencePipeline(unittest.TestCase):
     # Tests that the ensemble algorithm runs and saves to the database correctly
     def testEnsemble(self):
         for trip in self.inferred_trips:
-            entries = esdt.get_sections_for_trip("analysis/inferred_labels", self.user_id, trip.get_id())
+            entries = esdt.get_sections_for_trip("analysis/inferred_labels", self.testUUID, trip.get_id())
             self.assertEqual(len(entries), 1)
             entry = entries[0]
             # TODO: when we have a real ensemble implemented:
@@ -81,5 +79,12 @@ class TestLabelInferencePipeline(unittest.TestCase):
     # Tests that the final inferred labels in the inferred trip are the same as those given by the ensemble algorithm
     def testInferredTrip(self):
         for trip in self.inferred_trips:
-            entry = esdt.get_sections_for_trip("analysis/inferred_labels", self.user_id, trip.get_id())[0]
+            entry = esdt.get_sections_for_trip("analysis/inferred_labels", self.testUUID, trip.get_id())[0]
             self.assertEqual(trip["data"]["inferred_labels"], entry["data"]["prediction"])
+
+def main():
+    etc.configLogging()
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
