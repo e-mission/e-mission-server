81ba5fd68e52ecbde4989e7a6d10b164a8d7f4d1
Chunrui Huang <corinnehcr@gmail.com>
2021-07-19 14:55:14 -0700

    refactored plotting code

--- emission/analysis/modelling/tour_model/get_plot.py
+++ emission/analysis/modelling/tour_model/get_plot.py
@@ -9,70 +9,37 @@ import load_predict as predict
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-# def get_scatter(percentage,homo_score,valid_users):
-#     x=percentage
-#     y=homo_score
-#     v=valid_users
-#     cmp = cm.get_cmap('Dark2', len(valid_users))
-#
-#     sc = []
-#     for i in range(len(valid_users)):
-#         for n in range(len(x[i])):
-#             point = plt.scatter(x[i][n], y[i][n], color=cmp.colors[i], s=70, alpha=0.7)
-#         sc.append(point)
-#     plt.legend(sc,v,markerscale=0.8,scatterpoints=1,bbox_to_anchor=(1.23,1))
-#     plt.xlabel('user input request percentage',fontsize=16)
-#     plt.ylabel('homogeneity score',fontsize=16)
-#     plt.xticks(np.arange(0.4,1.1,step=0.1),fontsize=14)
-#     plt.yticks(np.arange(0.2,1.1,step=0.1),fontsize=14)
-def get_scatter(valid_users,collect_filename,first_round,second_round):
-    all_filename = predict.loadModelStage()
+def get_scatter(valid_users,all_filename,first_round,second_round):
     sc = []
+    cmp = cm.get_cmap('Dark2', len(valid_users))
     for i in range(len(valid_users)):
         df = pd.read_csv(all_filename[i], index_col='split')
-        plot_scatter(df, valid_users[i], i, sc, first_round, second_round)
-    plt.legend(sc, valid_users, markerscale=0.8, scatterpoints=1, bbox_to_anchor=(1.1, 1))
+        color = cmp.colors[i]
+        plot_scatter(df, sc, color, first_round, second_round)
+    plt.legend(sc, valid_users, markerscale=0.8, scatterpoints=1, bbox_to_anchor=(1.21, 1))
+    plt.xlabel('user input request percentage',fontsize=16)
+    plt.ylabel('homogeneity score',fontsize=16)
+    plt.xticks(np.arange(0.4,1.1,step=0.1),fontsize=14)
+    plt.yticks(np.arange(0.2,1.1,step=0.1),fontsize=14)
     plt.show()
 
-def plot_scatter(result_df,valid_user,user_index,sc,first_round,second_round):
-    v = valid_user
-    cmp = cm.get_cmap('Dark2', len(valid_users))
 
+def plot_scatter(result_df,sc,color,first_round,second_round):
     if first_round:
         x = result_df['percentage of 1st round']
         y = result_df['homogeneity socre of 1st round']
     elif second_round:
         x = result_df['percentage of 2nd round']
         y = result_df['homogeneity socre of 2nd round']
-    # print(x)
-    point = plt.scatter(x, y, color=cmp.colors[user_index], s=70, alpha=0.7)
+    point = plt.scatter(x, y, color=color, s=70, alpha=0.7)
     sc.append(point)
-
-    # plt.legend(sc,v,markerscale=0.8,scatterpoints=1,bbox_to_anchor=(1.23,1))
-    # plt.legend(sc, v, markerscale=0.8, scatterpoints=1,bbox_to_anchor=(1.15,1))
-    plt.xlabel('user input request percentage',fontsize=16)
-    plt.ylabel('homogeneity score',fontsize=16)
-    plt.xticks(np.arange(0.4,1.1,step=0.1),fontsize=14)
-    plt.yticks(np.arange(0.2,1.1,step=0.1),fontsize=14)
-
-
-
-    # for i in range(len(valid_users)):
-    #     for n in range(len(x[i])):
-    #         point = plt.scatter(x[i][n], y[i][n], color=cmp.colors[i], s=70, alpha=0.7)
-    #     sc.append(point)
-    # plt.legend(sc,v,markerscale=0.8,scatterpoints=1,bbox_to_anchor=(1.23,1))
-    # plt.xlabel('user input request percentage',fontsize=16)
-    # plt.ylabel('homogeneity score',fontsize=16)
-    # plt.xticks(np.arange(0.4,1.1,step=0.1),fontsize=14)
-    # plt.yticks(np.arange(0.2,1.1,step=0.1),fontsize=14)
 
 
 def same_cluster_map(cluster,filter_trips,bins):
     color_map = clm.linear.Set1_07.to_step(len(bins), index=[i for i in range(len(bins)+1)])
     first_trip = filter_trips[cluster[0]]
     map = folium.Map(location=[first_trip.data.start_loc["coordinates"][1], first_trip.data.start_loc["coordinates"][0]],
-                   zoom_start=12, max_zoom=30, control_scale=True)
+                     zoom_start=12, max_zoom=30, control_scale=True)
 
     zoom_points = []
     for curr_trip_index in cluster:
@@ -97,7 +64,13 @@ def same_cluster_map(cluster,filter_trips,bins):
     return map
 
 if __name__ == '__main__':
-    valid_users=['user1', 'user3']
+    # modify valid_users list for the user results you have saved
+    valid_users=['user1', 'user3', 'user4', 'user6', 'user8', 'user9', 'user10', 'user13']
+    # - collect_filename: filenames of all results from 2 rounds of clustering
     collect_filename = predict.loadModelStage("collect_filename")
+    # scatter plot from the result of the 1st round
     plt.figure()
     get_scatter(valid_users, collect_filename, first_round=True, second_round=False)
+    # scatter plot from the result of the 2nd round
+    plt.figure()
+    get_scatter(valid_users, collect_filename, first_round=False, second_round=True)
