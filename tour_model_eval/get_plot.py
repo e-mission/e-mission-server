bb21c7a55c2b0b615aeeb602534b100bbd861e32
Chunrui Huang <68228318+corinne-hcr@users.noreply.github.com>
2021-07-24 07:41:13 -0700

    check in notebook code (#23)
    * check in notebook code
    
    * update notebook
    
    * update notebook code, the previous one was not the latest one
    
    * add some functions
    
    * refactor evaluation_pipeline code, pull out get_socre function from get_tuning_score.py to get_scores.py
    
    * add kmeans after running hierarchical clustering for re-building the model, only for testing, using user1
    
    * adding kmeans only in the test step
    
    * check in codes for generating result for Gabriel's function, will move them to the server repo after refactoring
    
    * change the line of importing label_processing
    
    * address the problems from the previous commit, but has not yet done with geometric shape
    
    * Update tour_model_eval/build_save_model.py
    
    Co-authored-by: shankari <shankari@eecs.berkeley.edu>
    
    * refactored notebook code, not done with plot
    
    * update build_save_model according to notebook refactoring
    
    * check in the changes in the notebook, have put the original clustering code in evaluation_pipeline
    
    * delete output from the notebook
    
    * modify test notebook and add comments on it, remove extraneous files
    
    * add plot code, read filename directly from user id, add another way to read all data from the database in the notebook
    
    Co-authored-by: shankari <shankari@eecs.berkeley.edu>

--- tour_model_eval/get_plot.py
+++ tour_model_eval/get_plot.py
@@ -1,34 +1,52 @@
 import matplotlib.pyplot as plt
+import emission.core.get_database as edb
+import emission.analysis.modelling.tour_model.get_users as gu
 import numpy as np
 import pandas as pd
 from matplotlib import cm
 import folium
 import branca.colormap as clm
+# import emission.analysis.modelling.tour_model.load_predict as predict
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
-def get_scatter(percentage,homo_score,valid_users):
-    x=percentage
-    y=homo_score
-    v=valid_users
-    cmp = cm.get_cmap('Dark2', len(valid_users))
-
+def get_scatter(valid_users,file_path,first_round,second_round):
     sc = []
-    for i in range(len(valid_users)):
-        for n in range(len(x[i])):
-            point = plt.scatter(x[i][n], y[i][n], color=cmp.colors[i], s=70, alpha=0.7)
-        sc.append(point)
-    plt.legend(sc,v,markerscale=0.8,scatterpoints=1,bbox_to_anchor=(1.23,1))
+    cmp = cm.get_cmap('Dark2', len(valid_users))
+    valid_users_ls = list(valid_users.keys())
+    for i,key in enumerate(valid_users_ls):
+        try:
+            df = pd.read_csv((file_path +
+                              'user_'+str(valid_users[key])+'.csv'), index_col='split')
+        except IOError as e:
+            continue
+        color = cmp.colors[i]
+        plot_scatter(df, sc, color, first_round, second_round)
+    plt.legend(sc, valid_users, markerscale=0.8, scatterpoints=1, bbox_to_anchor=(1.23, 1))
     plt.xlabel('user input request percentage',fontsize=16)
     plt.ylabel('homogeneity score',fontsize=16)
     plt.xticks(np.arange(0.4,1.1,step=0.1),fontsize=14)
     plt.yticks(np.arange(0.2,1.1,step=0.1),fontsize=14)
+    plt.show()
+
+
+def plot_scatter(result_df,sc,color,first_round,second_round):
+    if first_round:
+        x = result_df['percentage of 1st round']
+        y = result_df['homogeneity socre of 1st round']
+    elif second_round:
+        x = result_df['percentage of 2nd round']
+        y = result_df['homogeneity socre of 2nd round']
+    point = plt.scatter(x, y, color=color, s=70, alpha=0.7)
+    sc.append(point)
 
 
 def same_cluster_map(cluster,filter_trips,bins):
     color_map = clm.linear.Set1_07.to_step(len(bins), index=[i for i in range(len(bins)+1)])
     first_trip = filter_trips[cluster[0]]
     map = folium.Map(location=[first_trip.data.start_loc["coordinates"][1], first_trip.data.start_loc["coordinates"][0]],
-                   zoom_start=12, max_zoom=30, control_scale=True)
+                     zoom_start=12, max_zoom=30, control_scale=True)
 
     zoom_points = []
     for curr_trip_index in cluster:
@@ -52,3 +70,15 @@ def same_cluster_map(cluster,filter_trips,bins):
 
     return map
 
+if __name__ == '__main__':
+    # read user id from database
+    participant_uuid_obj = list(edb.get_profile_db().find({"install_group": "participant"}, {"user_id": 1, "_id": 0}))
+    all_users = [u["user_id"] for u in participant_uuid_obj]
+    radius = 100
+    user_ls, valid_users = gu.get_user_ls(all_users, radius)
+    # scatter plot from the result of the 1st round
+    plt.figure()
+    get_scatter(valid_users, first_round=True, second_round=False)
+    # scatter plot from the result of the 2nd round
+    plt.figure()
+    get_scatter(valid_users, first_round=False, second_round=True)
