30cb728f790b47964e5234f29e3b4e8716686302
Shankari <shankari@eecs.berkeley.edu>
2021-12-21 07:32:39 -0800

    Replace median_speed by mean_speed
    Keep a redirect from median_speed to mean_speed for backward compatibility
    We should remove this redirect in ~ 1 year.
    
    We primarily use the median speed for the calorie calculation, but:
    - it is not clear that we need the median speed
    - it is harder to calculate for trip-based entities
    https://github.com/e-mission/e-mission-docs/issues/688#issuecomment-998466419
    
    So we switch to the mean speed instead. This is currently the grand mean (sum
    of all distances / sum of all durations), but we can fairly easily change it to
    mean of means if we want.
    
    + remove _get_speeds_for_trip
    + fix answers in `testAllTimestampMetrics`
    + change `testAllTimestampMetrics` to retrieve both mean and median speeds for now
    
    Testing done:
    `testAllTimestampMetrics` passes

--- emission/analysis/result/metrics/simple_metrics.py
+++ emission/analysis/result/metrics/simple_metrics.py
@@ -14,7 +14,8 @@ def get_summary_fn(key):
         "count": get_count,
         "distance": get_distance,
         "duration": get_duration,
-        "median_speed": get_median_speed
+        "median_speed": get_median_speed,
+        "mean_speed": get_mean_speed
     }
     return summary_fn_map[key]
 
@@ -36,34 +37,15 @@ def get_duration(mode_section_grouped_df):
         ret_dict[mode] = float(mode_section_df.duration.sum())
     return ret_dict
 
+# Redirect from median to mean for backwards compatibility
+# TODO: Remove in Dec 2022
 def get_median_speed(mode_section_grouped_df):
+    return get_mean_speed(mode_section_grouped_df)
+
+def get_mean_speed(mode_section_grouped_df):
     ret_dict = {}
     for (mode, mode_section_df) in mode_section_grouped_df:
-        # print("while getting median speed %s, %s" % (mode, mode_section_df.columns))
-        if "speeds" in mode_section_df.columns:
-            speeds_list = mode_section_df.speeds
-        else:
-            # we are using the confirmed trips, which don't have the speed list
-            # let's get it by concatenating from the sections
-            speeds_list = mode_section_df.apply(_get_speeds_for_trip, axis=1)
-
-        # speeds series is a series with one row per section/trip where the
-        # value is the list of speeds in that section/trip
-        median_speeds = [pd.Series(sl).dropna().median() for sl
-                            in speeds_list]
-        mode_median = pd.Series(median_speeds).dropna().median()
-        if np.isnan(mode_median):
-            logging.debug("still found nan for mode %s, skipping")
-        else:
-            ret_dict[mode] = float(mode_median)
+        # mean_speeds is a series with one row per section/trip where the
+        # value is the mean speed (distance/duration) for that section/trip
+        ret_dict[mode] = float(mode_section_df.distance.sum() / mode_section_df.duration.sum())
     return ret_dict
-
-def _get_speeds_for_trip(trip_df_row):
-    import itertools
-    import emission.storage.decorations.trip_queries as esdt
-
-    section_list = esdt.get_cleaned_sections_for_trip(trip_df_row.user_id, trip_df_row.cleaned_trip)
-    logging.debug("Found %s matching sections for trip %s" % (len(section_list), trip_df_row._id))
-    speed_list_of_lists = [s["data"]["speeds"] for s in section_list]
-    speed_list = list(itertools.chain(*speed_list_of_lists))
-    return speed_list
