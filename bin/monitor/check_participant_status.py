89d53bf0573720d494aa64872b79f9363ef7a4b1
Shankari <shankari@eecs.berkeley.edu>
2022-08-09 22:51:56 -0700

    Print out the labeling % for the last month
    The printed string is getting a bit unweildy and we may want to split into
    multiple lines
    
    Note that this commit **will not** work on master or the gis branch until the
    leaderboard changes are also ported over to master/gis branch

--- bin/monitor/check_participant_status.py
+++ bin/monitor/check_participant_status.py
@@ -1,4 +1,7 @@
+import arrow
 import emission.core.get_database as edb
+import emission.core.wrapper.user as ecwu
+import emission.storage.timeseries.timequery as estt
 
 for ue in edb.get_uuid_db().find():
     trip_count = edb.get_analysis_timeseries_db().count_documents({"user_id": ue["uuid"], "metadata.key": "analysis/confirmed_trip"})
@@ -7,4 +10,8 @@ for ue in edb.get_uuid_db().find():
     first_trip_time = first_trip[0]["data"]["end_fmt_time"] if len(first_trip) > 0 else None
     last_trip = list(edb.get_analysis_timeseries_db().find({"user_id": ue["uuid"], "metadata.key": "analysis/confirmed_trip"}).sort("data.end_ts", -1).limit(1))
     last_trip_time = last_trip[0]["data"]["end_fmt_time"] if len(last_trip) > 0 else None
-    print(f"For {ue['user_email']}: Trip count = {trip_count}, location count = {location_count}, first trip = {first_trip_time}, last trip = {last_trip_time}")
+    now = arrow.now()
+    month_ago = now.shift(months=-1)
+    last_month_tq = estt.TimeQuery("data.start_ts", month_ago.timestamp, now.timestamp)
+    confirmed_pct, valid_replacement_pct, score = ecwu.User.computeConfirmed(ue["uuid"], last_month_tq)
+    print(f"For {ue['user_email']}: Trip count = {trip_count}, location count = {location_count}, first trip = {first_trip_time}, last trip = {last_trip_time}, confirmed_pct ({month_ago} -> {now}) = {confirmed_pct:.2f}")
