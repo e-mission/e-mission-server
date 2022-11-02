25c7fe4edd997af09be365c3c38800d7a6ebbd37
aGuttman <slaker117@gmail.com>
2022-11-02 09:08:01 -0600

    Update bin/auth/insert_tokens.py
    Delete old, commented-out code
    
    Co-authored-by: shankari <shankari@eecs.berkeley.edu>

--- bin/auth/insert_tokens.py
+++ bin/auth/insert_tokens.py
@@ -1,17 +1,6 @@
 import argparse
 import emission.storage.decorations.token_queries as esdt
 
-# def get_tokens_from_file(file):
-#     with open(file) as tlf:
-#         raw_token_list = tlf.readlines()
-#     token_list = [t.strip() for t in raw_token_list]
-#     return(token_list)
-
-# def load_tokens_into_db(token_list):
-#     esdt.insert_many_tokens(token_list)
-#     # for t in token_list:
-#     #     esdt.insert({"token":t})
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="insert_tokens")
 
