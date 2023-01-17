50a6d770155d335159bb3810bc54660c5fccca74
Shankari <shankari@eecs.berkeley.edu>
2023-01-17 15:02:46 -0800

    Rename all `pilot_ebike` to `e-bike`
    Back when we were collecting data using CanBikeCO, we used the `pilot_ebike`
    label for e-bike trips since they were the pilot we were evaluating.
    NREL OpenPATH is more generic and can (theoretically) be used for more
    different types of pilots, so we have now changed the label to `e-bike`.
    
    The public dashboard, in particular, has the mode of interest labeled as
    `e-bike`.
    
    So when we copied the smart commute data over from the CanBikeCO instance to
    NREL OpenPATH, we need to rename all the `pilot_ebike` -> `e-bike` to get the
    public dashboard to work properly.
    
    This repurposes some prior scripts to label air trips correctly. The main
    changes are to summarize the results because there were only a few air trips,
    but there are a lot of e-bike trips, at least in this cohort.
    
    Testing done:
    
    Before modification:
    
    ```
    __________ label predictions __________
    269 [ObjectId('611fdd9abc8ce4a9e3911bf6'), ObjectId('611fdd9abc8ce4a9e3911bf6'), ObjectId('611fdd9abc8ce4a9e3911bf6'), ObjectId('611fdd9bbc8ce4a9e3911c3e'), ObjectId('611fdd9bbc8ce4a9e3911c3e'), ObjectId('611fdd9bbc8ce4a9e3911c3e'), ObjectId('611fdd9bbc8ce4a9e3911c41'), ObjectId('611fdd9bbc8ce4a9e3911c41'), ObjectId('611fdd9bbc8ce4a9e3911c41'), ObjectId('611fdd9bbc8ce4a9e3911c44')]
    __________ inferred trips __________
    269 [ObjectId('611fdd9abc8ce4a9e3911bf5'), ObjectId('611fdd9abc8ce4a9e3911bf5'), ObjectId('611fdd9abc8ce4a9e3911bf5'), ObjectId('611fdd9bbc8ce4a9e3911c3d'), ObjectId('611fdd9bbc8ce4a9e3911c3d'), ObjectId('611fdd9bbc8ce4a9e3911c3d'), ObjectId('611fdd9bbc8ce4a9e3911c40'), ObjectId('611fdd9bbc8ce4a9e3911c40'), ObjectId('611fdd9bbc8ce4a9e3911c40'), ObjectId('611fdd9bbc8ce4a9e3911c43')]
    __________ expected trips __________
    269 [ObjectId('611fdda5bc8ce4a9e3911ff4'), ObjectId('611fdda5bc8ce4a9e3911ff4'), ObjectId('611fdda5bc8ce4a9e3911ff4'), ObjectId('611fdda5bc8ce4a9e391200c'), ObjectId('611fdda5bc8ce4a9e391200c'), ObjectId('611fdda5bc8ce4a9e391200c'), ObjectId('611fdda5bc8ce4a9e391200d'), ObjectId('611fdda5bc8ce4a9e391200d'), ObjectId('611fdda5bc8ce4a9e391200d'), ObjectId('611fdda5bc8ce4a9e391200e')]
    __________ confirmed trips __________
    269 [ObjectId('611fdda7bc8ce4a9e391214f'), ObjectId('611fdda7bc8ce4a9e391214f'), ObjectId('611fdda7bc8ce4a9e391214f'), ObjectId('611fdda9bc8ce4a9e3912167'), ObjectId('611fdda9bc8ce4a9e3912167'), ObjectId('611fdda9bc8ce4a9e3912167'), ObjectId('611fdda9bc8ce4a9e3912168'), ObjectId('611fdda9bc8ce4a9e3912168'), ObjectId('611fdda9bc8ce4a9e3912168'), ObjectId('611fdda9bc8ce4a9e3912169')]
    ```
    
    Modification
    
    ```
    ---------- inputs ----------
    __________ manual/mode_confirm __________
    {'n': 68, 'nModified': 68, 'ok': 1.0, 'updatedExisting': True}
    __________ analysis/confirmed_trip __________
    {'n': 61, 'nModified': 61, 'ok': 1.0, 'updatedExisting': True}
    ---------- inferred_labels ----------
    __________ label predictions __________
    Fixed 138 entries
    __________ inferred trips __________
    Fixed 138 entries
    __________ expected trips __________
    Fixed 138 entries
    __________ confirmed trips __________
    Fixed 138 entries
    ```
    
    After modification:
    
    ```
    __________ label predictions __________
    0 []
    __________ inferred trips __________
    0 []
    __________ expected trips __________
    0 []
    __________ confirmed trips __________
    0 []
    ```

---
+++ bin/historical/sc_2023_ebike_changes/modify_unique_user_inputs.py
@@ -0,0 +1,55 @@
+import emission.core.get_database as edb
+
+OLD_EBIKE_LABELS = ["pilot_ebike"]
+NEW_EBIKE_LABEL = "e-bike"
+
+print("-" * 10, "inputs", "-" * 10)
+print("_" * 10, "manual/mode_confirm", "_" * 10)
+print(edb.get_timeseries_db().update_many({"metadata.key": "manual/mode_confirm", "data.label": {"$in": OLD_EBIKE_LABELS}}, {"$set": {"data.label": "e-bike"}}).raw_result)
+print("_" * 10, "analysis/confirmed_trip", "_" * 10)
+print(edb.get_analysis_timeseries_db().update_many({"metadata.key": "analysis/confirmed_trip", "data.user_input.mode_confirm": {"$in": OLD_EBIKE_LABELS}}, {"$set": {"data.user_input.mode_confirm": "e-bike"}}).raw_result)
+print("-" * 10, "inferred_labels", "-" * 10)
+
+### See whether we have entries which need to be changed
+def has_inferred_air_labels(lpe):
+    for label_opt in lpe:
+        if "mode_confirm" in label_opt["labels"] and label_opt["labels"]["mode_confirm"] in OLD_EBIKE_LABELS:
+            return True
+    return False
+
+### Pass in only the entries which need to be changed
+def fix_inferred_labels(field, tp):
+    # print("Invoking with %s" % tp)
+    idx = tp["_id"]
+    lpe = tp["data"][field]
+    for label_opt in lpe:
+        # print(label_opt)
+        if "mode_confirm" in label_opt["labels"] and label_opt["labels"]["mode_confirm"] in OLD_EBIKE_LABELS:
+            label_opt["labels"]["mode_confirm"] = "e-bike"
+            data_field = "data.%s" % field
+            update_response = edb.get_analysis_timeseries_db().update_many({"_id": idx}, {"$set": {data_field: lpe}})
+            if update_response.matched_count != 1 or update_response.modified_count != 1:
+                print(f"Tried to update {idx}, got response {update_response.raw_result}")
+            # print("Update response %s" % edb.get_analysis_timeseries_db().update_many({"_id": idx}, {"$set": {data_field: lpe}}))
+
+def fix_label_field_if_needed(field, trip_projections):
+    fixed_entry_count = 0
+    for tp in trip_projections:
+        # print("Checking tp %s" % tp)
+        if field in tp["data"]:
+            if has_inferred_air_labels(tp["data"][field]):
+                # print("Fixing projection %s" % tp)
+                fix_inferred_labels(field, tp)
+                fixed_entry_count = fixed_entry_count + 1
+                # print("Fixed projection %s" % tp)
+    return "Fixed %s entries" % fixed_entry_count
+
+print("_" * 10, "label predictions", "_" * 10)
+print(fix_label_field_if_needed("prediction", list(edb.get_analysis_timeseries_db().find({"metadata.key": "inference/labels"},{"data.prediction": 1, "_id": 1}))))
+print("_" * 10, "inferred trips", "_" * 10)
+print(fix_label_field_if_needed("inferred_labels", list(edb.get_analysis_timeseries_db().find({"metadata.key": "analysis/inferred_trip"},{"data.inferred_labels": 1, "_id": 1}))))
+print("_" * 10, "expected trips", "_" * 10)
+print(fix_label_field_if_needed("inferred_labels", list(edb.get_analysis_timeseries_db().find({"metadata.key": "analysis/expected_trip"},{"data.inferred_labels": 1, "_id": 1}))))
+print("_" * 10, "confirmed trips", "_" * 10)
+print(fix_label_field_if_needed("inferred_labels", list(edb.get_analysis_timeseries_db().find({"metadata.key": "analysis/confirmed_trip"},{"data.inferred_labels": 1, "_id": 1}))))
+# print([e for e in edb.get_analysis_timeseries_db().find({"metadata.key": "analysis/confirmed_trip"}) if "inferred_labels" not in e["data"]])
