e-mission/e-mission-server

commit 63f3bb3cea0dd72f9c79a4b2abcad7938f70a796

Remove hack where programs starting with `stage` always default to token list
This was a hack added in
https://github.com/e-mission/e-mission-server/pull/889/commits/2a7f11312b5c7d69ce7b1efc01fdf8668a8eca89
from https://github.com/e-mission/e-mission-server/pull/889

Now that we have finished testing, we can remove the hack

emission/net/api/cfc_webapp.py
@@ -498,10 +498,6 @@ def resolve_auth(auth_method):
                 f"for {dynamic_config['intro']['translated_text']['en']['deployment_name']} "\
                 f"and data collection URL {dynamic_config['server']['connectUrl']}")
 
-            if STUDY_CONFIG.startswith('stage-'):
-                logging.debug("On staging, use token_list for testing purposes")
-                return "token_list"
-
             elif dynamic_config["intro"]["program_or_study"] == "program":
                 logging.debug("is a program set auth_method to token_list")
                 return "token_list"

commit ae839a6d40ff7b63419ae048c15ceb354f4171a5

Fix the `elif` check before any `if`
Fix regression caused by 63f3bb3cea0dd72f9c79a4b2abcad7938f70a796

Error

```
======================================================================
ERROR: netTests.TestWebserver (unittest.loader._FailedTest)
----------------------------------------------------------------------
ImportError: Failed to import test module: netTests.TestWebserver
Traceback (most recent call last):
  File "/home/runner/miniconda-23.1.0/envs/emissiontest/lib/python3.9/unittest/loader.py", line 436, in _find_test_path
    module = self._get_module_from_name(name)
  File "/home/runner/miniconda-23.1.0/envs/emissiontest/lib/python3.9/unittest/loader.py", line 377, in _get_module_from_name
    __import__(name)
  File "/home/runner/work/e-mission-server/e-mission-server/emission/tests/netTests/TestWebserver.py", line 21, in <module>
    import emission.net.api.cfc_webapp as enacw
  File "/home/runner/work/e-mission-server/e-mission-server/emission/net/api/cfc_webapp.py", line 502
    elif dynamic_config["intro"]["program_or_study"] == "program":
    ^
SyntaxError: invalid syntax
```

emission/net/api/cfc_webapp.py
@@ -499,7 +499,7 @@ def resolve_auth(auth_method):
                 f"for {dynamic_config['intro']['translated_text']['en']['deployment_name']} "\
                 f"and data collection URL {dynamic_config['server']['connectUrl']}")
 
-            elif dynamic_config["intro"]["program_or_study"] == "program":
+            if dynamic_config["intro"]["program_or_study"] == "program":
                 logging.debug("is a program set auth_method to token_list")
                 return "token_list"
             else: