7cc1989b41c3ddf24ecb62ab2450fbcb26673334
Jack Greenlee <greenljk@mail.uc.edu>
2023-05-20 00:30:03 -0400

    include 'sections' in data model + remove extras
    All we need is a list of cleaned_section entries. These entries have information about the sensed mode for those sections.
    Since CompositeTrip inherits from ConfirmedTrip, we already have information about the inferred mode of the trip as a whole.
    Thus, we don't need all this other stuff.

--- emission/core/wrapper/compositetrip.py
+++ emission/core/wrapper/compositetrip.py
@@ -16,19 +16,8 @@ class Compositetrip(ecwc.Confirmedtrip):
                   "end_confirmed_place": ecwb.WrapperBase.Access.WORM, # object contains all properties for the destination confirmed_place object
                   "confirmed_trip": ecwb.WrapperBase.Access.WORM, # the id of the corresponding confirmed trip
                   "locations": ecwb.WrapperBase.Access.WORM, # object containing cleaned location entries (max 100)
-                  #      # sections stuff
-                  "cleaned_section": ecwb.WrapperBase.Access.WORM,
-                  "inferred_section": ecwb.WrapperBase.Access.WORM,
-                  "inferred_mode": ecwb.WrapperBase.Access.WORM, # inferred by mode inference algo
-                  "confirmed_mode": ecwb.WrapperBase.Access.WORM, # confirmed by user
-# mode to be used for analysis; confirmed mode if we know factors for it, inferred mode otherwise
-                  "analysis_mode": ecwb.WrapperBase.Access.WORM,
-# mode for user display; inferred mode if not confirmed; confirmed mode otherwise
-                  "display_mode": ecwb.WrapperBase.Access.WORM
+                  "sections": ecwb.WrapperBase.Access.WORM, # list containing cleaned sections during the trip
     })
-    #      # sections stuff
-    enums = {"inferred_mode": ecwm.PredictedModeTypes,
-             "analysis_mode": ecwm.PredictedModeTypes}
 
     def _populateDependencies(self):
         super(Compositetrip, self)._populateDependencies()
