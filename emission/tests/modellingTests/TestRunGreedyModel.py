3372604f5efcd9b13927612b1b02fb8817ef21e6
Mahadik, Mukul Chandrakant <MukulChandrakant.Mahadik@nrel.gov>
2023-12-01 16:39:39 -0700

    Passed user_id in TestRunGreedyModel.py
    Failing test due to missed user_id parameter.
    Passed user_id to rectify.

--- emission/tests/modellingTests/TestRunGreedyModel.py
+++ emission/tests/modellingTests/TestRunGreedyModel.py
@@ -165,7 +165,7 @@ class TestRunGreedyModel(unittest.TestCase):
         )
 
         model = eamur._load_stored_trip_model(
-            user_id, 
+            user_id=self.user_id,
             model_type=eamumt.ModelType.GREEDY_SIMILARITY_BINNING,
             model_storage=eamums.ModelStorage.DOCUMENT_DATABASE,
             model_config=greedy_model_config
