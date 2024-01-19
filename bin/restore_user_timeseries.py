28719f400f0f0af109f3da43ed18bb78b4c7360c
Mahadik, Mukul Chandrakant <MukulChandrakant.Mahadik@nrel.gov>
2024-01-19 11:35:48 -0700

    Added import options
    Import file type added as command line argument with appropriate functions.

--- bin/restore_user_timeseries.py
+++ bin/restore_user_timeseries.py
@@ -13,14 +13,8 @@ from bson.binary import Binary
 from bson import ObjectId
 import json
 
-def restoreUserTimeseries(filename):
-    # df = pd.read_csv(filename)
-    # df['_id'] = df['_id'].apply(lambda x: ObjectId(x))
-    # data = df.to_dict(orient='records')
-    # print(df)
-    # result = edb.get_timeseries_db().insert_many(data)
-
-    logging.info("Importing data from file...")
+def importOldTimeseriesFromJson(filename):
+    logging.info("Importing data from JSON...")
     with open(filename, 'r') as file:
         data = json.load(file)
 
@@ -28,6 +22,21 @@ def restoreUserTimeseries(filename):
     for document in data:
         document["_id"] = ObjectId(document["_id"])
         document["user_id"] = Binary(uuid.UUID(document["user_id"]).bytes, 0x03)
+        print(document["user_id"])
+    logging.info("Old timeseries data loaded from JSON...")
+    return data
+
+importOptions = {
+    'json_import': importOldTimeseriesFromJson
+}
+
+def restoreUserTimeseries(importFileFlags, filename):
+    for key in importFileFlags:
+        logging.info(f"{key} = {importFileFlags[key]}")
+        if importFileFlags[key] is True:
+            data = importOptions[key](filename)
+            logging.info("Printing data to be inserted...")
+            break
 
     logging.info("Inserting data into database...")
     result = edb.get_timeseries_db().insert_many(data)
@@ -37,10 +46,19 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
     parser = argparse.ArgumentParser(prog="restore_user_timeseries")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "--json_import", 
+        help="Importing from JSON file",
+        action='store_true'
+    )
     parser.add_argument(
         "-f", "--file_name", 
-        help="Path to the CSV file containing data to be imported"
+        help="Path to the JSON file containing data to be imported"
     )
 
     args = parser.parse_args()
-    restoreUserTimeseries(args.file_name)
+    importFileFlags = {
+        'json_import': args.json_import if args.json_import is not None else False
+    }
+    restoreUserTimeseries(importFileFlags, args.file_name)
