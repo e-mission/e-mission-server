b1b0698a4e12890366b7f06599e7d4bf411e0adc
Natalie Schultz <90212258+nataliejschultz@users.noreply.github.com>
2024-02-07 11:56:16 -0700

    Adding shell script and workflow file
    Workflow file runs the shell script.
    
    Shell script sets up emission environment and runs TestOverpass.py.

---
+++ emission/individual_tests/setup_overpass_tests.sh
@@ -0,0 +1,29 @@
+#Set up the testing environment
+# Using an automated install 
+cd /src/e-mission-server
+
+#set database URL using environment variable
+echo ${DB_HOST}
+if [ -z ${DB_HOST} ] ; then
+    local_host=`hostname -i`
+    sed "s_localhost_${local_host}_" conf/storage/db.conf.sample > conf/storage/db.conf
+else
+    sed "s_localhost_${DB_HOST}_" conf/storage/db.conf.sample > conf/storage/db.conf
+fi
+cat conf/storage/db.conf
+
+echo "Setting up conda..."
+source setup/setup_conda.sh Linux-x86_64
+
+echo "Setting up the test environment..."
+source setup/setup_tests.sh
+
+echo "Running tests..."
+source setup/activate_tests.sh
+
+echo "Adding permissions for the runIntegrationTests.sh script"
+chmod +x runIntegrationTests.sh
+echo "Permissions added for the runIntegrationTests.sh script"
+
+set -e
+PYTHONPATH=. python -m unittest emission/individual_tests/TestOverpass.py
