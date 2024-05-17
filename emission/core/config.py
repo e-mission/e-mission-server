a0f2424308ad2b3ccad9791f66aae73b4f0e529c
Natalie Schultz <90212258+nataliejschultz@users.noreply.github.com>
2024-05-17 12:08:24 -0600

    Removing check_unset_env_vars
    Removing the functionality of check_unset_env_vars. DB_HOST should be caught in start_script.sh. When I rebuilt the image without this functionality and ran the container, all tests passed.

--- emission/core/config.py
+++ emission/core/config.py
@@ -11,24 +11,12 @@ def get_config_data_from_env():
     }
     return config_data_env
 
-def check_unset_env_vars():
-    config_data_env = {
-        "timeseries": {
-            "url": os.getenv('DB_HOST'),
-            "result_limit": os.getenv('DB_TS_RESULT_LIMIT')
-        }
-    }
-    return not any(config_data_env.values())
-
 def get_config_data():
     try:
         config_file = open('conf/storage/db.conf')
         ret_val = json.load(config_file)
         config_file.close()
     except:
-        # Check if all DB environment variables are not set
-        # if check_unset_env_vars():
-            # print("All DB environment variables are set to None")
         ret_val = get_config_data_from_env()
         if ret_val["timeseries"]["url"] == "localhost":
             print("storage not configured, falling back to sample, default configuration")
