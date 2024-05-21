d11d529e57443f8fe79a6335a98564864fd1fac6
Jack Greenlee <greenljk@mail.uc.edu>
2024-05-21 16:52:34 -0400

    support 'yyyy_mm_dd' time type in 'result/metrics' endpoint
    The updated in-app dashboard will be using this new time type via `/result/metrics/yyyy_mm_dd`.
    When using this time type, `e-mission-common` is used for the metrics calculations, which has a new structure (https://github.com/e-mission/e-mission-server/pull/966#issuecomment-2119705314)
    `start_time` and `end_time` are given as ISO dates

--- emission/net/api/metrics.py
+++ emission/net/api/metrics.py
@@ -10,15 +10,30 @@ import logging
 
 import emission.analysis.result.metrics.time_grouping as earmt
 import emission.analysis.result.metrics.simple_metrics as earms
+import emission.storage.decorations.analysis_timeseries_queries as esda
+import emission.storage.decorations.local_date_queries as esdl
+import emission.storage.timeseries.tcquery as esttc
 
-def summarize_by_timestamp(user_id, start_ts, end_ts, freq, metric_list, include_aggregate):
+import emcommon.metrics.metrics_summaries as emcms
+
+def summarize_by_timestamp(user_id, start_ts, end_ts, freq, metric_list, include_aggregate, app_config=None):
     return _call_group_fn(earmt.group_by_timestamp, user_id, start_ts, end_ts,
                           freq, metric_list, include_aggregate)
 
-def summarize_by_local_date(user_id, start_ld, end_ld, freq_name, metric_list, include_aggregate):
+def summarize_by_local_date(user_id, start_ld, end_ld, freq_name, metric_list, include_aggregate, app_config=None):
     local_freq = earmt.LocalFreq[freq_name]
     return _call_group_fn(earmt.group_by_local_date, user_id, start_ld, end_ld,
                           local_freq, metric_list, include_aggregate)
+
+def summarize_by_yyyy_mm_dd(user_id, start_ymd, end_ymd, freq, metric_list, include_agg, app_config):
+    time_query = esttc.TimeComponentQuery(
+        "data.start_local_dt",
+        esdl.yyyy_mm_dd_to_local_date(start_ymd),
+        esdl.yyyy_mm_dd_to_local_date(end_ymd)
+    )
+    trips = esda.get_entries(esda.COMPOSITE_TRIP_KEY, None, time_query)
+    print('found ' + str([e for e in trips]))
+    return emcms.generate_summaries(metric_list, trips, app_config)
 
 def _call_group_fn(group_fn, user_id, start_time, end_time, freq, metric_list, include_aggregate):
     summary_fn_list = [earms.get_summary_fn(metric_name)
