da485c257cdf955f9e44b325ebd967a71c42633b
Natalie Schultz <90212258+nataliejschultz@users.noreply.github.com>
2024-05-23 16:57:35 -0600

    Removing redundant DB_HOST setting
    DB_HOST is hardcoded in the compose file that runs this script anyway.

--- emission/integrationTests/start_integration_tests.sh
+++ emission/integrationTests/start_integration_tests.sh
@@ -2,13 +2,7 @@
 # Using an automated install 
 cd /src/e-mission-server
 
-#set database URL using environment variable
 echo ${DB_HOST}
-if [ -z ${DB_HOST} ] ; then
-    local_host=`hostname -i`
-    export DB_HOST=$local_host
-    echo "Setting db host environment variable to localhost"
-fi
 cat conf/storage/db.conf
 
 echo "Setting up conda..."
