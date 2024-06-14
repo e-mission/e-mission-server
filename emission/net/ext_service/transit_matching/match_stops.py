b1f7d4feb7e9f7bd5d338ded9649de6a56f9c7cd
Natalie Schultz <90212258+nataliejschultz@users.noreply.github.com>
2024-06-14 17:34:22 -0600

    Printing request url

--- emission/net/ext_service/transit_matching/match_stops.py
+++ emission/net/ext_service/transit_matching/match_stops.py
@@ -27,7 +27,7 @@ RETRY = -1
 def make_request_and_catch(overpass_query):
     try:
         response = requests.post(url + "api/interpreter", data=overpass_query)
-        print("*********RESPONSE:" , response.text)
+        print("*********RESPONSE:" , response.request.url)
     except requests.exceptions.ChunkedEncodingError as e:
         logging.info("ChunkedEncodingError while creating request %s" % (e))
         time.sleep(10)
