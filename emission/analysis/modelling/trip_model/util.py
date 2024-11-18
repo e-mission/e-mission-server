419babb9aa3ed6df9514e0f407e86da97efb00dd
Mahadik, Mukul Chandrakant <MukulChandrakant.Mahadik@nrel.gov>
2024-11-17 23:50:16 -0800

    Reverting changes; utility functions already present in clustering.py
    Utility functions were added in this commit from the original PR
    https://github.com/e-mission/e-mission-server/commit/e9abd5173255a3d68539d44e73bdf4b30674fea8#diff-e910051a05987388fa06c96f484c23b3e491408a61136ed62235117c445fd664
    
    However, these are already present in clustering.py, which is also imported and used in models.py.
    But util.py functions are not being used anywhere.

--- emission/analysis/modelling/trip_model/util.py
+++ emission/analysis/modelling/trip_model/util.py
@@ -1,7 +1,6 @@
 from typing import List, Tuple
 from past.utils import old_div
-import numpy as np
-import pandas as pd
+import numpy
 from numpy.linalg import norm
 
 
@@ -27,47 +26,16 @@ def find_knee_point(values: List[float]) -> Tuple[float, int]:
     x = list(range(N))
     max = 0
     index = -1
-    a = np.array([x[0], values[0]])
-    b = np.array([x[-1], values[-1]])
+    a = numpy.array([x[0], values[0]])
+    b = numpy.array([x[-1], values[-1]])
     n = norm(b - a)
     new_y = []
     for i in range(0, N):
-        p = np.array([x[i], values[i]])
-        dist = old_div(norm(np.cross(p - a, p - b)), n)
+        p = numpy.array([x[i], values[i]])
+        dist = old_div(norm(numpy.cross(p - a, p - b)), n)
         new_y.append(dist)
         if dist > max:
             max = dist
             index = i
     value = values[index]
     return [index, value]
-
-    def get_distance_matrix(loc_df, loc_type):
-        """ Args:
-                loc_df (dataframe): must have columns 'start_lat' and 'start_lon' 
-                    or 'end_lat' and 'end_lon'
-                loc_type (str): 'start' or 'end'
-        """
-        assert loc_type == 'start' or loc_type == 'end'
-
-        radians_lat_lon = np.radians(loc_df[[loc_type + "_lat", loc_type + "_lon"]])
-
-        dist_matrix_meters = pd.DataFrame(
-            smp.haversine_distances(radians_lat_lon, radians_lat_lon) *
-            EARTH_RADIUS)
-        return dist_matrix_meters
-
-def single_cluster_purity(points_in_cluster, label_col='purpose_confirm'):
-    """ Calculates purity of a cluster (i.e. % of trips that have the most 
-        common label)
-    
-        Args:
-            points_in_cluster (df): dataframe containing points in the same 
-                cluster
-            label_col (str): column in the dataframe containing labels
-    """
-    assert label_col in points_in_cluster.columns
-
-    most_freq_label = points_in_cluster[label_col].mode()[0]
-    purity = len(points_in_cluster[points_in_cluster[label_col] ==
-                                   most_freq_label]) / len(points_in_cluster)
-    return purity
