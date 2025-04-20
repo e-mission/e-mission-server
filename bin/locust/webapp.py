95e5c48905137060e308b7b1f4c0864337e1e55f
Shankari <shankari@eecs.berkeley.edu>
2025-04-20 01:06:12 -0700

    ✨ Add code to cover additional test scenarios
    We now support:
    - mongodump in src_db used to populate randomly created users in dest db (existing)
    - mongodump in src db used to populate single user in dest db (test against staging)
    - mongodump in connected db used to read (and potentially pull and rewrite)
    
    Changes:
    - add functions for the three use cases; users will choose one of them `on_start`
    - pulled out the wait code into a separate function that can be enabled as needed
    - support reading either locations or composite entries and querying for a week
      or forever depending on the key
    - add a new function to read the metrics, both user level and aggregate

--- bin/locust/webapp.py
+++ bin/locust/webapp.py
@@ -14,22 +14,56 @@ def generate_random_string(length=12):
     characters = string.ascii_letters + string.digits
     return ''.join(secrets.choice(characters) for _ in range(length))
 
+def random_src_random_created_dest(self):
+    self.opcode = generate_random_string(12)
+    self.client.post("/profile/create", json={"user": self.opcode})
+    self.src_uuid = secrets.choice(uuid_db_list)["uuid"]
+    print(f"Connecting existing user {self.src_uuid=} to opcode {self.opcode=}")
+    self.key_to_read = "background/filtered_location"
+
+def random_src_single_existing_dest(self):
+    self.opcode = "some_dest_opcode_REPLACEME"
+    self.src_uuid = secrets.choice(uuid_db_list)["uuid"]
+    self.key_to_read = "background/filtered_location"
+    print(f"Connecting existing source user {self.src_uuid=} to existing dest opcode {self.opcode=}")
+
+def same_src_dest_random(self):
+    # we can't really copy data from a different user in the same program
+    # because it may overlap with the existing users' data and mess everything up
+    # so we just copy the data from the same user if that task is enabled
+    chosen_user = secrets.choice(uuid_db_list)
+    self.opcode = chosen_user["user_email"]
+    self.src_uuid = chosen_user["uuid"]
+    self.key_to_read = "analysis/composite_trip"
+    print(f"Simulating existing user with {self.src_uuid=} and {self.opcode=}")
+
+def backoff_wait_time(self):
+    self.iteration = self.iteration + 1
+    if self.iteration % 100 == 0:
+        print(f"Iteration {self.iteration}, wait time = {self.iteration / 100}")
+    return self.iteration / 100
+
+uuid_db_list = list(edb.get_uuid_db().find())
+
 class PhoneAppUser(HttpUser):
+    # host = "https://openpath-stage.nrel.gov/api"
     host = "http://nginxrp/api"
     iteration = 0
-    uuid_db_list = list(edb.get_uuid_db().find())
 
     def wait_time(self):
-        self.iteration = self.iteration + 1
-        if self.iteration % 100 == 0:
-            print(f"Iteration {self.iteration}, wait time = {self.iteration / 100}")
-        return self.iteration / 100    
+        return backoff_wait_time(self)
+        # return 5
     
     def on_start(self):
-       self.opcode = generate_random_string(12)
-       self.client.post("/profile/create", json={"user": self.opcode})
-       self.src_uuid = secrets.choice(self.uuid_db_list)["uuid"]
-       print(f"Connecting existing user {self.src_uuid=} to opcode {self.opcode=}")
+        # use this to copy data from an existing dump to a new dummy local
+        # program, this will focus on writes
+        # random_src_random_created_dest(self)
+        # use this with staging, remember to fill in the single opcode
+        # random_src_single_existing_dest(self)
+        # use this with when the existing dump has already been loaded in the
+        # database under load; this will focus on reads
+        same_src_dest_random(self)
+
 
     @task(3)
     def profile_update(self):
@@ -43,10 +77,12 @@ class PhoneAppUser(HttpUser):
 
     @task(2)
     def find_entries_timestamp(self):
+       start_ts = 0 if self.key_to_read == "background/location" else \
+            arrow.utcnow().shift(days=-7).timestamp()
        self.client.post("/datastreams/find_entries/timestamp",
                         json={"user": self.opcode,
-                              "key_list": ["background/location"],
-                              "start_time": 0,
+                              "key_list": [self.key_to_read],
+                              "start_time": start_ts,
                               "end_time": arrow.utcnow().timestamp()})
 
     @task
@@ -60,3 +96,20 @@ class PhoneAppUser(HttpUser):
        wrapped_json = {"user": self.opcode, "phone_to_server": entries}
        self.client.post("/usercache/put", data=esj.wrapped_dumps(wrapped_json),
                         headers={"Content-Type": "application/json"})
+
+    @task
+    def metrics(self):
+       DEFAULT_METRIC_LIST = {
+           'footprint': ['mode_confirm'],
+           'distance': ['mode_confirm'],
+           'duration': ['mode_confirm'],
+           'count': ['mode_confirm'],
+       }
+       wrapped_json = {"user": self.opcode,
+          "metric_list": DEFAULT_METRIC_LIST,
+          "start_time": arrow.utcnow().shift(days=-7).isoformat(),
+          "end_time": arrow.utcnow().isoformat(),
+          "is_return_aggregate": True if self.iteration % 2 == 0 else False,
+          "freq": "D"}
+       self.client.post("/result/metrics/yyyy_mm_dd", data=esj.wrapped_dumps(wrapped_json),
+                         headers={"Content-Type": "application/json"})
