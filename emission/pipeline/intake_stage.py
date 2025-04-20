8859260f92713f2af5669ba627d71f889110b2d9
Shankari <shankari@eecs.berkeley.edu>
2025-04-20 14:42:55 -0700

    👔 Improve the logic to whether we should skip the pipeline
    Before this, we used to skip the pipeline run in production settings if there
    was no new data. But if the pipeline is unstable, we can have people whose
    pipeline stopped running, and then they uninstalled the app. When we reset the
    pipeline, it will never re-run because there is no new data, potentially
    leading to a lot of unprocessed data sitting around.
    
    Now that we store additional metrics in the profile, we can use that for a more
    principled check that will work in both production and dev settings.
    
    The check is simple:
    - if there is unprocessed data, run the pipeline, otherwise skip
    - the check for unprocessed data is "do we have any location points beyond the end of the pipeline"?
    
    Note that, since there is a little bit of fuzz in trip end detection, the check is also fuzzy.
    For example, if I stop moving at 3:00pm, the app will continue collecting data
    until 3:10 so that it can see that I have stopped moving; otherwise, it will
    stop at every stoplight, which is not great. But then if we end the trip at
    3:00pm (or 3:05), it will look like the data from 3:05 to 3:10 is unprocessed,
    which will cause us to re-run the pipeline and spin our wheels.
    
    The fuzz allows us to fix this:
    - if the user is dormant, we will miss any trips in the last 10 mins, which is NBD
    - if the user is not dormant, we will handle the locations as part of the next run
    
    This is a partial fix for
    https://github.com/e-mission/e-mission-docs/issues/1105#issuecomment-2798124327
    
    Justification for test changes:
    - change common to set the `last_location_ts` and the `pipeline_range` while
      setting up real examples since:
        - they are now required for the pipeline
        - we expect that if there is any data in the database, at least
          `last_location_ts` will be filled in, so having it be filled in ensures a
          consistent state
    - In `TestPipelineCornerCases.py`, where we used to manually toggle the
      `skip_..` flag instead of having it be consistent, we now manually set the profile
      to skip or not skip the pipeline run. Again, this will not happen in
      practice, but we are testing corner cases here.
    
    Testing done:
    - Ran against a local dev server copy with a mixture of situations
    
    ```
    For uuid=UUID('...'), last location entry is at 2025-04-03T03:42:00.507721+00:00(1743651720.507721), pipeline has run until 2025-04-03T03:42:00.507721+00:00(1743651720.507721), difference = 0:00:00, skipping
    For uuid=UUID('...'), last location entry is at 2025-03-19T21:02:02.588467+00:00(1742418122.588467), pipeline has run until 2025-03-19T21:02:02.588467+00:00(1742418122.588467), difference = 0:00:00, skipping
    2025-04-20 21:38:42,192:INFO:140737476424192:For uuid=UUID('...'), last location entry is at 2025-04-03T22:22:59.341000+00:00(1743718979.341), pipeline has run until 2025-04-01T18:16:29.187000+00:00(1743531389.187), difference = 2 days, 4:06:30.154000, continuing
    ```

--- emission/pipeline/intake_stage.py
+++ emission/pipeline/intake_stage.py
@@ -42,7 +42,7 @@ import emission.storage.decorations.stats_queries as esds
 import emission.core.wrapper.user as ecwu
 import emission.analysis.result.user_stat as eaurs
 
-def run_intake_pipeline(process_number, uuid_list, skip_if_no_new_data=False):
+def run_intake_pipeline(process_number, uuid_list):
     """
     Run the intake pipeline with the specified process number and uuid list.
     Note that the process_number is only really used to customize the log file name
@@ -75,13 +75,32 @@ def run_intake_pipeline(process_number, uuid_list, skip_if_no_new_data=False):
             continue
 
         try:
-            run_intake_pipeline_for_user(uuid, skip_if_no_new_data)
+            run_intake_pipeline_for_user(uuid)
         except Exception as e:
             esds.store_pipeline_error(uuid, "WHOLE_PIPELINE", time.time(), None)
             logging.exception("Found error %s while processing pipeline "
                               "for user %s, skipping" % (e, uuid))
 
-def run_intake_pipeline_for_user(uuid, skip_if_no_new_data):
+def run_intake_pipeline_for_user(uuid):
+        user_profile = edb.get_profile_db().find_one({"user_id": uuid})
+        # Before this, we used to use a check of "have I moved new entries to long-term"
+        # But that fails if a pipeline has been stuck for a long time, and the user is dormant
+        # Since we added profile-level metrics to display in the admin
+        # dashboard, we can now use those for a more principled check. But in
+        # trip segmentation, we elide the last few location points since there
+        # is a delay of 5-10 minutes while detecting trip end.
+        # - if the user is dormant, we will miss any trips in the last 10 mins, which is NBD
+        # - if the user is not dormant, we will handle the locations as part of the next run
+        last_loc_ts = arrow.get(user_profile.get('last_location_ts', 0))
+        last_proc_time = user_profile.get('pipeline_range', {}).get('end_ts', None)
+        last_proc_ts = arrow.get(last_proc_time) if last_proc_time is not None else arrow.get(0)
+        fmt_string = f"For {uuid=}, last location entry is at {last_loc_ts}({last_loc_ts.timestamp()}), pipeline has run until {last_proc_ts}({last_proc_ts.timestamp()}), difference = {last_loc_ts - last_proc_ts}({(last_loc_ts.timestamp() - last_proc_ts.timestamp())})"
+        if  (last_loc_ts.timestamp() - last_proc_ts.timestamp()) <= 10 * 60: # 10 minutes
+            print(f"{fmt_string}, skipping")
+            return
+        else:
+            logging.info(f"{fmt_string}, continuing")
+
         uh = euah.UserCacheHandler.getUserCacheHandler(uuid)
 
         with ect.Timer() as uct:
@@ -91,13 +110,6 @@ def run_intake_pipeline_for_user(uuid, skip_if_no_new_data):
 
         esds.store_pipeline_time(uuid, ecwp.PipelineStages.USERCACHE.name,
                                  time.time(), uct.elapsed)
-
-        if skip_if_no_new_data and new_entry_count == 0:
-            print("No new entries, and skip_if_no_new_data = %s, skipping the rest of the pipeline" % skip_if_no_new_data)
-            return
-        else:
-            print("New entry count == %s and skip_if_no_new_data = %s, continuing" %
-                (new_entry_count, skip_if_no_new_data))
 
         with ect.Timer() as uit:
             logging.info("*" * 10 + "UUID %s: updating incoming user inputs" % uuid + "*" * 10)
--- emission/tests/common.py
+++ emission/tests/common.py
@@ -134,6 +134,8 @@ def createAndFillUUID(testObj):
     else:
         logging.info("No reg email found, not registering email")
         testObj.testUUID = uuid.uuid4()
+    # ensure that there is unprocessed data so that the pipeline will actually be run
+    edb.get_profile_db().update_one({"user_id": testObj.testUUID}, {"$set": {"last_location_ts": 3650, "pipeline_range": {"end_ts": None}}}, upsert=True)
 
 def setupRealExample(testObj, dump_file):
     logging.info("Before loading from %s, timeseries db size = %s" %
