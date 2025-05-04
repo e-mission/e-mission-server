bed56e2f3fabf03528fca03e601724199b27968a
K. Shankari <k.shankari@nrel.gov>
2025-05-03 21:22:27 -0700

    ♻️  Refactor and simplify the conda install script
    - Support specifying an install prefix
    - Since we now have three input variables, restructure the input checks and add
      new ones (suggested by copilot)
    
    I also tried to remove the mamba-solver installation (`conda install -n base
    conda-libmamba-solver`) since it is included by default
    
    https://conda.github.io/conda-libmamba-solver/user-guide/
    
    > If you have a recent conda (23.10 or later), you don’t have to do anything. conda-libmamba-solver is already preconfigured as default. For older versions conda, we simply recommend updating conda to a more recent version:
    
    However, removing it cause the tests to fail with
    
    ```
    ======================================================================
    ERROR: analysisTests.modeinferTests.TestPipeline (unittest.loader._FailedTest.analysisTests.modeinferTests.TestPipeline)
    ----------------------------------------------------------------------
    ImportError: Failed to import test module: analysisTests.modeinferTests.TestPipeline
    Traceback (most recent call last):
      File "/home/runner/miniconda-25.1.1/envs/emissiontest/lib/python3.11/unittest/loader.py", line 419, in _find_test_path
        module = self._get_module_from_name(name)
                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/home/runner/miniconda-25.1.1/envs/emissiontest/lib/python3.11/unittest/loader.py", line 362, in _get_module_from_name
        __import__(name)
      File "/home/runner/work/e-mission-server/e-mission-server/emission/tests/analysisTests/modeinferTests/TestPipeline.py", line 22, in <module>
        import emission.core.wrapper.localdate as ecwl
      File "/home/runner/work/e-mission-server/e-mission-server/emission/core/wrapper/localdate.py", line 10, in <module>
        import emission.core.wrapper.wrapperbase as ecwb
      File "/home/runner/work/e-mission-server/e-mission-server/emission/core/wrapper/wrapperbase.py", line 9, in <module>
        import attrdict as ad
      File "/home/runner/miniconda-25.1.1/envs/emissiontest/lib/python3.11/site-packages/attrdict/__init__.py", line 5, in <module>
        from attrdict.mapping import AttrMap
      File "/home/runner/miniconda-25.1.1/envs/emissiontest/lib/python3.11/site-packages/attrdict/mapping.py", line 4, in <module>
        from collections import Mapping
    ImportError: cannot import name 'Mapping' from 'collections' (/home/runner/miniconda-25.1.1/envs/emissiontest/lib/python3.11/collections/__init__.py)
    ```
    
    It looks like including the libmambasolver installs python 3.9 instead, which
    works. We will need to upgrade to a later version of python, and handle the
    mapping issue, but punting on it for now...
    
    Testing done:
    - Used to install the new miniconda for c4333fdad09c1ba57ab0d541f8bcd36d28ecb9f0

--- setup/setup_conda.sh
+++ setup/setup_conda.sh
@@ -1,25 +1,44 @@
+if [[ ! -f setup/export_versions.sh ]]; then
+    echo "Error: setup/export_versions.sh file is missing. Please ensure it exists before running this script."
+    exit 1
+fi
+
 source setup/export_versions.sh
+if [[ $? -ne 0 ]]; then
+    echo "Error: Failed to source setup/export_versions.sh. Please check the file for errors."
+    exit 1
+fi
+
+if [[ -z $EXP_CONDA_VER ]]; then
+    echo "Error: Requires the EXP_CONDA_VER variable to be set"
+    echo "Please check the 'export_versions.sh' script and ensure that it works"
+    exit 1
+fi
 
 PLATFORM=$1
-echo "Installing for version $EXP_CONDA_VER and platform $PLATFORM"
+INSTALL_PREFIX=$2
 
-if [[ -z $EXP_CONDA_VER || -z $PLATFORM ]]; then
-    echo "Usage: setup_conda.sh <platform>"
-    echo "   Assumes that the EXP_CONDA_VER variable is set"
-    echo "   Platform options are Linux-x86_64, MacOSX-x86_64"
-    echo "   For Windows, manually download and install https://repo.anaconda.com/miniconda/Miniconda3-py39_$EXP_CONDA_VER-$EXP_CONDA_VER_SUFFIX-Windows-x86_64.exe"
-else
-    INSTALL_PREFIX=$HOME/miniconda-$EXP_CONDA_VER
-    SOURCE_SCRIPT="$HOME/miniconda-$EXP_CONDA_VER/etc/profile.d/conda.sh"
-    
-    curl -o miniconda.sh -L https://repo.anaconda.com/miniconda/Miniconda3-py39_$EXP_CONDA_VER-$EXP_CONDA_VER_SUFFIX-$PLATFORM.sh;
-    bash miniconda.sh -b -p $INSTALL_PREFIX
-    source $SOURCE_SCRIPT
-    hash -r
-    conda install -n base conda-libmamba-solver
-    conda config --set solver libmamba
-    conda config --set always_yes yes
-    # Useful for debugging any issues with conda
-    conda info -a
-    echo "Successfully installed at $INSTALL_PREFIX. Please activate with 'source setup/activateXXX.sh' in every terminal where you want to use conda" 
+if [[ -z $PLATFORM ]]; then
+    echo "Usage: setup_conda.sh <platform> [install_prefix]"
+    echo "   Platform options are Linux-x86_64, MacOSX-x86_64, MacOSX-arm64"
+    WINDOWS_INSTALLER_URL="https://repo.anaconda.com/miniconda/Miniconda3-py39_$EXP_CONDA_VER-$EXP_CONDA_VER_SUFFIX-Windows-x86_64.exe"
+    echo "   For Windows, manually download and install $WINDOWS_INSTALLER_URL"
+    exit 2
 fi
+
+if [[ -z $INSTALL_PREFIX ]]; then
+    INSTALL_PREFIX=$HOME/miniconda-$EXP_CONDA_VER
+fi
+SOURCE_SCRIPT="$INSTALL_PREFIX/etc/profile.d/conda.sh"
+
+echo "Installing for version $EXP_CONDA_VER and platform $PLATFORM at $INSTALL_PREFIX"
+curl -o miniconda.sh -L https://repo.anaconda.com/miniconda/Miniconda3-py312_$EXP_CONDA_VER-$EXP_CONDA_VER_SUFFIX-$PLATFORM.sh
+bash miniconda.sh -b -p $INSTALL_PREFIX
+source $SOURCE_SCRIPT
+hash -r
+conda install -n base conda-libmamba-solver
+conda config --set solver libmamba
+conda config --set always_yes yes
+# Useful for debugging any issues with conda
+conda info -a
+echo "Successfully installed at $INSTALL_PREFIX. Please activate with 'source setup/activate_conda.sh' in every terminal where you want to use conda" 
