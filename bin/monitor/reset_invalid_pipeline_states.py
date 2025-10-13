a086ca6d1d6b9c8f2603bf473959279b371f7aaf
K. Shankari <k.shankari@nrel.gov>
2025-10-13 14:23:17 -0700

    𐂷 Expand to support running with federation
    Expand the pipeline reset script to support running with federation by invoking
    the `run_on_all_deployments` function. This function
    (https://github.com/e-mission/e-mission-server/pull/1009/commits/1c16800450656ea940ed6d252d219f42dae55403)
    pulls the set of active deployments and runs the script on each of them.
    
    This will allow us to track, through an automated script, whether the pipeline
    on any of the deployments is "stuck" and needs to be reset.
    
    Testing done:
    - Changed the implementation of the function to
    
    ```
    def reset_all_invalid_state(args):
        print(f"Running against {edb.url} with {args=}")
        # epr.auto_reset(args.dry_run, args.only_calc)
    ```
    
    Ran
    
    ```
    ./e-mission-py.bash bin/monitor/reset_invalid_pipeline_states.py -c -f
    ```
    
    Output was
    
    ```
    Retrieved config: {'DB_HOST': None, 'DB_RESULT_LIMIT': None, 'USE_HINTS': None, 'MONITOR_DB': None}
    URL not formatted, defaulting to "Stage_database"
    Connecting to database URL localhost
    created tmpdirname='/var/folders/y5/cx3cfzrd2q116myv9ly86sw1rnlmdj/T/tmpu9dp7rjk' to find list of configs
    Cloning into 'nrel-openpath-deploy-configs'...
    remote: Enumerating objects: 3680, done.
    remote: Counting objects: 100% (1730/1730), done.
    remote: Compressing objects: 100% (599/599), done.
    remote: Total 3680 (delta 1346), reused 1151 (delta 1131), pack-reused 1950 (from 2)
    Receiving objects: 100% (3680/3680), 2.64 MiB | 4.73 MiB/s, done.
    Resolving deltas: 100% (1980/1980), done.
    PROD_LIST: ['wyoming', 'mm-masscec', 'uue', 'ccebikes', 'walk-study-psu', 'unc-ebike', 'uw-prs', 'uprm-civic', 'sm-ebike', 'dfc-fermata', '4core-ebike', 'godcgo', 'sgv-ebike', 'e-bikes-for-essentials', 'uw-ebike', 'arlingtonebike', 'doee-electricbike-proj', 'ride2own', 'smart-commute-ebike', 'ebikethere-garfield-county', 'usaid-laos-ev', 'caeb-co', 'denver-casr', 'r2ohillsboro', 'nrel-commute', 'stm-community', 'r2omilwaukie', 'open-access', 'ca-ebike', 'r2oparkrose', 'r2oseportland', 'uprm-nicr', 'durham', 'nc-transit-equity-study', 'test', 'ebikegj', 'la-mw-ii', 'cortezebikes', 'dcebike', 'washingtoncommons', 'choose-your-ride', 'cosa-ebike-project', 'fortmorgan']
    About to run reset_all_invalid_state(Namespace(only_calc=True, dry_run=False, federated=True),) on 43 deployments. Proceed? [y/n]
    Running reset_all_invalid_state for wyoming on DB wyoming
    Config file not found, returning a copy of the environment variables instead...
    Retrieved config: {'DB_HOST': 'mongodb://localhost:27017/openpath_prod_wyoming', 'DB_RESULT_LIMIT': None, 'USE_HINTS': None, 'MONITOR_DB': None}
    Connecting to database URL mongodb://localhost:27017/openpath_prod_wyoming
    Running against mongodb://localhost:27017/openpath_prod_wyoming with args=Namespace(only_calc=True, dry_run=False, federated=True)
    Running reset_all_invalid_state for mm-masscec on DB mm_masscec
    Config file not found, returning a copy of the environment variables instead...
    Retrieved config: {'DB_HOST': 'mongodb://localhost:27017/openpath_prod_mm_masscec', 'DB_RESULT_LIMIT': None, 'USE_HINTS': None, 'MONITOR_DB': None}
    Connecting to database URL mongodb://localhost:27017/openpath_prod_mm_masscec
    Running against mongodb://localhost:27017/openpath_prod_mm_masscec with args=Namespace(only_calc=True, dry_run=False, federated=True)
    ...
    Running reset_all_invalid_state for fortmorgan on DB fortmorgan
    Config file not found, returning a copy of the environment variables instead...
    Retrieved config: {'DB_HOST': 'mongodb://localhost:27017/openpath_prod_fortmorgan', 'DB_RESULT_LIMIT': None, 'USE_HINTS': None, 'MONITOR_DB': None}
    Connecting to database URL mongodb://localhost:27017/openpath_prod_fortmorgan
    Running against mongodb://localhost:27017/openpath_prod_fortmorgan with args=Namespace(only_calc=True, dry_run=False, federated=True)
    ```

--- bin/monitor/reset_invalid_pipeline_states.py
+++ bin/monitor/reset_invalid_pipeline_states.py
@@ -3,6 +3,7 @@ import logging
 import argparse
 import pandas as pd
 
+from bin.federation import run_on_all_deployments
 import emission.core.wrapper.pipelinestate as ecwp
 import emission.core.get_database as edb
 import emission.pipeline.reset as epr
@@ -11,7 +12,6 @@ import emission.pipeline.reset as epr
 # sudo docker exec $CONTAINER bash -c 'cd e-mission-server; source setup/activate.sh; ./e-mission-py.bash bin/debug/reset_invalid_pipeline_state.py'
 
 def reset_all_invalid_state(args):
-    # all_invalid_states = [ecwp.PipelineState(p) for p in edb.get_pipeline_state_db().find({"curr_run_ts": {"$ne": None}})]
     epr.auto_reset(args.dry_run, args.only_calc)
 
 if __name__ == '__main__':
@@ -21,5 +21,12 @@ if __name__ == '__main__':
                         help="only calculate the reset timestamps, don't launch the reset process")
     parser.add_argument("-n", "--dry_run", action="store_true", default=False,
                         help="do everything except actually perform the operations")
+
+    parser.add_argument("-f", "--federated", action="store_true", default=False,
+                        help="Run against all active deployments")
+
     args = parser.parse_args()
-    reset_all_invalid_state(args)
+    if not args.federated:
+        reset_all_invalid_state(args)
+    else:
+        run_on_all_deployments(reset_all_invalid_state, args)
